db0a7ac70817578d01183f3d69673f429098e5b0
Dave Mielke <dave@mielke.cc>
2004-03-07 07:41:45 +0000

    Fix a 2.6 kernel problem with submitting an input URB for an interrupt endpoint. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@573 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/usb_linux.c
+++ Programs/usb_linux.c
@@ -285,7 +285,12 @@ usbSubmitRequest (
     LogPrint(LOG_DEBUG, "USB submit: urb=%p typ=%02X ept=%02X flg=%X sig=%d buf=%p len=%d ctx=%p",
              urb, urb->type, urb->endpoint, urb->flags, urb->signr,
              urb->buffer, urb->buffer_length, urb->usercontext);
+  submit:
     if (ioctl(device->file, USBDEVFS_SUBMITURB, urb) != -1) return urb;
+    if ((errno == EINVAL) && (urb->type == USBDEVFS_URB_TYPE_BULK)) {
+      urb->type = USBDEVFS_URB_TYPE_INTERRUPT;
+      goto submit;
+    }
 
     /* UHCI support returns ENXIO if a URB is already submitted. */
     if (errno != ENXIO) LogError("USB URB submit");
