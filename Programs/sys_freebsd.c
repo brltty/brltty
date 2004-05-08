4fe7c41d68d07e354fd5645d4407742ea8c458c0
Dave Mielke <dave@mielke.cc>
2004-05-08 01:26:18 +0000

    Add beep and PCM support for FreeBSD. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@749 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/sys_freebsd.c
+++ Programs/sys_freebsd.c
@@ -21,7 +21,14 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <dlfcn.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/kbio.h>
+#include <sys/soundcard.h>
 
 #include "misc.h"
 #include "system.h"
@@ -53,78 +60,158 @@ findSharedSymbol (void *object, const char *symbol, const void **address) {
   return 0;
 }
 
+#define BEEP_DIVIDEND 1193180
+
 int
 canBeep (void) {
+  if (getConsole() != -1) return 1;
   return 0;
 }
 
 int
 timedBeep (unsigned short frequency, unsigned short milliseconds) {
+  int console = getConsole();
+  if (console != -1) {
+    if (ioctl(console, KDMKTONE, ((milliseconds << 0X10) | (BEEP_DIVIDEND / frequency))) != -1) return 1;
+    LogError("ioctl KDMKTONE");
+  }
   return 0;
 }
 
 int
 startBeep (unsigned short frequency) {
+  int console = getConsole();
+  if (console != -1) {
+    if (ioctl(console, KIOCSOUND, BEEP_DIVIDEND/frequency) != -1) return 1;
+    LogError("ioctl KIOCSOUND");
+  }
   return 0;
 }
 
 int
 stopBeep (void) {
+  int console = getConsole();
+  if (console != -1) {
+    if (ioctl(console, KIOCSOUND, 0) != -1) return 1;
+    LogError("ioctl KIOCSOUND");
+  }
   return 0;
 }
 
 #ifdef ENABLE_PCM_TUNES
 int
 getPcmDevice (int errorLevel) {
-  LogPrint(errorLevel, "PCM device not supported.");
-  return -1;
+  const char *path = "/dev/dsp";
+  int descriptor = open(path, O_WRONLY|O_NONBLOCK);
+  if (descriptor == -1) LogPrint(errorLevel, "Cannot open PCM device: %s: %s", path, strerror(errno));
+  return descriptor;
 }
 
 int
 getPcmBlockSize (int descriptor) {
-  return 0X100;
+  int fragmentCount = (1 << 0X10) - 1;
+  int fragmentShift = 7;
+  int fragmentSize = 1 << fragmentShift;
+  int fragmentSetting = (fragmentCount << 0X10) | fragmentShift;
+  if (descriptor != -1) {
+    int blockSize;
+    ioctl(descriptor, SNDCTL_DSP_SETFRAGMENT, &fragmentSetting);
+    if (ioctl(descriptor, SNDCTL_DSP_GETBLKSIZE, &blockSize) != -1) return blockSize;
+  }
+  return fragmentSize;
 }
 
 int
 getPcmSampleRate (int descriptor) {
+  if (descriptor != -1) {
+    int rate;
+    if (ioctl(descriptor, SOUND_PCM_READ_RATE, &rate) != -1) return rate;
+  }
   return 8000;
 }
 
 int
 setPcmSampleRate (int descriptor, int rate) {
+  if (descriptor != -1) {
+    if (ioctl(descriptor, SOUND_PCM_WRITE_RATE, &rate) != -1) return rate;
+  }
   return getPcmSampleRate(descriptor);
 }
 
 int
 getPcmChannelCount (int descriptor) {
+  if (descriptor != -1) {
+    int channels;
+    if (ioctl(descriptor, SOUND_PCM_READ_CHANNELS, &channels) != -1) return channels;
+  }
   return 1;
 }
 
 int
 setPcmChannelCount (int descriptor, int channels) {
+  if (descriptor != -1) {
+    if (ioctl(descriptor, SOUND_PCM_WRITE_CHANNELS, &channels) != -1) return channels;
+  }
   return getPcmChannelCount(descriptor);
 }
 
-PcmAmplitudeFormat
-getPcmAmplitudeFormat (int descriptor) {
+typedef struct {
+  PcmAmplitudeFormat internal;
+  int external;
+} AmplitudeFormatEntry;
+static const AmplitudeFormatEntry amplitudeFormatTable[] = {
+  {PCM_FMT_U8     , AFMT_U8    },
+  {PCM_FMT_S8     , AFMT_S8    },
+  {PCM_FMT_U16B   , AFMT_U16_BE},
+  {PCM_FMT_S16B   , AFMT_S16_BE},
+  {PCM_FMT_U16L   , AFMT_U16_LE},
+  {PCM_FMT_S16L   , AFMT_S16_LE},
+  {PCM_FMT_ULAW   , AFMT_MU_LAW},
+  {PCM_FMT_UNKNOWN, AFMT_QUERY }
+};
+
+static PcmAmplitudeFormat
+doPcmAmplitudeFormat (int descriptor, int format) {
+  if (descriptor != -1) {
+    if (ioctl(descriptor, SNDCTL_DSP_SETFMT, &format) != -1) {
+      const AmplitudeFormatEntry *entry = amplitudeFormatTable;
+      while (entry->internal != PCM_FMT_UNKNOWN) {
+        if (entry->external == format) return entry->internal;
+        ++entry;
+      }
+    }
+  }
   return PCM_FMT_UNKNOWN;
 }
 
 PcmAmplitudeFormat
+getPcmAmplitudeFormat (int descriptor) {
+  return doPcmAmplitudeFormat(descriptor, AFMT_QUERY);
+}
+
+PcmAmplitudeFormat
 setPcmAmplitudeFormat (int descriptor, PcmAmplitudeFormat format) {
-  return getPcmAmplitudeFormat(descriptor);
+  const AmplitudeFormatEntry *entry = amplitudeFormatTable;
+  while (entry->internal != PCM_FMT_UNKNOWN) {
+    if (entry->internal == format) break;
+    ++entry;
+  }
+  return doPcmAmplitudeFormat(descriptor, entry->external);
 }
 
 void
 forcePcmOutput (int descriptor) {
+  ioctl(descriptor, SNDCTL_DSP_POST, 0);
 }
 
 void
 awaitPcmOutput (int descriptor) {
+  ioctl(descriptor, SNDCTL_DSP_SYNC, 0);
 }
 
 void
 cancelPcmOutput (int descriptor) {
+  ioctl(descriptor, SNDCTL_DSP_RESET, 0);
 }
 #endif /* ENABLE_PCM_TUNES */
 
