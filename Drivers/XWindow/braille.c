e4c90997ccc3bae11f9d69a9e4d977619aa3751f
Dave Mielke <dave@mielke.cc>
2004-08-02 03:13:36 +0000

    Add Xm support to the XWindow driver. (st)
    git-svn-id: svn://mielke.cc/main/brltty@1002 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/XWindow/braille.c
+++ Drivers/XWindow/braille.c
@@ -37,38 +37,88 @@
 #include <X11/Shell.h>
 #define HAVE_LIB_XAW
 #if defined(HAVE_LIB_XAW)
+#define USE_XAW
 #include <X11/Xaw/Form.h>
 #include <X11/Xaw/Paned.h>
 #include <X11/Xaw/Label.h>
 #include <X11/Xaw/Command.h>
 #include <X11/Xaw/Repeater.h>
 #elif defined(HAVE_LIB_XAW3D)
+#define USE_XAW
 #include <X11/Xaw3d/Form.h>
 #include <X11/Xaw3d/Paned.h>
 #include <X11/Xaw3d/Label.h>
 #include <X11/Xaw3d/Command.h>
 #include <X11/Xaw3d/Repeater.h>
 #elif defined(HAVE_LIB_NEXTAW)
+#define USE_XAW
 #include <X11/neXtaw/Form.h>
 #include <X11/neXtaw/Paned.h>
 #include <X11/neXtaw/Label.h>
 #include <X11/neXtaw/Command.h>
 #include <X11/neXtaw/Repeater.h>
 #elif defined(HAVE_LIB_XAWPLUS)
+#define USE_XAW
 #include <X11/XawPlus/Form.h>
 #include <X11/XawPlus/Paned.h>
 #include <X11/XawPlus/Label.h>
 #include <X11/XawPlus/Command.h>
 #include <X11/XawPlus/Repeater.h>
+#elif defined(HAVE_LIB_XM)
+#define USE_XM
+#include <Xm/Xm.h>
+#include <Xm/Form.h>
+#include <Xm/PanedW.h>
+#include <Xm/Label.h>
+#include <Xm/PushB.h>
+#endif
+
+#ifdef USE_XAW
+#define formWidgetClass     formWidgetClass
+#define panedWidgetClass    panedWidgetClass
+#define labelWidgetClass    labelWidgetClass
+#define commandWidgetClass  commandWidgetClass
+#define repeaterWidgetClass repeaterWidgetClass
+#define Nlabel              XtNlabel
+#define Ncallback           XtNcallback
+#define Ntop                XtNtop
+#define Nbottom             XtNbottom
+#define Nleft               XtNleft
+#define Nright              XtNright
+#define ChainTop            XtChainTop
+#define ChainBottom         XtChainTop
+#define ChainLeft           XtChainLeft
+#define ChainRight          XtChainLeft
+#define NvertDistance       XtNvertDistance
+#define NhorizDistance      XtNhorizDistance
+#else
+#define formWidgetClass     xmFormWidgetClass
+#define panedWidgetClass    xmPanedWindowWidgetClass
+#define labelWidgetClass    xmLabelWidgetClass
+#define commandWidgetClass  xmPushButtonWidgetClass
+#define repeaterWidgetClass xmPushButtonWidgetClass
+#define Nlabel              XmNlabelString
+#define Ncallback           XmNactivateCallback
+#define Ntop                XmNtopAttachment
+#define Nbottom             XmNbottomAttachment
+#define Nleft               XmNleftAttachment
+#define Nright              XmNrightAttachment
+#define ChainTop            XmATTACH_FORM
+#define ChainBottom         XmATTACH_NONE
+#define ChainLeft           XmATTACH_FORM
+#define ChainRight          XmATTACH_NONE
+#define NvertDistance       XmNtopOffset
+#define NhorizDistance      XmNleftOffset
 #endif
 
 typedef enum {
   PARM_DISPLAY,
   PARM_TERM,
   PARM_LINES,
-  PARM_COLS
+  PARM_COLS,
+  PARM_MODEL
 } DriverParameter;
-#define BRLPARMS "display", "term", "lines", "cols"
+#define BRLPARMS "display", "term", "lines", "cols", "model"
 
 #include "Programs/brl_driver.h"
 #include "braille.h"
@@ -77,12 +127,16 @@ typedef enum {
 #define MAXCOLS 80
 #define WHOLESIZE (MAXLINES * MAXCOLS)
 static int cols,lines;
+static char *model;
 
 #define BUTWIDTH 48
 #define BUTHEIGHT 32
 
 static Widget toplevel,vbox,keybox,display;
 static XtAppContext app_con;
+#ifdef USE_XM
+static XmString display_cs;
+#endif
 
 static long keypressed;
 
@@ -98,15 +152,17 @@ static inline Widget crKeyBut(String name, long keycode, int repeat,
   button = XtVaCreateManagedWidget(name,
     repeat?repeaterWidgetClass:commandWidgetClass, keybox,
     XtNwidth, BUTWIDTH, XtNheight, BUTHEIGHT,
+#ifdef USE_XAW
     XtNinitialDelay, 500, XtNminimumDelay, 100,
-    XtNhorizDistance, horizDistance,
-    XtNvertDistance, vertDistance,
-    XtNtop, XtChainTop,
-    XtNbottom, XtChainTop,
-    XtNleft, XtChainLeft,
-    XtNright, XtChainLeft,
+#endif
+    NhorizDistance, horizDistance,
+    NvertDistance, vertDistance,
+    Ntop, ChainTop,
+    Nbottom, ChainBottom,
+    Nleft, ChainLeft,
+    Nright, ChainRight,
     NULL);
-  XtAddCallback(button, XtNcallback, KeyPressCB, (XtPointer) keycode);
+  XtAddCallback(button, Ncallback, KeyPressCB, (XtPointer) keycode);
   return button;
 }
 
@@ -116,6 +172,10 @@ static void createKeyButtons(void) {
  crKeyBut("Home",  BRL_CMD_HOME,   0, BUTWIDTH+1,     BUTHEIGHT+1);
  crKeyBut("Right", BRL_CMD_FWINRT, 1, 2*(BUTWIDTH+1), BUTHEIGHT+1);
  crKeyBut("Down",  BRL_CMD_LNDN,   1, BUTWIDTH+1,     2*(BUTHEIGHT+1));
+ crKeyBut("alt-a", BRL_FLG_CHAR_META | BRL_BLK_PASSCHAR | 'a', 0, 0, 3*(BUTHEIGHT+1));
+ crKeyBut("ctrl-a", BRL_FLG_CHAR_CONTROL | BRL_BLK_PASSCHAR | 'a', 0, BUTWIDTH+1, 3*(BUTHEIGHT+1));
+ crKeyBut("a", BRL_BLK_PASSCHAR | 'a', 0, 2*(BUTWIDTH)+1, 3*(BUTHEIGHT+1));
+ crKeyBut("A", BRL_BLK_PASSCHAR | 'A', 0, 3*(BUTWIDTH)+1, 3*(BUTHEIGHT+1));
 }
 
 static int brl_readCommand(BrailleDisplay *brl, BRL_DriverCommandContext context)
@@ -182,6 +242,9 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
   argv[argc++]="-display";
   argv[argc++]=parameters[PARM_DISPLAY];
  }
+
+ if (*parameters[PARM_MODEL])
+  model = parameters[PARM_MODEL];
  
  XtSetLanguageProc(NULL, NULL, NULL);
  
@@ -195,6 +258,11 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
 
  /* vertical separation */
  vbox = XtVaCreateManagedWidget("vbox",panedWidgetClass,toplevel,
+#ifdef USE_XM
+   XmNmarginHeight, 0,
+   XmNmarginWidth, 0,
+   XmNspacing, 1,
+#endif
    XtNresize, True,
    NULL);
 
@@ -205,18 +273,38 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
   disp[(y+1)*(cols+1)-1]='\n';
  }
  disp[lines*(cols+1)]=0;
+#ifdef USE_XM
+ display_cs = XmStringCreateLocalized(disp);
+#endif
  display = XtVaCreateManagedWidget("display",labelWidgetClass,vbox,
+#ifdef USE_XAW
    XtNshowGrip,False,
-   XtNresize, True,
+#else
+   XmNpaneMaximum,20,
+   XmNpaneMinimum,20,
+   XmNskipAdjust, True,
+#endif
+#ifdef USE_XAW
    XtNlabel, disp,
+#else
+   XmNlabelString, display_cs,
+#endif
    NULL);
  XtFree(disp);
+#ifdef USE_XM
+ XmStringFree(display_cs);
+#endif
 
- /* key box */
- keybox = XtVaCreateManagedWidget("keybox",formWidgetClass,vbox,
-   XtNdefaultDistance,0,
-   NULL);
- createKeyButtons();
+
+ if (!model || strcmp(model,"bare")) {
+   /* key box */
+   keybox = XtVaCreateManagedWidget("keybox",formWidgetClass,vbox,
+#ifdef USE_XAW
+     XtNdefaultDistance,0,
+#endif
+     NULL);
+   createKeyButtons();
+ }
 
  /* go go go */
  XtRealizeWidget(toplevel);
@@ -254,7 +342,19 @@ static void brl_writeWindow(BrailleDisplay *brl)
 
  memcpy(displayed,data,brl->y*(brl->x+1));
 
- XtVaSetValues(display,XtNlabel,displayed,NULL);
+#ifdef USE_XM
+ display_cs = XmStringCreateLocalized(displayed);
+#endif
+ XtVaSetValues(display,
+#ifdef USE_XAW
+   XtNlabel, displayed,
+#else
+   XmNlabelString, display_cs,
+#endif
+   NULL);
+#ifdef USE_XM
+ XmStringFree(display_cs);
+#endif
 }
 
 static void brl_writeStatus(BrailleDisplay *brl, const unsigned char *s)
