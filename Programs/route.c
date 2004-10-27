da52f6b2f9ae22dc6aca63dd109cf3a69ac40f4a
Dave Mielke <dave@mielke.cc>
2004-10-27 11:19:45 +0000

    New cursor routing algorithm (works with editors which wrap long lines). (dm)
    git-svn-id: svn://mielke.cc/main/brltty@1195 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/route.c
+++ Programs/route.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <signal.h>
 
 #include "misc.h"
@@ -35,109 +36,186 @@
  * NOTE: if you try to route the cursor to an invalid place, BRLTTY won't
  * give up until the timeout has elapsed!
  */
-#define CURSOR_ROUTING_NICENESS 10	/* niceness of cursor routing subprocess */
-#define CURSOR_ROUTING_TIMEOUT 2000	/* max wait for response to key press */
-#define CURSOR_ROUTING_INTERVAL 0	/* how often to check for response */
-#define CURSOR_ROUTING_SETTLE 400	/* grace period for temporary cursor relocation */
+#define CURSOR_ROUTING_NICENESS	10	/* niceness of cursor routing subprocess */
+#define CURSOR_ROUTING_INTERVAL	0	/* how often to check for response */
+#define CURSOR_ROUTING_TIMEOUT	2000	/* max wait for response to key press */
 
 volatile pid_t routingProcess = 0;
 volatile int routingStatus = -1;
 
+typedef enum {
+  CRR_DONE,
+  CRR_NEAR,
+  CRR_FAIL
+} CursorRoutingResult;
+
+typedef struct {
+  sigset_t signalMask;
+
+  int screenNumber;
+  int cury, curx;
+  int oldy, oldx;
+
+  long timeSum;
+  int timeCount;
+} CursorRoutingData;
+
+static int
+getCurrentPosition (CursorRoutingData *crd) {
+  ScreenDescription scr;
+  describeRoutingScreen(&scr);
+
+  if (scr.no != crd->screenNumber) {
+    crd->screenNumber = scr.no;
+    return 0;
+  }
+
+  crd->cury = scr.posy;
+  crd->curx = scr.posx;
+  return 1;
+}
+
 static void
-insertCursorKey (ScreenKey key, const sigset_t *mask) {
-  sigset_t old;
-  sigprocmask(SIG_BLOCK, mask, &old);
+insertCursorKey (CursorRoutingData *crd, ScreenKey key) {
+  sigset_t oldMask;
+  sigprocmask(SIG_BLOCK, &crd->signalMask, &oldMask);
   insertKey(key);
-  sigprocmask(SIG_SETMASK, &old, NULL);
+  sigprocmask(SIG_SETMASK, &oldMask, NULL);
+}
+
+static int
+awaitCursorMotion (CursorRoutingData *crd) {
+  long timeout = crd->timeSum / crd->timeCount;
+  struct timeval start;
+  gettimeofday(&start, NULL);
+
+  while (1) {
+    long time;
+    approximateDelay(CURSOR_ROUTING_INTERVAL);
+    time = millisecondsSince(&start) + 1;
+
+    crd->oldy = crd->cury;
+    crd->oldx = crd->curx;
+    if (!getCurrentPosition(crd)) return 0;
+
+    if ((crd->cury != crd->oldy) || (crd->curx != crd->oldx)) {
+      crd->timeSum += time * 8;
+      crd->timeCount += 1;
+      break;
+    }
+
+    if (time > timeout) break;
+  }
+
+  return 1;
+}
+
+static CursorRoutingResult
+adjustCursorPosition (CursorRoutingData *crd, int where, int trgy, int trgx, ScreenKey forward, ScreenKey backward) {
+  while (1) {
+    int dify = trgy - crd->cury;
+    int difx = (trgx >= 0)? (trgx - crd->curx): 0;
+    int dir;
+
+    /* determine which direction the cursor needs to move in */
+    if (dify) {
+      dir = (dify > 0)? 1: -1;
+    } else if (difx) {
+      dir = (difx > 0)? 1: -1;
+    } else {
+      return CRR_DONE;
+    }
+
+    /* tell the cursor to move in the needed direction */
+    insertCursorKey(crd, ((dir > 0)? forward: backward));
+    if (!awaitCursorMotion(crd)) return CRR_FAIL;
+
+    if (crd->cury != crd->oldy) {
+      if (crd->oldy != trgy) {
+        if (((crd->cury - crd->oldy) * dir) > 0) {
+          int dif = trgy - crd->cury;
+          if ((dif * dify) >= 0) continue;
+          if (where > 0) {
+            if (crd->cury > trgy) return CRR_NEAR;
+          } else if (where < 0) {
+            if (crd->cury < trgy) return CRR_NEAR;
+          } else {
+            if ((dif * dif) < (dify * dify)) return CRR_NEAR;
+          }
+        }
+      }
+    } else if (crd->curx != crd->oldx) {
+      if (((crd->curx - crd->oldx) * dir) > 0) {
+        int dif = trgx - crd->curx;
+        if (crd->cury != trgy) continue;
+        if ((dif * difx) >= 0) continue;
+        if (where > 0) {
+          if (crd->curx > trgx) return CRR_NEAR;
+        } else if (where < 0) {
+          if (crd->curx < trgx) return CRR_NEAR;
+        } else {
+          if ((dif * dif) < (difx * difx)) return CRR_NEAR;
+        }
+      }
+    } else {
+      return CRR_NEAR;
+    }
+
+    /* We're getting farther from our target. Before giving up, let's
+     * try going back to the previous position since it was obviously
+     * the nearest ever reached.
+     */
+    insertCursorKey(crd, ((dir > 0)? backward: forward));
+    return awaitCursorMotion(crd)? CRR_NEAR: CRR_FAIL;
+  }
+}
+
+static CursorRoutingResult
+adjustCursorHorizontally (CursorRoutingData *crd, int where, int trgy, int trgx) {
+  return adjustCursorPosition(crd, where, trgy, trgx, SCR_KEY_CURSOR_RIGHT, SCR_KEY_CURSOR_LEFT);
+}
+
+static CursorRoutingResult
+adjustCursorVertically (CursorRoutingData *crd, int where, int trgy) {
+  return adjustCursorPosition(crd, where, trgy, -1, SCR_KEY_CURSOR_DOWN, SCR_KEY_CURSOR_UP);
 }
 
 static int
 doCursorRouting (int column, int row, int screen) {
-  ScreenDescription scr;		/* for screen state infos */
-  int oldx, oldy, dif;
-  sigset_t mask;		/* for blocking of SIGUSR1 */
+  CursorRoutingData crd;
 
   /* Configure the cursor routing subprocess. */
   nice(CURSOR_ROUTING_NICENESS); /* reduce scheduling priority */
 
   /* Set up the signal mask. */
-  sigemptyset(&mask);
-  sigaddset(&mask, SIGUSR1);
-  sigprocmask(SIG_UNBLOCK, &mask, NULL);
+  sigemptyset(&crd.signalMask);
+  sigaddset(&crd.signalMask, SIGUSR1);
+  sigprocmask(SIG_UNBLOCK, &crd.signalMask, NULL);
+
+  /* initialize the key response time cache */
+  crd.timeSum = CURSOR_ROUTING_TIMEOUT;
+  crd.timeCount = 1;
 
   /* Initialise second thread of screen reading: */
   if (!openRoutingScreen()) return ROUTE_ERROR;
-  describeRoutingScreen(&scr);
+  crd.screenNumber = screen;
 
-  /* Deal with vertical movement first, ignoring horizontal jumping ... */
-  while ((dif = row - scr.posy) && (scr.no == screen)) {
-    insertCursorKey((dif > 0)? SCR_KEY_CURSOR_DOWN: SCR_KEY_CURSOR_UP, &mask);
-    hasTimedOut(0);		/* initialise stop-watch */
-    while (1) {
-      approximateDelay(CURSOR_ROUTING_INTERVAL);	/* sleep a while ... */
-
-      oldy = scr.posy;
-      oldx = scr.posx;
-      describeRoutingScreen(&scr);
-      if ((scr.posy != oldy) || (scr.posx != oldx)) break;
-
-      if (hasTimedOut(CURSOR_ROUTING_TIMEOUT)) return ROUTE_WRONG_ROW;
-    }
-
-    if ((scr.posy == oldy && (scr.posx - oldx) * dif <= 0) ||
-        (scr.posy != oldy && (row - scr.posy) * (row - scr.posy) >= dif * dif)) {
-      approximateDelay(CURSOR_ROUTING_SETTLE);
-      describeRoutingScreen(&scr);
-      if ((scr.posy == oldy && (scr.posx - oldx) * dif <= 0) ||
-          (scr.posy != oldy && (row - scr.posy) * (row - scr.posy) >= dif * dif)) {
-        /* 
-         * We are getting farther from our target... Let's try to 
-         * go back to the previous position wich was obviously the 
-         * nearest ever reached to date before giving up.
-         */
-        insertCursorKey((dif < 0)? SCR_KEY_CURSOR_DOWN: SCR_KEY_CURSOR_UP, &mask);
-        break;
-      }
-    }
-  }
-
-  if (column >= 0) {	/* don't do this for vertical-only routing (column=-1) */
-    /* Now horizontal movement, quitting if the vertical position is wrong: */
-    while ((dif = column - scr.posx) && (scr.posy == row) && (scr.no == screen)) {
-      insertCursorKey((dif > 0)? SCR_KEY_CURSOR_RIGHT: SCR_KEY_CURSOR_LEFT, &mask);
-      hasTimedOut(0);	/* initialise stop-watch */
-      while (1) {
-        approximateDelay(CURSOR_ROUTING_INTERVAL);	/* sleep a while ... */
-
-        oldx = scr.posx;
-        describeRoutingScreen(&scr);
-        if ((scr.posx != oldx) || (scr.posy != row)) break;
-
-        if (hasTimedOut(CURSOR_ROUTING_TIMEOUT)) return ROUTE_WRONG_COLUMN;
-      }
-
-      if (scr.posy != row ||
-          (column - scr.posx) * (column - scr.posx) >= dif * dif) {
-        approximateDelay(CURSOR_ROUTING_SETTLE);
-        describeRoutingScreen(&scr);
-        if (scr.posy != row ||
-            (column - scr.posx) * (column - scr.posx) >= dif * dif) {
-          /* 
-           * We probably wrapped on a short line... or are getting 
-           * farther from our target. Try to get back to the previous
-           * position which was obviously the nearest ever reached
-           * to date before we exit.
-           */
-          insertCursorKey((dif > 0)? SCR_KEY_CURSOR_LEFT: SCR_KEY_CURSOR_RIGHT, &mask);
-          break;
-        }
-      }
+  if (getCurrentPosition(&crd)) {
+    if (column < 0) {
+      adjustCursorVertically(&crd, 0, row);
+    } else {
+      (adjustCursorVertically(&crd, -1, row) == CRR_FAIL) ||
+      (adjustCursorHorizontally(&crd, 0, row, column) != CRR_NEAR) ||
+      (crd.cury >= row) ||
+      (adjustCursorVertically(&crd, 1, crd.cury+1) == CRR_FAIL) ||
+      (adjustCursorHorizontally(&crd, 0, row, column) != CRR_NEAR);
     }
   }
 
   closeRoutingScreen();		/* close second thread of screen reading */
-  if (scr.posy != row) return ROUTE_WRONG_ROW;
-  if ((column >= 0) && (scr.posx != column)) return ROUTE_WRONG_COLUMN;
+  if (crd.screenNumber != screen) return ROUTE_ERROR;
+  if (crd.cury != row) return ROUTE_WRONG_ROW;
+  if ((column >= 0) && (crd.curx != column)) return ROUTE_WRONG_COLUMN;
   return ROUTE_OK;
 }
 
