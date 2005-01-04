9f7344e6d253edb47a863a527bbffdb3e0d839e7
Dave Mielke <dave@mielke.cc>
2005-01-04 03:16:43 +0000

    Remove the use of PATH_MAX from the Windows implementation of getProgramPath(). (dm)
    git-svn-id: svn://mielke.cc/main/brltty@1287 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/sys_prog_windows.h
+++ Programs/sys_prog_windows.h
@@ -17,12 +17,41 @@
 
 char *
 getProgramPath (void) {
-  CHAR path[MAX_PATH];
-  DWORD length = GetModuleFileName(GetModuleHandle(NULL), path, sizeof(path));
+  char *path = NULL;
+  HMODULE handle;
 
-  while (length > 0)
-    if (path[--length] == '\\')
-      path[length] = '/';
+  if ((handle = GetModuleHandle(NULL))) {
+    size_t size = 0X80;
+    char *buffer = NULL;
 
-  return strdupWrapper(path);
+    while (1) {
+      buffer = reallocWrapper(buffer, size<<=1);
+
+      {
+        DWORD length = GetModuleFileName(handle, buffer, size);
+
+        if (!length) {
+          LogWindowsError("GetModuleFileName");
+          break;
+        }
+
+        if (length < size) {
+          buffer[length] = 0;
+          path = strdupWrapper(buffer);
+
+          while (length > 0)
+            if (path[--length] == '\\')
+              path[length] = '/';
+
+          break;
+        }
+      }
+    }
+
+    free(buffer);
+  } else {
+    LogWindowsError("GetModuleHandle");
+  }
+
+  return path;
 }
