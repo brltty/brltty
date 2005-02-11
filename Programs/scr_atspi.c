67d7db2237dcd915de50ac33fa5058ad60c1e9fc
Dave Mielke <dave@mielke.cc>
2005-02-11 01:53:12 +0000

    Add the atspi screen driver. (st)
    git-svn-id: svn://mielke.cc/main/brltty@1386 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Programs/scr_atspi.c
@@ -0,0 +1,549 @@
+/*
+ * BRLTTY - A background process providing access to AT-SPI terminals
+ *          for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2005 by The BRLTTY Team. All rights reserved.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <wchar.h>
+#include <pthread.h>
+#include <limits.h>
+#include <langinfo.h>
+#include <semaphore.h>
+#include <locale.h>
+
+#include <cspi/spi.h>
+#include <X11/keysym.h>
+
+#include "misc.h"
+#include "scr.h"
+#include "scr_real.h"
+#include "brldefs.h"
+
+static AccessibleText *curTerm;
+static Accessible *curFocus;
+
+static long curNumRows, curNumCols;
+static wchar_t **curRows;
+static long *curRowLengths;
+static long curPosX,curPosY;
+static pthread_mutex_t updateMutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_t SPI_main_thread;
+
+static size_t mbslen(const char *s, size_t n) {
+  mbstate_t ps;
+  size_t ret=0;
+  size_t eaten;
+  memset(&ps,0,sizeof(ps));
+  while(n) {
+    if ((eaten = mbrlen(s,n,&ps))<0)
+      return eaten;
+    if (!(eaten)) return ret;
+    s+=eaten;
+    n-=eaten;
+    ret++;
+  }
+  return ret;
+}
+
+static void addRows(long pos, long num) {
+  curNumRows += num;
+  curRows = realloc(curRows,curNumRows*sizeof(*curRows));
+  curRowLengths = realloc(curRowLengths,curNumRows*sizeof(*curRowLengths));
+  memmove(curRows      +pos+num,curRows      +pos,(curNumRows-(pos+num))*sizeof(*curRows));
+  memmove(curRowLengths+pos+num,curRowLengths+pos,(curNumRows-(pos+num))*sizeof(*curRowLengths));
+}
+
+static void delRows(long pos, long num) {
+  long y;
+  for (y=pos;y<pos+num;y++)
+    free(curRows[y]);
+  memmove(curRows      +pos,curRows      +pos+num,(curNumRows-(pos+num))*sizeof(*curRows));
+  memmove(curRowLengths+pos,curRowLengths+pos+num,(curNumRows-(pos+num))*sizeof(*curRowLengths));
+  curNumRows -= num;
+  curRows = realloc(curRows,curNumRows*sizeof(*curRows));
+  curRowLengths = realloc(curRowLengths,curNumRows*sizeof(*curRowLengths));
+}
+
+static const char *const screenParameters[] = {
+  NULL
+};
+
+static const char *const *
+parameters_AtSPIScreen (void) {
+  return screenParameters;
+}
+
+static int
+prepare_AtSPIScreen (char **parameters) {
+  char *codeset;
+  setlocale(LC_ALL,"");
+  codeset = nl_langinfo(CODESET);
+  if (!codeset || strcmp(codeset,"UTF-8")) {
+    LogPrint(LOG_ERR,"no UTF-8 support !");
+    return 0;
+  }
+  return 1;
+}
+
+static void findPosition(long position, long *px, long *py) {
+  long offset=0, newoffset, x, y;
+  /* XXX: I don't know what they do with necessary combining accents */
+  for (y=0; y<curNumRows; y++) {
+    if ((newoffset = offset + curRowLengths[y]) > position)
+      break;
+    offset = newoffset;
+  }
+  x = position-offset;
+  *px = x;
+  *py = y;
+}
+
+static void caretPosition(long caret) {
+  findPosition(caret,&curPosX,&curPosY);
+}
+
+static void finiTerm(void) {
+  Accessible_unref(curTerm);
+  curTerm = NULL;
+  Accessible_unref(curFocus);
+  curFocus = NULL;
+  curPosX = curPosY = 0;
+}
+
+static void restartTerm(Accessible *newTerm) {
+  char *c,*d;
+  const char *e;
+  long i,len;
+  char *text;
+  finiTerm();
+  Accessible_ref(curFocus = newTerm);
+  curTerm = Accessible_getText(newTerm);
+  LogPrint(LOG_DEBUG,"new term %p",curTerm);
+  text = AccessibleText_getText(curTerm,0,LONG_MAX);
+  curNumRows = 0;
+  if (curRows) {
+    for (i=0;i<curNumRows;i++)
+      free(curRows[i]);
+    free(curRows);
+  }
+  free(curRowLengths);
+  c = text;
+  while (*c) {
+    curNumRows++;
+    if (!(c = strchr(c,'\n')))
+      break;
+    c++;
+  }
+  LogPrint(LOG_DEBUG,"%ld rows",curNumRows);
+  curRows = malloc(curNumRows * sizeof(*curRows));
+  curRowLengths = malloc(curNumRows * sizeof(*curRowLengths));
+  i = 0;
+  curNumCols = 0;
+  for (c = text; *c; c = d+1) {
+    d = strchr(c,'\n');
+    if (d)
+      *d = 0;
+    e = c;
+    curRowLengths[i] = (len = mbsrtowcs(NULL,&e,0,NULL)) + (d != NULL);
+    if (len > curNumCols)
+      curNumCols = len;
+    else if (len < 0) {
+      if (len==-2)
+	LogPrint(LOG_ERR,"unterminated sequence %s",c);
+      else if (len==-1)
+	LogError("mbrlen");
+      curRowLengths[i] = (len = -1) + (d != NULL);
+    }
+    curRows[i] = malloc((len + (d!=NULL)) * sizeof(*curRows[i]));
+    e = c;
+    mbsrtowcs(curRows[i],&e,len,NULL);
+    if (d)
+      curRows[i][len]='\n';
+    else
+      break;
+    i++;
+  }
+  LogPrint(LOG_DEBUG,"%ld cols",curNumCols);
+  SPI_freeString(text);
+  caretPosition(AccessibleText_getCaretOffset(curTerm));
+}
+
+static struct evList {
+  struct evList *next;
+  const AccessibleEvent *ev;
+} *evs;
+
+static void evListenerCB(const AccessibleEvent *event, void *user_data) {
+  static int running = 0;
+  struct evList *ev = malloc(sizeof(*ev));
+  AccessibleEvent_ref(event);
+  ev->next = evs;
+  ev->ev = event;
+  evs = ev;
+
+  /* this is not atomic but we can only be recursively called within calls
+   * to the lib */
+  if (running)
+    return;
+  else
+    running = 1;
+
+  while (evs) {
+    pthread_mutex_lock(&updateMutex);
+    /* pickup a list of events to handle */
+    ev = evs;
+    evs = NULL;
+    for (; ev; ev = ev->next) {
+      event = ev->ev;
+      if (!strcmp(event->type,"focus:")) {
+	if (Accessible_getRole(event->source) == SPI_ROLE_TERMINAL)
+	  restartTerm(event->source);
+	else
+	  finiTerm();
+      } else if (!strcmp(event->type,"object:text-caret-moved")) {
+	if (event->source != curFocus) continue;
+	caretPosition(event->detail1);
+      } else if (!strcmp(event->type,"object:text-changed:delete")) {
+	long x,y,toDelete = event->detail2;
+	long length, toCopy;
+	long downTo; /* line that will provide what will follow x */
+	if (event->source != curFocus) continue;
+	findPosition(event->detail1,&x,&y);
+	downTo = y;
+	length = curRowLengths[downTo];
+	while (x+toDelete >= length) {
+	  downTo++;
+	  if (downTo <= curNumRows - 1)
+	    length += curRowLengths[downTo];
+	  else {
+	    /* imaginary extra line doesn't provide more length, and shouldn't need to ! */
+	    if (x+toDelete > length) {
+	      LogPrint(LOG_ERR,"deleting past end of text !");
+	      /* discarding */
+	      toDelete = length - x;
+	    }
+	    break; /* deleting up to end */
+	  }
+	}
+	if (length-toDelete>0) {
+	  /* still something on line y */
+	  if ((toCopy = length-toDelete-x))
+	    memcpy(curRows[y]+x,curRows[downTo]+curRowLengths[downTo]-toCopy,toCopy*sizeof(curRows[downTo]));
+	  curRowLengths[y] = length-toDelete;
+	  curRows[y]=realloc(curRows[y],curRowLengths[y]*sizeof(*curRows[y]));
+	} else {
+	  /* kills this line as well ! */
+	  y--;
+	}
+	if (downTo==curNumRows)
+	  /* imaginary extra line needs not be deleted */
+	  downTo--;
+	delRows(y+1,downTo-y);
+	caretPosition(AccessibleText_getCaretOffset(curTerm));
+      } else if (!strcmp(event->type,"object:text-changed:insert")) {
+	long len=event->detail2,semilen,x,y;
+	char *added;
+	const char *adding,*c;
+	if (event->source != curFocus) continue;
+	findPosition(event->detail1,&x,&y);
+	adding = c = added = AccessibleTextChangedEvent_getChangeString(event);
+	if (x && (c = strchr(adding,'\n'))) {
+	  /* splitting line */
+	  addRows(y,1);
+	  semilen=mbslen(adding,c+1-adding);
+	  curRowLengths[y]=x+semilen;
+	  if (x+semilen-1>curNumCols)
+	    curNumCols=x+semilen-1;
+
+	  /* copy beginning */
+	  curRows[y]=malloc(curRowLengths[y]*sizeof(*curRows[y]));
+	  memcpy(curRows[y],curRows[y+1],x*sizeof(*curRows[y]));
+	  /* add */
+	  mbsrtowcs(curRows[y]+x,&adding,semilen,NULL);
+	  len-=semilen;
+	  adding=c+1;
+	  /* shift end */
+	  curRowLengths[y+1]-=x;
+	  memmove(curRows[y+1],curRows[y+1]+x,curRowLengths[y+1]*sizeof(*curRows[y+1]));
+	  x=0;
+	  y++;
+	}
+	while ((c = strchr(adding,'\n'))) {
+	  /* adding lines */
+	  addRows(y,1);
+	  semilen=mbslen(adding,c+1-adding);
+	  curRowLengths[y]=semilen;
+	  if (semilen-1>curNumCols)
+	    curNumCols=semilen-1;
+	  curRows[y]=malloc(semilen*sizeof(*curRows[y]));
+	  mbsrtowcs(curRows[y],&adding,semilen,NULL);
+	  len-=semilen;
+	  adding=c+1;
+	  y++;
+	}
+	if (len) {
+	  /* still length to add on the line following it */
+	  if (y==curNumRows) {
+	    /* It won't insert ending \n yet */
+	    addRows(y,1);
+	    curRows[y]=NULL;
+	    curRowLengths[y]=0;
+	  }
+	  curRowLengths[y] += len;
+	  curRows[y]=realloc(curRows[y],curRowLengths[y]*sizeof(*curRows[y]));
+	  memmove(curRows[y]+x+len,curRows[y]+x,(curRowLengths[y]-(x+len))*sizeof(*curRows[y]));
+	  mbsrtowcs(curRows[y]+x,&adding,len,NULL);
+	  if (curRowLengths[y]-(curRows[y][curRowLengths[y]-1]=='\n')>curNumCols)
+	    curNumCols=curRowLengths[y]-(curRows[y][curRowLengths[y]-1]=='\n');
+	}
+	SPI_freeString(added);
+	caretPosition(AccessibleText_getCaretOffset(curTerm));
+      } else
+	LogPrint(LOG_INFO,"event %s, source %p, detail1 %lu detail2 %lu",event->type,event->source,event->detail1,event->detail2);
+      AccessibleEvent_unref(event);
+    }
+    pthread_mutex_unlock(&updateMutex);
+  }
+  running = 0;
+}
+
+static void *doAtSPIScreenOpen(void *arg) {
+  AccessibleEventListener *evListener;
+  sem_t *SPI_init_sem = (sem_t *)arg;
+  int res;
+  static const char *events[] = {
+    "object:text-changed",
+    "object:text-caret-moved",
+    "focus:",
+  };
+  const char **event;
+  if ((res=SPI_init())) {
+    LogPrint(LOG_ERR,"SPI_init returned %d",res);
+    return 0;
+  }
+  if (!(evListener = SPI_createAccessibleEventListener(evListenerCB,NULL)))
+    LogPrint(LOG_ERR,"SPI_createAccessibleEventListener failed");
+  else for (event=events; event<&events[sizeof(events)/sizeof(*events)]; event++)
+    if (!(SPI_registerGlobalEventListener(evListener,*event)))
+      LogPrint(LOG_ERR,"SPI_registerGlobalEventListener(%s) failed",*event);
+  sem_post(SPI_init_sem);
+  SPI_event_main();
+  if (!(SPI_deregisterGlobalEventListenerAll(evListener)))
+    LogPrint(LOG_ERR,"SPI_deregisterGlobalEventListenerAll failed");
+  AccessibleEventListener_unref(evListener);
+  if ((res=SPI_exit()))
+    LogPrint(LOG_ERR,"SPI_exit returned %d",res);
+  return NULL;
+}
+
+static int
+open_AtSPIScreen (void) {
+  sem_t SPI_init_sem;
+  sem_init(&SPI_init_sem,0,0);
+  if (pthread_create(&SPI_main_thread,NULL,doAtSPIScreenOpen,(void *)&SPI_init_sem)) {
+    LogPrint(LOG_ERR,"main SPI thread failed to be launched");
+    return 0;
+  }
+  sem_wait(&SPI_init_sem);
+  LogPrint(LOG_DEBUG,"SPI initialized");
+  return 1;
+}
+
+static int
+setup_AtSPIScreen (void) {
+  return 1;
+}
+
+static void
+close_AtSPIScreen (void) {
+  SPI_event_quit();
+  pthread_join(SPI_main_thread,NULL);
+  LogPrint(LOG_DEBUG,"SPI stopped");
+}
+
+static void
+describe_AtSPIScreen (ScreenDescription *description) {
+  pthread_mutex_lock(&updateMutex);
+  description->cols = curNumCols;
+  description->rows = curNumRows?:1;
+  description->posx = curPosX;
+  description->posy = curPosY;
+  pthread_mutex_unlock(&updateMutex);
+  description->no = 0;
+}
+
+static unsigned char *
+read_AtSPIScreen (ScreenBox box, unsigned char *buffer, ScreenMode mode) {
+  long x,y;
+  wchar_t c;
+  if (box.height<0 || box.width<0) return NULL;
+  memset(buffer,' ',box.height*box.width);
+  pthread_mutex_lock(&updateMutex);
+  if (!curTerm) goto out;
+  if (box.top+box.height>curNumRows)
+    box.height = curNumRows-box.top;
+  if (box.top<curNumRows && box.left<curNumCols)
+  for (y=0; y<box.height; y++)
+    for (x=0; x<box.width; x++) {
+      if (box.left+x<curRowLengths[box.top+y] - (curRows[box.top+y][curRowLengths[box.top+y]-1]=='\n')) {
+	if ((c = curRows[box.top+y][box.left+x])<0x100)
+	  /* latin1 only */
+	  buffer[y*box.width+x] = c;
+	else
+	  buffer[y*box.width+x] = '?';
+      }
+    }
+out:
+  pthread_mutex_unlock(&updateMutex);
+  return buffer;
+}
+
+static int
+insert_AtSPIScreen (ScreenKey key) {
+  long keysym;
+  int modMeta = 0, modControl=0;
+  LogPrint(LOG_DEBUG, "Insert key: %4.4X", key);
+  if (key < SCR_KEY_ENTER) {
+    if (key & SCR_KEY_MOD_META) {
+      key &= ~ SCR_KEY_MOD_META;
+      modMeta = 1;
+      SPI_generateKeyboardEvent(XK_Meta_L,NULL,SPI_KEY_SYM);
+    }
+    if (!(key & 0xE0)) {
+      key |= 0x40;
+      SPI_generateKeyboardEvent(XK_Control_L,NULL,SPI_KEY_SYM);
+    }
+  }
+
+  switch (key) {
+    case SCR_KEY_ENTER:         keysym = XK_KP_Enter;  break;
+    case SCR_KEY_TAB:           keysym = XK_Tab;       break;
+    case SCR_KEY_BACKSPACE:     keysym = XK_BackSpace; break;
+    case SCR_KEY_ESCAPE:        keysym = XK_Escape;    break;
+    case SCR_KEY_CURSOR_LEFT:   keysym = XK_Left;      break;
+    case SCR_KEY_CURSOR_RIGHT:  keysym = XK_Right;     break;
+    case SCR_KEY_CURSOR_UP:     keysym = XK_Up;        break;
+    case SCR_KEY_CURSOR_DOWN:   keysym = XK_Down;      break;
+    case SCR_KEY_PAGE_UP:       keysym = XK_Page_Up;   break;
+    case SCR_KEY_PAGE_DOWN:     keysym = XK_Page_Down; break;
+    case SCR_KEY_HOME:          keysym = XK_Home;      break;
+    case SCR_KEY_END:           keysym = XK_End;       break;
+    case SCR_KEY_INSERT:        keysym = XK_Insert;    break;
+    case SCR_KEY_DELETE:        keysym = XK_Delete;    break;
+    case SCR_KEY_FUNCTION + 0:  keysym = XK_F1;        break;
+    case SCR_KEY_FUNCTION + 1:  keysym = XK_F2;        break;
+    case SCR_KEY_FUNCTION + 2:  keysym = XK_F3;        break;
+    case SCR_KEY_FUNCTION + 3:  keysym = XK_F4;        break;
+    case SCR_KEY_FUNCTION + 4:  keysym = XK_F5;        break;
+    case SCR_KEY_FUNCTION + 5:  keysym = XK_F6;        break;
+    case SCR_KEY_FUNCTION + 6:  keysym = XK_F7;        break;
+    case SCR_KEY_FUNCTION + 7:  keysym = XK_F8;        break;
+    case SCR_KEY_FUNCTION + 8:  keysym = XK_F9;        break;
+    case SCR_KEY_FUNCTION + 9:  keysym = XK_F10;       break;
+    case SCR_KEY_FUNCTION + 10: keysym = XK_F11;       break;
+    case SCR_KEY_FUNCTION + 11: keysym = XK_F12;       break;
+    case SCR_KEY_FUNCTION + 12: keysym = XK_F13;       break;
+    case SCR_KEY_FUNCTION + 13: keysym = XK_F14;       break;
+    case SCR_KEY_FUNCTION + 14: keysym = XK_F15;       break;
+    case SCR_KEY_FUNCTION + 15: keysym = XK_F16;       break;
+    case SCR_KEY_FUNCTION + 16: keysym = XK_F17;       break;
+    case SCR_KEY_FUNCTION + 17: keysym = XK_F18;       break;
+    case SCR_KEY_FUNCTION + 18: keysym = XK_F19;       break;
+    case SCR_KEY_FUNCTION + 19: keysym = XK_F20;       break;
+    case SCR_KEY_FUNCTION + 20: keysym = XK_F21;       break;
+    case SCR_KEY_FUNCTION + 21: keysym = XK_F22;       break;
+    case SCR_KEY_FUNCTION + 22: keysym = XK_F23;       break;
+    case SCR_KEY_FUNCTION + 23: keysym = XK_F24;       break;
+    case SCR_KEY_FUNCTION + 24: keysym = XK_F25;       break;
+    case SCR_KEY_FUNCTION + 25: keysym = XK_F26;       break;
+    case SCR_KEY_FUNCTION + 26: keysym = XK_F27;       break;
+    case SCR_KEY_FUNCTION + 27: keysym = XK_F28;       break;
+    case SCR_KEY_FUNCTION + 28: keysym = XK_F29;       break;
+    case SCR_KEY_FUNCTION + 29: keysym = XK_F30;       break;
+    case SCR_KEY_FUNCTION + 30: keysym = XK_F31;       break;
+    case SCR_KEY_FUNCTION + 31: keysym = XK_F32;       break;
+    case SCR_KEY_FUNCTION + 32: keysym = XK_F33;       break;
+    case SCR_KEY_FUNCTION + 33: keysym = XK_F34;       break;
+    case SCR_KEY_FUNCTION + 34: keysym = XK_F35;       break;
+    default: keysym = key; break;
+  }
+
+  SPI_generateKeyboardEvent(keysym,NULL,SPI_KEY_SYM);
+  if (modControl)
+    SPI_generateKeyboardEvent(XK_Control_L,NULL,SPI_KEY_SYM);
+  if (modMeta)
+    SPI_generateKeyboardEvent(XK_Meta_L,NULL,SPI_KEY_SYM);
+  return 0;
+}
+
+static int
+selectvt_AtSPIScreen (int vt) {
+  return 0;
+}
+
+static int
+switchvt_AtSPIScreen (int vt) {
+  return 0;
+}
+
+static int
+currentvt_AtSPIScreen (void) {
+  ScreenDescription description;
+  describe_AtSPIScreen(&description);
+  return description.no;
+}
+
+static int
+execute_AtSPIScreen (int command) {
+  int blk = command & BRL_MSK_BLK;
+  int arg
+#ifdef HAVE_ATTRIBUTE_UNUSED
+      __attribute__((unused))
+#endif /* HAVE_ATTRIBUTE_UNUSED */
+      = command & BRL_MSK_ARG;
+
+  switch (blk) {
+    case BRL_BLK_PASSAT2:
+      break;
+  }
+  return 0;
+}
+
+void
+initializeLiveScreen (MainScreen *main) {
+  initializeRealScreen(main);
+  main->base.describe = describe_AtSPIScreen;
+  main->base.read = read_AtSPIScreen;
+  main->base.insert = insert_AtSPIScreen;
+  main->base.selectvt = selectvt_AtSPIScreen;
+  main->base.switchvt = switchvt_AtSPIScreen;
+  main->base.currentvt = currentvt_AtSPIScreen;
+  main->base.execute = execute_AtSPIScreen;
+  main->parameters = parameters_AtSPIScreen;
+  main->prepare = prepare_AtSPIScreen;
+  main->open = open_AtSPIScreen;
+  main->setup = setup_AtSPIScreen;
+  main->close = close_AtSPIScreen;
+}
