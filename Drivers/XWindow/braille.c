9c015fe5952f30726373fe31e3f44583201d1202
Dave Mielke <dave@mielke.cc>
2005-03-20 17:06:32 +0000

    XWindow braille driver changes: (st)    Add Windows support.    Rename the xtparms parameter to tkparms.
    git-svn-id: svn://mielke.cc/main/brltty@1451 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/XWindow/braille.c
+++ Drivers/XWindow/braille.c
@@ -29,13 +29,9 @@
 
 #include "Programs/misc.h"
 
-#include <X11/Intrinsic.h>
-#include <X11/Xlib.h>
-#include <X11/StringDefs.h>
-#include <X11/Shell.h>
-
 #if defined(HAVE_PKG_XAW)
 #define USE_XAW
+#define USE_XT
 #include <X11/Xaw/Form.h>
 #include <X11/Xaw/Paned.h>
 #include <X11/Xaw/Label.h>
@@ -43,6 +39,7 @@
 #include <X11/Xaw/Repeater.h>
 #elif defined(HAVE_PKG_XAW3D)
 #define USE_XAW
+#define USE_XT
 #include <X11/Xaw3d/Form.h>
 #include <X11/Xaw3d/Paned.h>
 #include <X11/Xaw3d/Label.h>
@@ -50,6 +47,7 @@
 #include <X11/Xaw3d/Repeater.h>
 #elif defined(HAVE_PKG_NEXTAW)
 #define USE_XAW
+#define USE_XT
 #include <X11/neXtaw/Form.h>
 #include <X11/neXtaw/Paned.h>
 #include <X11/neXtaw/Label.h>
@@ -57,6 +55,7 @@
 #include <X11/neXtaw/Repeater.h>
 #elif defined(HAVE_PKG_XAWPLUS)
 #define USE_XAW
+#define USE_XT
 #include <X11/XawPlus/Form.h>
 #include <X11/XawPlus/Paned.h>
 #include <X11/XawPlus/Label.h>
@@ -64,14 +63,28 @@
 #include <X11/XawPlus/Repeater.h>
 #elif defined(HAVE_PKG_XM)
 #define USE_XM
+#define USE_XT
 #include <Xm/Xm.h>
 #include <Xm/Form.h>
 #include <Xm/PanedW.h>
 #include <Xm/Label.h>
 #include <Xm/PushB.h>
+#elif defined(WINDOWS)
+#define USE_WINDOWS
 #else /* HAVE_PKG_ */
-#error Xt toolkit either unspecified or unsupported
+#error GUI toolkit either unspecified or unsupported
 #endif /* HAVE_PKG_ */
+
+#ifdef USE_XT
+#include <X11/Intrinsic.h>
+#include <X11/Xlib.h>
+#include <X11/StringDefs.h>
+#include <X11/Shell.h>
+#endif /* USE_XT */
+
+#ifdef USE_WINDOWS
+#include <commctrl.h>
+#endif /* USE_WINDOWS */
 
 #if defined(USE_XAW)
 #define formWidgetClass     formWidgetClass
@@ -109,17 +122,21 @@
 #define ChainRight          XmATTACH_NONE
 #define NvertDistance       XmNtopOffset
 #define NhorizDistance      XmNleftOffset
+#elif defined(USE_WINDOWS)
+#define Widget HWND
+#define CHRX 16
+#define CHRY 20
 #else /* USE_ */
-#error Xt paradigm either unspecified or unsupported
+#error GUI toolkit paradigm either unspecified or unsupported
 #endif /* USE_ */
 
 typedef enum {
-  PARM_XTPARMS,
+  PARM_TKPARMS,
   PARM_LINES,
   PARM_COLS,
   PARM_MODEL
 } DriverParameter;
-#define BRLPARMS "xtparms", "lines", "cols", "model"
+#define BRLPARMS "tkparms", "lines", "cols", "model"
 
 #define BRL_HAVE_VISUAL_DISPLAY
 #include "Programs/brl_driver.h"
@@ -134,19 +151,28 @@ static char *model;
 #define BUTWIDTH 48
 #define BUTHEIGHT 32
 
-static Widget toplevel,vbox,hbox,keybox,display[WHOLESIZE];
+static Widget toplevel,hbox,display[WHOLESIZE];
+static int lastcursor = -1;
+#ifdef USE_XT
+static Widget vbox,keybox;
 static Pixel displayForeground,displayBackground;
 #ifdef USE_XAW
 static Widget displayb[WHOLESIZE];
 static XFontSet fontset;
-#endif
+#endif /* USE_XAW */
 static XtAppContext app_con;
 #ifdef USE_XM
 static XmString display_cs;
-#endif
+#endif /* USE_XAW */
+#endif /* USE_XT */
+
+#ifdef USE_WINDOWS
+static int modelWidth,modelHeight;
+#endif /* USE_WINDOWS */
 
 static long keypressed;
 
+#ifdef USE_XT
 static void KeyPressCB(Widget w, XtPointer closure, XtPointer callData) {
  LogPrint(LOG_DEBUG,"keypress(%p)",closure);
  keypressed=(long)closure;
@@ -157,17 +183,33 @@ static void route(Widget w, XEvent *event, String *params, Cardinal *num_params)
  LogPrint(LOG_DEBUG,"route(%u)", index);
  keypressed = BRL_BLK_ROUTE | (index&BRL_MSK_ARG);
 }
+#endif /* USE_XT */
 
-static inline Widget crKeyBut(String name, long keycode, int repeat,
+#ifdef USE_WINDOWS
+static LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+ if (uMsg == WM_COMMAND) {
+  hwnd = (HWND) lParam;
+  keypressed = GetWindowLong(hwnd, GWL_USERDATA);
+  LogPrint(LOG_DEBUG,"keypress(%lx)", keypressed);
+  if ((keypressed & BRL_MSK_BLK) == BRL_BLK_ROUTE)
+   lastcursor = -1;
+  return 0;
+ }
+ return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+#endif /* USE_WINDOWS */
+
+static inline Widget crKeyBut(char *name, long keycode, int repeat,
     int horizDistance, int vertDistance)
 {
   Widget button;
+#if defined(USE_XT)
   button = XtVaCreateManagedWidget(name,
     repeat?repeaterWidgetClass:commandWidgetClass, keybox,
     XtNwidth, BUTWIDTH, XtNheight, BUTHEIGHT,
 #ifdef USE_XAW
     XtNinitialDelay, 500, XtNminimumDelay, 100,
-#endif
+#endif /* USE_XAW */
     NhorizDistance, horizDistance,
     NvertDistance, vertDistance,
     Ntop, ChainTop,
@@ -176,14 +218,26 @@ static inline Widget crKeyBut(String name, long keycode, int repeat,
     Nright, ChainRight,
     NULL);
   XtAddCallback(button, Ncallback, KeyPressCB, (XtPointer) keycode);
+#elif defined(USE_WINDOWS)
+  button = CreateWindow(WC_BUTTON, name, WS_CHILD | WS_VISIBLE, horizDistance, lines*CHRY+1+vertDistance, BUTWIDTH, BUTHEIGHT, toplevel, NULL, NULL, NULL);
+  SetWindowLong(button, GWL_USERDATA, (long) keycode);
+#else /* USE_ */
+#error Toolkit button creation unspecified
+#endif /* USE_ */
   return button;
 }
 
-struct button{
- String label;
+struct button {
+ char *label;
  long keycode;
  int repeat;
  int x,y;
+};
+
+struct model {
+ char *name;
+ struct button *buttons;
+ int width,height;
 };
 
 static struct button buttons_simple[] = {
@@ -238,6 +292,12 @@ static struct button buttons_vs[] = {
  { NULL,     0,              0, 0, 0},
 };
 
+static struct model models[] = {
+ { "normal",	buttons_simple,	4, 4 },
+ { "vs",	buttons_vs,	9, 5 },
+ { NULL,	NULL,		0, 0 },
+};
+
 static void createKeyButtons(struct button *buttons) {
  struct button *b;
  for (b=buttons; b->label; b++)
@@ -247,8 +307,24 @@ static void createKeyButtons(struct button *buttons) {
 static int brl_readCommand(BrailleDisplay *brl, BRL_DriverCommandContext context)
 {
  int res=EOF;
+#if defined(USE_XT)
  while (XtAppPending(app_con)) {
   XtAppProcessEvent(app_con,XtIMAll);
+#elif defined(USE_WINDOWS)
+ MSG msg;
+
+ while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+  if (msg.message == WM_QUIT)
+   return BRL_CMD_RESTARTBRL;
+  else {
+   if (msg.message == WM_COMMAND)
+    fprintf(stderr,"message %p %d\n",msg.hwnd,msg.message);
+   TranslateMessage(&msg);
+   DispatchMessage(&msg);
+  }
+#else /* USE_ */
+#error Toolkit loop unspecified
+#endif /* USE_ */
   if (keypressed) {
    res=keypressed;
    keypressed=BRL_CMD_NOOP;
@@ -258,6 +334,7 @@ static int brl_readCommand(BrailleDisplay *brl, BRL_DriverCommandContext context
  return res;
 }
 
+#ifdef USE_XT
 static XrmOptionDescRec optionDescList[] = { };
 
 static char *fallback_resources[] = {
@@ -265,10 +342,11 @@ static char *fallback_resources[] = {
 #ifdef USE_XAW
  "*displayb.background: black",
  "*displayb.foreground: white",
-#endif
+#endif /* USE_XAW */
  "*keybox.background: lightgrey",
  NULL
 };
+#endif /* USE_XT */
 
 static void brl_identify()
 {
@@ -277,10 +355,16 @@ static void brl_identify()
 }
 
 static void brl_display() {
- char *disp,*dispb;
- int y,x;
+#ifdef USE_XT
  Widget tmp_vbox;
+ char *disp;
+#ifdef USE_XAW
+ char *dispb;
+#endif /* USE_XAW */
+#endif /* USE_XT */
+ int y,x;
 
+#ifdef USE_XT
  /* horizontal separation */
  hbox = XtVaCreateManagedWidget("hbox",panedWidgetClass,vbox,
    XtNorientation, XtEhorizontal,
@@ -288,14 +372,14 @@ static void brl_display() {
    XmNmarginHeight, 0,
    XmNmarginWidth, 0,
    XmNspacing, 0,
-#endif
+#endif /* USE_XM */
 #ifdef USE_XAW
    XtNshowGrip,False,
-#else
+#else /* USE_XAW */
    XmNpaneMaximum,20,
    XmNpaneMinimum,20,
    XmNskipAdjust, True,
-#endif
+#endif /* USE_XAW */
    XtNresize, True,
    NULL);
 
@@ -304,6 +388,7 @@ static void brl_display() {
  disp[0]=' ';
  disp[1]=0;
 
+#ifdef USE_XAW
  dispb=XtMalloc(4);
  dispb[0]=0xe0|((0x28>>4)&0x0f);
  dispb[1]=0x80|((0x28<<2)&0x3f);
@@ -313,30 +398,40 @@ static void brl_display() {
  dispb[1]=0;
  */
  dispb[3]=0;
+#endif /* USE_XAW */
 
 #ifdef USE_XM
  display_cs = XmStringCreateLocalized(disp);
-#endif
+#endif /* USE_XM */
+#endif /* USE_XT */
+
+#ifdef USE_WINDOWS
+ hbox = CreateWindow(WC_STATIC, "", WS_CHILD | WS_VISIBLE, 0, 0,
+      modelWidth, lines*CHRY+modelHeight, toplevel, NULL, NULL, NULL);
+#endif /* USE_WINDOWS */
 
  for (x=0;x<cols;x++) {
+#ifdef USE_XT
   /* vertical separation */
   tmp_vbox = XtVaCreateManagedWidget("tmp_vbox",panedWidgetClass,hbox,
 #ifdef USE_XAW
    XtNshowGrip,False,
-#else
+#else /* USE_XAW */
    XmNpaneMaximum,20,
    XmNpaneMinimum,20,
    XmNskipAdjust, True,
-#endif
+#endif /* USE_XAW */
 #ifdef USE_XM
    XmNmarginHeight, 0,
    XmNmarginWidth, 0,
    XmNspacing, 0,
-#endif
+#endif /* USE_XM */
    XtNresize, True,
    NULL);
+#endif /* USE_XT */
 
   for (y=0;y<lines;y++) {
+#if defined(USE_XT)
    char action[] = "<Btn1Up>: route(100)";
    XtTranslations transl;
 
@@ -347,16 +442,16 @@ static void brl_display() {
     XtNtranslations, transl,
 #ifdef USE_XAW
     XtNshowGrip,False,
-#else
+#else /* USE_XAW */
     XmNpaneMaximum,20,
     XmNpaneMinimum,20,
     XmNskipAdjust, True,
-#endif
+#endif /* USE_XAW */
 #ifdef USE_XAW
     XtNlabel, disp,
-#else
+#else /* USE_XAW */
     XmNlabelString, display_cs,
-#endif
+#endif /* USE_XAW */
     NULL);
 
 #ifdef USE_XAW
@@ -369,14 +464,21 @@ static void brl_display() {
     XtNlabel, dispb,
     NULL);
    }
-#endif
+#endif /* USE_XAW */
+#elif defined(USE_WINDOWS)
+   display[y*cols+x] = CreateWindow(WC_BUTTON, " ", WS_CHILD | WS_VISIBLE | BS_CHECKBOX | BS_PUSHLIKE, x*CHRX, y*CHRY, CHRX, CHRY, toplevel, NULL, NULL, NULL);
+   SetWindowLong(display[y*cols+x], GWL_USERDATA, (long) (BRL_BLK_ROUTE | ((y*cols+x)&BRL_MSK_ARG)));
+#else /* USE_ */
+#error Toolkit display unspecified
+#endif /* USE_ */
   }
  }
+#ifdef USE_XT
 #ifdef USE_XM
  XmStringFree(display_cs);
-#endif
+#endif /* USE_XM */
  XtFree(disp);
-
+#endif /* USE_XT */
 }
 
 static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
@@ -384,12 +486,15 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
  int argc = 1;
  static char *defargv[]= { "brltty", NULL };
  char **argv = defargv;
+#ifdef USE_XT
 #ifdef USE_XAW
  char *def_string_return;
  char **missing_charset_list_return;
  int missing_charset_count_return;
-#endif
+#endif /* USE_XAW */
  XtActionsRec routeAction = { "route", route };
+#endif /* USE_XT */
+ struct model *keyModel = NULL;
 
  lines=1;
  if (*parameters[PARM_LINES]) {
@@ -412,17 +517,21 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
  brl->x=cols;
  brl->y=lines;
 
- if (*parameters[PARM_XTPARMS]) {
-  argv = splitString(parameters[PARM_XTPARMS],' ',&argc);
+ if (*parameters[PARM_TKPARMS]) {
+  argv = splitString(parameters[PARM_TKPARMS],' ',&argc);
   argv = reallocWrapper(argv, (argc+2) * sizeof(char *));
   memmove(argv+1,argv,(argc+1) * sizeof(char *));
   argv[0] = strdupWrapper(defargv[0]);
   argc++;
  }
 
- if (*parameters[PARM_MODEL])
+ if (*parameters[PARM_MODEL]) {
   model = parameters[PARM_MODEL];
+  for (keyModel = models; keyModel->name && strcmp(keyModel->name,model); keyModel++);
+  if (!keyModel->name) keyModel = NULL;
+ }
  
+#if defined(USE_XT)
  XtSetLanguageProc(NULL, NULL, NULL);
  
  /* toplevel */
@@ -432,41 +541,85 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
    sessionShellWidgetClass,
    XtNallowShellResize, True,
    NULL);
+#elif defined(USE_WINDOWS)
+ {
+  WNDCLASS wndclass = {
+   .style = 0,
+   .lpfnWndProc = wndProc,
+   .cbClsExtra = 0,
+   .cbWndExtra = 0,
+   .hInstance = NULL,
+   .hIcon = LoadIcon(NULL, IDI_APPLICATION), /* TODO: nice icon */
+   .hCursor = LoadCursor(NULL, IDC_ARROW),
+   .hbrBackground = NULL,
+   .lpszMenuName = NULL,
+   .lpszClassName = "BRLTTYWClass",
+  };
+  if (!(RegisterClass(&wndclass))) {
+   LogWindowsError("RegisterClass");
+   exit(1);
+  }
+  modelWidth = cols*CHRX;
+  if (keyModel) {
+   if (keyModel->width*(BUTWIDTH+1)+1 > modelWidth)
+    modelWidth = keyModel->width *(BUTWIDTH +1)-1;
+   modelHeight = keyModel->height*(BUTHEIGHT+1);
+  } else {
+   modelHeight = 0;
+  }
+  if (!(toplevel = CreateWindow("BRLTTYWClass", "BRLTTY", WS_POPUP,
+	  GetSystemMetrics(SM_CXSCREEN)-modelWidth-60, 0,
+          modelWidth, lines*CHRY+modelHeight, NULL, NULL, NULL, NULL))) {
+   LogWindowsError("CreateWindow");
+   exit(1);
+  }
+ }
+#else /* USE_ */
+#error Toolkit toplevel creation unspecified
+#endif /* USE_ */
 
  if (argv != defargv)
   deallocateStrings(argv);
 
+#ifdef USE_XT
  XtAppAddActions(app_con,&routeAction,1);
+#endif /* USE_XT */
 
  /* vertical separation */
+#ifdef USE_XT
  vbox = XtVaCreateManagedWidget("vbox",panedWidgetClass,toplevel,
 #ifdef USE_XM
    XmNmarginHeight, 0,
    XmNmarginWidth, 0,
    XmNspacing, 1,
-#endif
+#endif /* USE_XM */
    XtNresize, True,
    NULL);
+#endif /* USE_XT */
 
 #ifdef USE_XAW
  if (!(fontset = XCreateFontSet(XtDisplay(toplevel),"-*-clearlyu-*-*-*-*-17-*-*-*-*-*-iso10646-1", &missing_charset_list_return, &missing_charset_count_return, &def_string_return)))
   LogPrint(LOG_ERR,"Error while loading braille font");
-#endif
+#endif /* USE_XAW */
  
  brl_display();
 
+#ifdef USE_XT
  XtVaGetValues(display[0],
    XtNforeground, &displayForeground,
    XtNbackground, &displayBackground,
    NULL);
+#endif /* USE_XT */
 
  if (!model || strcmp(model,"bare")) {
    /* key box */
+#ifdef USE_XT
    keybox = XtVaCreateManagedWidget("keybox",formWidgetClass,vbox,
 #ifdef USE_XAW
      XtNdefaultDistance,0,
-#endif
+#endif /* USE_XAW */
      NULL);
+#endif /* USE_XT */
    if (model && !strcmp(model,"vs"))
     createKeyButtons(buttons_vs);
    else
@@ -474,18 +627,32 @@ static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
  }
 
  /* go go go */
+#if defined(USE_XT)
  XtRealizeWidget(toplevel);
+#elif defined(USE_WINDOWS)
+ ShowWindow(toplevel, SW_SHOWDEFAULT);
+ UpdateWindow(toplevel);
+#else /* USE_ */
+#error Toolkit toplevel realization unspecified
+#endif /* USE_ */
 
  return 1;
 }
 
 static void brl_close(BrailleDisplay *brl)
 {
+#if defined(USE_XT)
 #ifdef USE_XAW
  if (fontset)
   XFreeFontSet(XtDisplay(toplevel),fontset);
-#endif
+#endif /* USE_XAW */
  XtDestroyApplicationContext(app_con);
+#elif defined(USE_WINDOWS)
+ if (!DestroyWindow(toplevel))
+  LogWindowsError("DestroyWindow");
+#else /* USE_ */
+#error Toolkit toplevel destruction unspecified
+#endif /* USE_ */
 }
 
 static void brl_writeWindow(BrailleDisplay *brl)
@@ -519,29 +686,40 @@ static void brl_writeWindow(BrailleDisplay *brl)
    XtNlabel, data,
    NULL);
  }
-#endif
+#endif /* USE_XAW */
 }
 
 static void brl_writeVisual(BrailleDisplay *brl)
 {
  static unsigned char displayed[WHOLESIZE];
- static int lastcursor = -1;
  int i;
  unsigned char data[2];
 
  if (lastcursor != brl->cursor) {
   if (lastcursor>=0) {
+#if defined(USE_XT)
    XtVaSetValues(display[lastcursor],
     XtNforeground, displayForeground,
     XtNbackground, displayBackground,
     NULL);
+#elif defined(USE_WINDOWS)
+   SendMessage(display[lastcursor],BM_SETSTATE,FALSE,0);
+#else /* USE_ */
+#error Toolkit cursor not specified
+#endif /* USE_ */
   }
   lastcursor = brl->cursor;
   if (lastcursor>=0) {
+#if defined(USE_XT)
    XtVaSetValues(display[lastcursor],
     XtNforeground, displayBackground,
     XtNbackground, displayForeground,
     NULL);
+#elif defined(USE_WINDOWS)
+   SendMessage(display[lastcursor],BM_SETSTATE,TRUE,0);
+#else /* USE_ */
+#error Toolkit cursor not specified
+#endif /* USE_ */
   }
  }
 
@@ -553,19 +731,25 @@ static void brl_writeVisual(BrailleDisplay *brl)
   if (data[0]==0) data[0]=' ';
   data[1]=0;
 
+#if defined(USE_XT)
 #ifdef USE_XM
   display_cs = XmStringCreateLocalized(data);
-#endif
+#endif /* USE_XM */
   XtVaSetValues(display[i],
 #ifdef USE_XAW
    XtNlabel, data,
-#else
+#else /* USE_XAW */
    XmNlabelString, display_cs,
-#endif
+#endif /* USE_XAW */
    NULL);
 #ifdef USE_XM
   XmStringFree(display_cs);
-#endif
+#endif /* USE_XM */
+#elif defined(USE_WINDOWS)
+  SetWindowText(display[i],data);
+#else /* USE_ */
+#error Toolkit display refresh unspecified
+#endif /* USE_ */
  }
 }
 
