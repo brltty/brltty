ac72eaa4715272af1c9933fe888bddb0e7856846
Dave Mielke <dave@mielke.cc>
2005-04-12 06:09:19 +0000

    Add skeleton braille and speech driver source. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@1471 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Drivers/braille.c
@@ -0,0 +1,106 @@
+/*
+ * BRLTTY - A background process providing access to the console screen (when in
+ *          text mode) for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2005 by The BRLTTY Team. All rights reserved.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "Programs/misc.h"
+
+//#define BRL_HAVE_VISUAL_DISPLAY
+//#define BRL_HAVE_PACKET_IO
+//#define BRL_HAVE_KEY_CODES
+//#define BRL_HAVE_FIRMNESS
+#include "Programs/brl_driver.h"
+
+static TranslationTable outputTable;
+
+static void
+brl_identify (void) {
+}
+
+static int
+brl_open (BrailleDisplay *brl, char **parameters, const char *device) {
+  {
+    static const DotsTable dots = {0X01, 0X02, 0X04, 0X08, 0X10, 0X20, 0X40, 0X80};
+    makeOutputTable(&dots, &outputTable);
+  }
+  
+  return 0;
+}
+
+static void
+brl_close (BrailleDisplay *brl) {
+}
+
+#ifdef BRL_HAVE_PACKET_IO
+static ssize_t
+brl_readPacket (BrailleDisplay *brl, unsigned char *buffer, size_t size) {
+  return -1;
+}
+
+static ssize_t
+brl_writePacket (BrailleDisplay *brl, const unsigned char *packet, size_t length) {
+  return -1;
+}
+
+static int
+brl_reset (BrailleDisplay *brl) {
+  return 0;
+}
+#endif /* BRL_HAVE_PACKET_IO */
+
+#ifdef BRL_HAVE_KEY_CODES
+static int
+brl_readKey (BrailleDisplay *brl) {
+  return EOF;
+}
+
+static int
+brl_keyToCommand (BrailleDisplay *brl, BRL_DriverCommandContext context, int key) {
+  return EOF;
+}
+#endif /* BRL_HAVE_KEY_CODES */
+
+static void
+brl_writeWindow (BrailleDisplay *brl) {
+}
+
+static void
+brl_writeStatus (BrailleDisplay *brl, const unsigned char *status) {
+}
+
+#ifdef BRL_HAVE_VISUAL_DISPLAY
+static void
+brl_writeVisual (BrailleDisplay *brl) {
+}
+#endif /* BRL_HAVE_VISUAL_DISPLAY */
+
+static int
+brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
+  return EOF;
+}
+
+#ifdef BRL_HAVE_FIRMNESS
+static void
+brl_firmness (BrailleDisplay *brl, BrailleFirmness setting) {
+}
+#endif /* BRL_HAVE_FIRMNESS */
---
+++ Drivers/speech.c
@@ -0,0 +1,87 @@
+/*
+ * BRLTTY - A background process providing access to the console screen (when in
+ *          text mode) for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2005 by The BRLTTY Team. All rights reserved.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "Programs/misc.h"
+
+//#define SPK_HAVE_EXPRESS
+//#define SPK_HAVE_TRACK
+//#define SPK_HAVE_RATE
+//#define SPK_HAVE_VOLUME
+#include "Programs/spk_driver.h"
+
+static void
+spk_identify (void) {
+}
+
+static int
+spk_open (char **parameters) {
+  return 0;
+}
+
+static void
+spk_close (void) {
+}
+
+static void
+spk_say (const unsigned char *buffer, int length) {
+}
+
+static void
+spk_mute (void) {
+}
+
+#ifdef SPK_HAVE_EXPRESS
+static void
+spk_express (const unsigned char *buffer, int length) {
+}
+#endif /* SPK_HAVE_EXPRESS */
+
+#ifdef SPK_HAVE_TRACK
+static void
+spk_doTrack (void) {
+}
+
+static int
+spk_getTrack (void) {
+  return 0;
+}
+
+static int
+spk_isSpeaking (void) {
+  return 0;
+}
+#endif /* SPK_HAVE_TRACK */
+
+#ifdef SPK_HAVE_RATE
+static void
+spk_rate (float setting) {
+}
+#endif /* SPK_HAVE_RATE */
+
+#ifdef SPK_HAVE_VOLUME
+static void
+spk_volume (float setting) {
+}
+#endif /* SPK_HAVE_VOLUME */
