31f4cdb71a30d7a6fd156c40a4255f9280720775
Dave Mielke <dave@mielke.cc>
2005-05-24 06:24:46 +0000

    Add the make file targets tar,gz,bz2,rpm. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@1565 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ begin.sh
@@ -0,0 +1,46 @@
+###############################################################################
+# BRLTTY - A background process providing access to the console screen (when in
+#          text mode) for a blind person using a refreshable braille display.
+#
+# Copyright (C) 1995-2005 by The BRLTTY Team. All rights reserved.
+#
+# BRLTTY comes with ABSOLUTELY NO WARRANTY.
+#
+# This is free software, placed under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation.  Please see the file COPYING for details.
+#
+# Web Page: http://mielke.cc/brltty/
+#
+# This software is maintained by Dave Mielke <dave@mielke.cc>.
+###############################################################################
+
+initialDirectory="`pwd`"
+programName="`basename "${0}"`"
+
+programMessage() {
+   echo >&2 "${programName}: ${1}"
+}
+programError() {
+   [ -n "${2}" ] && programMessage "${2}"
+   exit "${1:-3}"
+}
+syntaxError() {
+   programError 2 "${1}"
+}
+
+needTemporaryDirectory() {
+   cleanup() {
+      set +e
+      cd /
+      [ -n "${temporaryDirectory}" ] && rm -f -r -- "${temporaryDirectory}"
+   }
+   trap "cleanup" 0
+
+   umask 022
+   [ -n "${TMPDIR}" -a -d "${TMPDIR}" -a -r "${TMPDIR}" -a -w "${TMPDIR}" -a -x "${TMPDIR}" ] || {
+      TMPDIR="/tmp"
+      export TMPDIR
+   }
+   temporaryDirectory="`mktemp -d "${TMPDIR}/${programName}.XXXXXX"`" && cd "${temporaryDirectory}" || exit 3
+}
