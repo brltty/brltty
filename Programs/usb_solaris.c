78a785f10a80571f5a4818c7fdcfd7678eb65e08
Dave Mielke <dave@mielke.cc>
2005-08-18 03:48:13 +0000

    Add the infrastructure for Solaris USB support. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@1827 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Programs/usb_solaris.c
@@ -0,0 +1,201 @@
+/*
+ * BRLTTY - A background process providing access to the console screen (when in
+ *          text mode) for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2005 by The BRLTTY Team. All rights reserved.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#include "prologue.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/usb/clients/ugen/usb_ugen.h>
+
+#include "misc.h"
+#include "usb.h"
+#include "usb_internal.h"
+
+int
+usbResetDevice (UsbDevice *device) {
+  errno = ENOSYS;
+  LogError("USB device reset");
+  return 0;
+}
+
+int
+usbSetConfiguration (
+  UsbDevice *device,
+  unsigned char configuration
+) {
+  errno = ENOSYS;
+  LogError("USB configuration set");
+  return 0;
+}
+
+int
+usbClaimInterface (
+  UsbDevice *device,
+  unsigned char interface
+) {
+  errno = ENOSYS;
+  LogError("USB interface claim");
+  return 0;
+}
+
+int
+usbReleaseInterface (
+  UsbDevice *device,
+  unsigned char interface
+) {
+  errno = ENOSYS;
+  LogError("USB interface release");
+  return 0;
+}
+
+int
+usbSetAlternative (
+  UsbDevice *device,
+  unsigned char interface,
+  unsigned char alternative
+) {
+  errno = ENOSYS;
+  LogError("USB alternative set");
+  return 0;
+}
+
+int
+usbResetEndpoint (
+  UsbDevice *device,
+  unsigned char endpointAddress
+) {
+  errno = ENOSYS;
+  LogError("USB endpoint reset");
+  return 0;
+}
+
+int
+usbClearEndpoint (
+  UsbDevice *device,
+  unsigned char endpointAddress
+) {
+  errno = ENOSYS;
+  LogError("USB endpoint clear");
+  return 0;
+}
+
+int
+usbControlTransfer (
+  UsbDevice *device,
+  unsigned char direction,
+  unsigned char recipient,
+  unsigned char type,
+  unsigned char request,
+  unsigned short value,
+  unsigned short index,
+  void *buffer,
+  int length,
+  int timeout
+) {
+  errno = ENOSYS;
+  LogError("USB control transfer");
+  return -1;
+}
+
+void *
+usbSubmitRequest (
+  UsbDevice *device,
+  unsigned char endpointAddress,
+  void *buffer,
+  int length,
+  void *data
+) {
+  errno = ENOSYS;
+  LogError("USB request submit");
+  return NULL;
+}
+
+int
+usbCancelRequest (
+  UsbDevice *device,
+  void *request
+) {
+  errno = ENOSYS;
+  LogError("USB request cancel");
+  return 0;
+}
+
+void *
+usbReapResponse (
+  UsbDevice *device,
+  unsigned char endpointAddress,
+  UsbResponse *response,
+  int wait
+) {
+  errno = ENOSYS;
+  LogError("USB request reap");
+  return NULL;
+}
+
+int
+usbReadEndpoint (
+  UsbDevice *device,
+  unsigned char endpointNumber,
+  void *buffer,
+  int length,
+  int timeout
+) {
+  errno = ENOSYS;
+  LogError("USB endpoint read");
+  return -1;
+}
+
+int
+usbWriteEndpoint (
+  UsbDevice *device,
+  unsigned char endpointNumber,
+  const void *buffer,
+  int length,
+  int timeout
+) {
+  errno = ENOSYS;
+  LogError("USB endpoint write");
+  return -1;
+}
+
+int
+usbReadDeviceDescriptor (UsbDevice *device) {
+  errno = ENOSYS;
+  LogError("USB device descriptor read");
+  return 0;
+}
+
+int
+usbAllocateEndpointExtension (UsbEndpoint *endpoint) {
+  return 1;
+}
+
+void
+usbDeallocateEndpointExtension (UsbEndpoint *endpoint) {
+}
+
+void
+usbDeallocateDeviceExtension (UsbDevice *device) {
+}
+
+UsbDevice *
+usbFindDevice (UsbDeviceChooser chooser, void *data) {
+  UsbDevice *device = NULL;
+  return device;
+}
