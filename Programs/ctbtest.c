7af4d0bd3bffee60c6d6bf9261eabb6c42d860ab
Dave Mielke <dave@mielke.cc>
2005-09-26 19:26:27 +0000

    ctbtest changes: (dm)    Specify the contraction table by -c rather than via the first parameter.    Default the contraction table to "en-us-g2.ctb".    Add the -w [--output-width=] option.    Interpret the parameters as input file names (- means standard input).
    git-svn-id: svn://mielke.cc/main/brltty@1900 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/ctbtest.c
+++ Programs/ctbtest.c
@@ -25,6 +25,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #include "options.h"
 #include "misc.h"
@@ -32,13 +33,23 @@
 #include "ctb.h"
 #include "tbl.h"
 
-static char *opt_textTable;
 static char *opt_dataDirectory;
+static char *opt_contractionTable;
+static char *opt_textTable;
+static char *opt_outputWidth;
 
 BEGIN_OPTION_TABLE
+  {"contraction-table", "file", 'c', 0, 0,
+   &opt_contractionTable, "en-us-g2.ctb",
+   "Path to contraction table file."},
+
   {"text-table", "file", 't', 0, 0,
    &opt_textTable, TEXT_TABLE,
    "Text translation table."},
+
+  {"output-width", "columns", 'w', 0, 0,
+   &opt_outputWidth, "",
+   "Maximum length of an output line."},
 
   {"data-directory", "file", 'D', 0, OPT_Hidden,
    &opt_dataDirectory, DATA_DIRECTORY,
@@ -49,80 +60,141 @@ TranslationTable textTable;
 TranslationTable untextTable;
 void *contractionTable;
 
+static int outputWidth;
+static int outputExtend;
+static unsigned char *outputBuffer;
+
 typedef struct {
   unsigned char status;
 } LineProcessingData;
+
 static int
-processLine (char *line, void *data) {
+contractLine (char *line, void *data) {
   LineProcessingData *lpd = data;
-  unsigned char buffer[0X100];
-  int bufferLength = sizeof(buffer);
   int lineLength = strlen(line);
-  int offsets[0X100];
-  if (contractText(contractionTable, (unsigned char *)line, &lineLength, buffer, &bufferLength, offsets, -1)) {
-    int index;
-    for (index=0; index<bufferLength; ++index)
-      buffer[index] = untextTable[buffer[index]];
-    fwrite(buffer, 1, bufferLength, stdout);
-    if (!ferror(stdout)) {
-      fputc('\n', stdout);
-      if (!ferror(stdout)) {
-        return 1;
-      } else {
-        lpd->status = 6;
+  int offsets[lineLength];
+
+  while (lineLength) {
+    int inputCount = lineLength;
+    int outputCount = outputWidth;
+
+    if (!outputBuffer) {
+      if (!(outputBuffer = malloc(outputWidth))) {
+        LogError("output buffer allocation");
+        lpd->status = 10;
+        return 0;
       }
-    } else {
-      lpd->status = 6;
     }
-  } else {
-    lpd->status = 7;
+
+    if (!contractText(contractionTable,
+                      (unsigned char *)line, &inputCount,
+                      outputBuffer, &outputCount,
+                      offsets, -1)) {
+      lpd->status = 11;
+      return 0;
+    }
+
+    if ((inputCount < lineLength) && outputExtend) {
+      free(outputBuffer);
+      outputBuffer = NULL;
+      outputWidth <<= 1;
+    } else {
+      {
+        int index;
+        for (index=0; index<outputCount; ++index)
+          outputBuffer[index] = untextTable[outputBuffer[index]];
+      }
+
+      {
+        FILE *output = stdout;
+        fwrite(outputBuffer, 1, outputCount, output);
+        if (!ferror(output)) fputc('\n', output);
+        if (ferror(output)) {
+          LogError("output");
+          lpd->status = 12;
+          return 0;
+        }
+      }
+
+      line += inputCount;
+      lineLength -= inputCount;
+    }
   }
-  return 0;
+
+  return 1;
+}
+
+static int
+contractFile (FILE *file) {
+  LineProcessingData lpd;
+  lpd.status = 0;
+  return processLines(file, contractLine, &lpd)? lpd.status: 5;
 }
 
 int
 main (int argc, char *argv[]) {
   int status = 3;
-  char *contractionTablePath;
 
   processOptions(optionTable, optionCount,
                  "ctbtest", &argc, &argv,
                  NULL, NULL, NULL,
                  "contraction-table");
 
-  if (argc == 0) {
-    LogPrint(LOG_ERR, "missing contraction table.");
-    exit(2);
+  if ((outputExtend = !*opt_outputWidth)) {
+    outputWidth = 0X80;
+  } else {
+    static const int minimum = 1;
+    if (!validateInteger(&outputWidth, "output width", opt_outputWidth, &minimum, NULL)) exit(2);
   }
-  contractionTablePath = *argv++, argc--;
+  outputBuffer = NULL;
 
-  if ((contractionTablePath = makePath(opt_dataDirectory, contractionTablePath))) {
-    if ((contractionTable = compileContractionTable(contractionTablePath))) {
-      char *textTablePath;
-      if ((textTablePath = makePath(opt_dataDirectory, opt_textTable))) {
-        if (loadTranslationTable(textTablePath, NULL, textTable, 0)) {
-          LineProcessingData lpd;
-          reverseTranslationTable(textTable, untextTable);
-          lpd.status = 0;
-          if (processLines(stdin, processLine, &lpd)) {
-            status = lpd.status;
+  {
+    char *contractionTablePath;
+
+    if ((contractionTablePath = makePath(opt_dataDirectory, opt_contractionTable))) {
+      if ((contractionTable = compileContractionTable(contractionTablePath))) {
+        char *textTablePath;
+
+        if ((textTablePath = makePath(opt_dataDirectory, opt_textTable))) {
+          if (loadTranslationTable(textTablePath, NULL, textTable, 0)) {
+            reverseTranslationTable(textTable, untextTable);
+
+            if (argc) {
+              do {
+                char *path = *argv;
+                if (strcmp(path, "-") == 0) {
+                  status = contractFile(stdin);
+                } else {
+                  FILE *file = fopen(path, "r");
+                  if (file) {
+                    status = contractFile(file);
+                  } else{
+                    LogPrint(LOG_ERR, "cannot open input file: %s: %s",
+                             path, strerror(errno));
+                    status = 6;
+                  }
+                }
+              } while ((status == 0) && (++argv, --argc));
+            } else {
+              status = contractFile(stdin);
+            }
           } else {
-            status = 5;
+            status = 4;
           }
+          free(textTablePath);
         } else {
           status = 4;
         }
-        free(textTablePath);
+        destroyContractionTable(contractionTable);
       } else {
-        status = 4;
+        status = 3;
       }
-      destroyContractionTable(contractionTable);
+      free(contractionTablePath);
     } else {
       status = 3;
     }
-    free(contractionTablePath);
-  } else {
-    status = 3;
   }
+
+  if (outputBuffer) free(outputBuffer);
   return status;
 }
