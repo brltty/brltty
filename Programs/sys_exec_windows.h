3145c18ae8bb461de1e7b96494acbafbfa5bd4ec
Dave Mielke <dave@mielke.cc>
2005-11-10 07:24:22 +0000

    Implement the command line builder for the Windows implementation of executeHostCommand(). (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2020 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/sys_exec_windows.h
+++ Programs/sys_exec_windows.h
@@ -15,9 +15,72 @@
  * This software is maintained by Dave Mielke <dave@mielke.cc>.
  */
 
-#warning host command execution not available on this platform
+static int
+addHostCommandLineCharacter (char **buffer, int *size, int *length, char character) {
+  if (*length == *size) {
+    char *newBuffer = realloc(*buffer, (*size = *size? *size<<1: 0X1));
+    if (!newBuffer) {
+      return 0;
+    }
+    *buffer = newBuffer;
+  }
+
+  (*buffer)[(*length)++] = character;
+  return 1;
+}
 
 int
 executeHostCommand (const char *const *arguments) {
-  return 1;
+  const char backslash = '\\';
+  const char quote = '"';
+  int result = 1;
+  const char *command = *arguments;
+  char *line = NULL;
+  int size = 0;
+  int length = 0;
+
+#define ADD(c) if (!addHostCommandLineCharacter(&line, &size, &length, (c))) goto error
+  while (*arguments) {
+    const char *character = *arguments;
+    int backslashCount = 0;
+    int needQuotes = 0;
+    int start = length;
+
+    while (*character) {
+      if (*character == backslash) {
+        ++backslashCount;
+      } else {
+        if (*character == quote) {
+          backslashCount = (backslashCount * 2) + 1;
+        } else if ((*character == ' ') || (*character == '\t')) {
+          needQuotes = 1;
+        }
+
+        while (backslashCount > 0) {
+          ADD(backslash);
+          --backslashCount;
+        }
+
+        ADD(*character);
+      }
+
+      ++character;
+    }
+
+    if (needQuotes) {
+      ADD(quote);
+      ADD(quote);
+      memmove(&line[start+1], &line[start], length-start-1);
+      line[start] = quote;
+    }
+
+    ADD(' ');
+    ++arguments;
+  }
+  line[length-1] = 0;
+#undef ADD
+
+error:
+  if (line) free(line);
+  return result;
 }
