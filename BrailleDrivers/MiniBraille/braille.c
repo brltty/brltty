485944f7274500a102d739401aaa60997d0d7973
Dave Mielke <dave@mielke.cc>
2006-09-26 20:11:04 +0000

    MiniBraille braille driver enhancements: (dm)    Add flow control.    Add a delay after each command written to the display.    Add error checking.    Fix the way messages are written to the display.    Use a more international format for the clock value.
    git-svn-id: svn://mielke.cc/main/brltty@2447 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- BrailleDrivers/MiniBraille/braille.c
+++ BrailleDrivers/MiniBraille/braille.c
@@ -37,209 +37,267 @@
 
 #define BRLSTAT ST_TiemanStyle
 #include "Programs/brl_driver.h"
-#include "braille.h"
-#include "minibraille.h"
+
 #include "Programs/io_serial.h"
-
-/* types */
-enum mode_t { NORMAL_MODE, F1_MODE, F2_MODE, MANAGE_MODE, CLOCK_MODE };
-
-/* global variables */
 static SerialDevice *serialDevice = NULL;
+static const int serialBaud = 9600;
+static int serialCharactersPerSecond;
+
+#define CR  0X0D
+#define ESC 0X1B
+
+#define KEY_F1    0x01
+#define KEY_F2    0x02
+#define KEY_LEFT  0x04
+#define KEY_UP    0x08
+#define KEY_C     0x10
+#define KEY_DOWN  0x20
+#define KEY_RIGHT 0x40
+
+#define AFTER_CMD_DELAY 30
+
+typedef enum {
+  IM_Normal,
+  IM_F1,
+  IM_F2,
+  IM_Manage
+} InputMode;
+static InputMode inputMode;
+
 static TranslationTable outputTable;
-unsigned char xlated[20]; /* translated content of display */
-unsigned char status[2]; /* xlated content of status area */
-static enum mode_t mode = NORMAL_MODE;
+static unsigned char textCells[20];
+static unsigned char statusCells[2];
+static int refreshNeeded;
 
-/* #defines */
-/* delay this time after command - better: use CTS, DTR, CTR or find other signal which means "ready for next command" */
-#define AFTER_CMD_DELAY_TIME 10000
-#define AFTER_CMD_DELAY do { approximateDelay(AFTER_CMD_DELAY_TIME/1000); } while (0)
+static int
+writeData (BrailleDisplay *brl, const unsigned char *bytes, int count) {
+  ssize_t result = serialWriteData(serialDevice, bytes, count);
 
-static void beep(void)
-{
-	AFTER_CMD_DELAY;
-	serialWriteData(serialDevice, "\x1b" "B\r", 3);
-	AFTER_CMD_DELAY;
+  if (result == -1) {
+    LogError("write");
+    return 0;
+  }
+
+  drainBrailleOutput(brl, 0);
+  brl->writeDelay += (result * 1000 / serialCharactersPerSecond) + AFTER_CMD_DELAY;
+  return 1;
 }
 
-/* interface */
-static int brl_open(BrailleDisplay *brl, char **parameters, const char *device)
-{
-	{
-		static const DotsTable dots = {0X01, 0X02, 0X04, 0X80, 0X40, 0X20, 0X08, 0X10};
-		makeOutputTable(dots, outputTable);
-	}
+static int
+writeCells  (BrailleDisplay *brl) {
+  static const unsigned char beginSequence[] = {ESC, 'Z', '1'};
+  static const unsigned char endSequence[] = {CR};
 
-	if (!isSerialDevice(&device)) {
-		unsupportedDevice(device);
-		return 0;
-	}
+  unsigned char buffer[sizeof(beginSequence) + sizeof(statusCells) + sizeof(textCells) + sizeof(endSequence)];
+  unsigned char *byte = buffer;
+  int i;
 
-	/* init geometry */
-	brl->x = 20;
-	brl->y = 1;
+  memcpy(byte, beginSequence, sizeof(beginSequence));
+  byte += sizeof(beginSequence);
 
-	/* open device */
-	if (!(serialDevice = serialOpenDevice(device))) goto __errexit;
-	serialRestartDevice(serialDevice, 9600);
-	/* hm, how to switch to 38400 ? 
-	serialWriteData(serialDevice, "\eV\r", 3);
-	serialDiscardInput(serialDevice);
-	serialSetBaud(serialDevice, 38400);
-	*/
+  for (i=0; i<sizeof(statusCells); ++i) *byte++ = outputTable[statusCells[i]];
+  for (i=0; i<sizeof(textCells); ++i) *byte++ = outputTable[textCells[i]];
 
-	message("BRLTTY Ready", 0);
-	beep();
-	return 1;
+  memcpy(byte, endSequence, sizeof(endSequence));
+  byte += sizeof(endSequence);
 
-__errexit:
-	LogPrint(LOG_ERR, "Cannot initialize MiniBraille");
-	return 0;
+  return writeData(brl, buffer, byte-buffer);
 }
 
-static void brl_close(BrailleDisplay *brl)
-{
-	serialCloseDevice(serialDevice);
+static void
+updateCells (unsigned char *target, const unsigned char *source, size_t count) {
+  if (memcmp(target, source, count) != 0) {
+    memcpy(target, source, count);
+    refreshNeeded = 1;
+  }
 }
 
-static void refresh(void)
-{
-	unsigned char datab[] = { 27, 'Z', '1' };
-	unsigned char datae[] = { 13 };
-	serialWriteData(serialDevice, datab, sizeof datab);
-	serialWriteData(serialDevice, status, sizeof status);
-	serialWriteData(serialDevice, xlated, sizeof xlated);
-	serialWriteData(serialDevice, datae, sizeof datae);
+static void
+clearCells (unsigned char *cells, size_t count) {
+  memset(cells, 0, count);
+  refreshNeeded = 1;
 }
 
-static void brl_writeWindow(BrailleDisplay *brl)
-{
-	int i;
-	for (i = 0; i < 20; i++) xlated[i] = outputTable[brl->buffer[i]];
-	refresh();
+static int
+beep (BrailleDisplay *brl) {
+  static const unsigned char sequence[] = {ESC, 'B', CR};
+  return writeData(brl, sequence, sizeof(sequence));
 }
 
+static int
+brl_open (BrailleDisplay *brl, char **parameters, const char *device) {
+  {
+    static const DotsTable dots = {0X01, 0X02, 0X04, 0X80, 0X40, 0X20, 0X08, 0X10};
+    makeOutputTable(dots, outputTable);
+  }
 
-static int brl_readCommand(BrailleDisplay *brl, BRL_DriverCommandContext context)
-{
-	unsigned char znak;
-	int rv;
-	rv = serialReadData(serialDevice, &znak, 1, 0, 0);
-	switch (mode) {
-	case NORMAL_MODE:
-		if (rv == -1) return EOF;
-		if (rv == 0) return EOF;
-		switch (znak) {
-		case KEY_F1:
-			mode = F1_MODE;
-			message("F1 mode active", 0);
-			return BRL_CMD_NOOP;
-		case KEY_F2:
-			mode = F2_MODE;
-			message("F2 mode active", 0);
-			return BRL_CMD_NOOP;
-		case KEY_UP:
-			return BRL_CMD_LNUP;
-		case KEY_DOWN:
-			return BRL_CMD_LNDN;
-		case KEY_C:
-			return BRL_CMD_HOME;
-		case KEY_LEFT:
-			return BRL_CMD_FWINLT;
-		case KEY_RIGHT:
-			return BRL_CMD_FWINRT;
-		};
-		beep();
-		return EOF; /* cannot do anythink */
-	case F1_MODE:
-		if (rv == 0) { usleep(20000); return BRL_CMD_NOOP; };
-		mode = NORMAL_MODE;
-		if (rv == -1) return BRL_CMD_NOOP;
-		switch (znak) {
-		case KEY_F1:
-			mode = MANAGE_MODE; /* F1-F1 prechod na management */
-			message("Management mode", 0);
-			return BRL_CMD_NOOP;
-		case KEY_F2:
-			return BRL_CMD_CSRVIS;
-		case KEY_LEFT:
-			return BRL_CMD_LNBEG;
-		case KEY_RIGHT:
-			return BRL_CMD_LNEND;
-		case KEY_UP:
-			return BRL_CMD_TOP_LEFT;
-		case KEY_DOWN:
-			return BRL_CMD_BOT_LEFT;
-		case KEY_C:
-			return BRL_CMD_CAPBLINK;
-		};
-		beep();
-		return EOF;
-	case F2_MODE:
-		if (rv == 0) { usleep(20000); return BRL_CMD_NOOP; };
-		mode = NORMAL_MODE;
-		if (rv == -1) return BRL_CMD_NOOP;
-		switch (znak) {
-		case KEY_F1:
-			return BRL_CMD_CSRSIZE;
-		case KEY_F2:
-			return BRL_CMD_SKPIDLNS;
-		case KEY_C:
-			return BRL_CMD_CSRTRK;
-		case KEY_RIGHT:
-			return BRL_CMD_SIXDOTS;
-		case KEY_DOWN:
-			return BRL_CMD_DISPMD;
-		case KEY_LEFT:
-			return BRL_CMD_INFO;
-		case KEY_UP: /* stepmode (by melo byt) */
-			return BRL_CMD_FREEZE;
-		};
-		beep();
-		return EOF;
-	case CLOCK_MODE: {
-			char t[200];
-			struct tm* c;
-			time_t cas;
-			cas = time(NULL);
-			c = localtime(&cas);
-			strftime(t, sizeof t, "%e.%m %Y %H:%M:%S", c);
-			message(t, 0);
-		};
-		if (rv == 0) { usleep(500000); return BRL_CMD_NOOP; };
-		if (rv == -1) return BRL_CMD_NOOP;
-		mode = NORMAL_MODE;
-		break;
-	case MANAGE_MODE:
-		if (rv == 0) { usleep(50000); return BRL_CMD_NOOP; };
-		mode =  NORMAL_MODE;
-		if (rv == -1) return BRL_CMD_NOOP;
-		switch (znak) {
-		case KEY_F1:
-			return BRL_CMD_HELP;
-		case KEY_F2:
-			return BRL_CMD_INFO;
-		case KEY_C:
-			return BRL_CMD_PREFLOAD;
-		case KEY_LEFT:
-			return BRL_CMD_RESTARTBRL;
-		case KEY_RIGHT:
-			mode = CLOCK_MODE;
-			return BRL_CMD_NOOP;
-		case KEY_UP:
-			return BRL_CMD_PREFMENU;
-		case KEY_DOWN:
-			return BRL_CMD_PREFSAVE;
-		};
-	};
-	return EOF; /* never should reach this */
+  if (!isSerialDevice(&device)) {
+    unsupportedDevice(device);
+    return 0;
+  }
+
+  if ((serialDevice = serialOpenDevice(device))) {
+    if (serialRestartDevice(serialDevice, serialBaud)) {
+      serialCharactersPerSecond = serialBaud / serialGetCharacterBits(serialDevice);
+
+      /* hm, how to switch to 38400 ? 
+      static const unsigned char sequence[] = {ESC, 'V', CR};
+      writeData(brl, sequence, sizeof(sequence));
+      serialDiscardInput(serialDevice);
+      serialSetBaud(serialDevice, 38400);
+      */
+
+      clearCells(textCells,  sizeof(textCells));
+      clearCells(statusCells,  sizeof(statusCells));
+      inputMode = IM_Normal;
+
+      brl->x = 20;
+      brl->y = 1;
+
+      beep(brl);
+      return 1;
+    }
+
+    serialCloseDevice(serialDevice);
+    serialDevice = NULL;
+  }
+
+  LogPrint(LOG_ERR, "cannot initialize MiniBraille");
+  return 0;
 }
 
-static void brl_writeStatus(BrailleDisplay *brl, const unsigned char *s)
-{
-	status[0] = outputTable[s[0]];
-	status[1] = outputTable[s[1]];
-	/* refresh(); */
+static void
+brl_close (BrailleDisplay *brl) {
+  serialCloseDevice(serialDevice);
 }
 
+static void
+brl_writeWindow (BrailleDisplay *brl) {
+  updateCells(textCells, brl->buffer, sizeof(textCells));
+  if (refreshNeeded && (inputMode == IM_Normal)) {
+    writeCells(brl);
+    refreshNeeded = 0;
+  }
+}
+
+static void
+brl_writeStatus (BrailleDisplay *brl, const unsigned char *s) {
+  updateCells(statusCells, s, sizeof(statusCells));
+}
+
+static void
+setInputMode (InputMode mode, const char *name) {
+  if (name) message(name, MSG_NODELAY|MSG_SILENT);
+  inputMode = mode;
+  refreshNeeded = 1;
+}
+
+static int
+brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
+  unsigned char byte;
+  InputMode mode;
+
+  {
+    int result = serialReadData(serialDevice, &byte, 1, 0, 0);
+    if (result == 0) return EOF;
+
+    if (result == -1) {
+      LogError("read");
+      return BRL_CMD_RESTARTBRL;
+    }
+  }
+
+  mode = inputMode;
+  inputMode = IM_Normal;
+
+  switch (mode) {
+    case IM_Normal:
+      switch (byte) {
+        case KEY_F1:
+          setInputMode(IM_F1, "F1 Mode");
+          return BRL_CMD_NOOP;
+        case KEY_F2:
+          setInputMode(IM_F2, "F2 Mode");
+          return BRL_CMD_NOOP;
+        case KEY_C:
+          return BRL_CMD_HOME;
+        case KEY_UP:
+          return BRL_CMD_LNUP;
+        case KEY_DOWN:
+          return BRL_CMD_LNDN;
+        case KEY_LEFT:
+          return BRL_CMD_FWINLT;
+        case KEY_RIGHT:
+          return BRL_CMD_FWINRT;
+      }
+      break;
+
+    case IM_F1:
+      switch (byte) {
+        case KEY_F1:
+          setInputMode(IM_Manage, "Manage Mode"); /* F1-F1 prechod na management */
+          return BRL_CMD_NOOP;
+        case KEY_F2:
+          return BRL_CMD_CSRVIS;
+        case KEY_C:
+          return BRL_CMD_CAPBLINK;
+        case KEY_UP:
+          return BRL_CMD_TOP_LEFT;
+        case KEY_DOWN:
+          return BRL_CMD_BOT_LEFT;
+        case KEY_LEFT:
+          return BRL_CMD_LNBEG;
+        case KEY_RIGHT:
+          return BRL_CMD_LNEND;
+      }
+      break;
+
+    case IM_F2:
+      switch (byte) {
+        case KEY_F1:
+          return BRL_CMD_CSRSIZE;
+        case KEY_F2:
+          return BRL_CMD_SKPIDLNS;
+        case KEY_C:
+          return BRL_CMD_CSRTRK;
+        case KEY_UP:
+          return BRL_CMD_FREEZE;
+        case KEY_DOWN:
+          return BRL_CMD_DISPMD;
+        case KEY_LEFT:
+          return BRL_CMD_INFO;
+        case KEY_RIGHT:
+          return BRL_CMD_SIXDOTS;
+      }
+      break;
+
+    case IM_Manage:
+      switch (byte) {
+        case KEY_F1:
+          return BRL_CMD_HELP;
+        case KEY_F2:
+          return BRL_CMD_INFO;
+        case KEY_C:
+          return BRL_CMD_PREFLOAD;
+        case KEY_UP:
+          return BRL_CMD_PREFMENU;
+        case KEY_DOWN:
+          return BRL_CMD_PREFSAVE;
+        case KEY_LEFT:
+          return BRL_CMD_RESTARTBRL;
+        case KEY_RIGHT: {
+          time_t clock = time(NULL);
+          const struct tm *local = localtime(&clock);
+          char text[200];
+          strftime(text, sizeof(text), "%Y-%m-%d %H:%M:%S", local);
+          message(text, 0);
+          return BRL_CMD_NOOP;
+        }
+      }
+      break;
+  }
+
+  LogPrint(LOG_WARNING, "unhandled key: %02X (mode %02X)", byte, mode);
+  beep(brl);
+  return EOF;
+}
