b26faf46fd156b8e4f185f8f43c9c30166823934
Dave Mielke <dave@mielke.cc>
2006-10-07 22:09:05 +0000

    api_client.c no longer needs to include brldefs.h. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2477 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/api_client.c
+++ Programs/api_client.c
@@ -89,7 +89,6 @@
 
 #include "api.h"
 #include "api_protocol.h"
-#include "brldefs.h"
 
 #define BRLAPI(fun) brlapi_ ## fun
 #include "api_common.h"
--- Programs/brldefs.h
+++ Programs/brldefs.h
@@ -7,10 +7,8 @@
  * BRLTTY comes with ABSOLUTELY NO WARRANTY.
  *
  * This is free software, placed under the terms of the
- * GNU Lesser General Public License, as published by the Free Software
- * Foundation; either version 2.1 of the License,
- * or (at your option) any later version.
- * Please see the file COPYING-API for details.
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
  *
  * Web Page: http://mielke.cc/brltty/
  *
