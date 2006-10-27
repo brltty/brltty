0b27fa987f1b92d42fbcac3ae4275d0728421075
Dave Mielke <dave@mielke.cc>
2006-10-27 02:22:58 +0000

    Forgot to add BrlapiSettings.java and BrlapiWriteStruct.java. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2596 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Bindings/Java/BrlapiSettings.java
@@ -0,0 +1,46 @@
+/*
+ * libbrlapi - A library providing access to braille terminals for applications.
+ *
+ * Copyright (C) 2002-2006 by
+ *   Samuel Thibault <Samuel.Thibault@ens-lyon.org>
+ *   Sébastien Hinderer <Sebastien.Hinderer@ens-lyon.org>
+ * All rights reserved.
+ *
+ * libbrlapi comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU Lesser General Public License, as published by the Free Software
+ * Foundation; either version 2.1 of the License,
+ * or (at your option) any later version.
+ * Please see the file COPYING-API for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+public class BrlapiSettings {
+  private String hostName;
+  private String authKey;
+
+  public BrlapiSettings (String hostName, String authKey) {
+    this.hostName = hostName;
+    this.authKey = authKey;
+  }
+
+  public BrlapiSettings (String hostName) {
+    this(hostName, null);
+  }
+
+  public BrlapiSettings () {
+    this(null);
+  }
+
+  public String getHostName () {
+    return hostName;
+  }
+
+  public String getAuthKey () {
+    return authKey;
+  }
+}
---
+++ Bindings/Java/BrlapiWriteStruct.java
@@ -0,0 +1,47 @@
+/*
+ * libbrlapi - A library providing access to braille terminals for applications.
+ *
+ * Copyright (C) 2002-2006 by
+ *   Samuel Thibault <Samuel.Thibault@ens-lyon.org>
+ *   Sébastien Hinderer <Sebastien.Hinderer@ens-lyon.org>
+ * All rights reserved.
+ *
+ * libbrlapi comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU Lesser General Public License, as published by the Free Software
+ * Foundation; either version 2.1 of the License,
+ * or (at your option) any later version.
+ * Please see the file COPYING-API for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+public class BrlapiWriteStruct {
+  public int displayNumber;
+  public int regionBegin;
+  public int regionSize;
+  public String text;
+  public byte attrAnd[];
+  public byte attrOr[];
+  public int cursor;
+
+  public BrlapiWriteStruct (
+    int displayNumber, int regionBegin, int regionSize,
+    String text, byte attrAnd[], byte attrOr[], int cursor
+  ) {
+    this.displayNumber = displayNumber;
+    this.regionBegin = regionBegin;
+    this.regionSize = regionSize;
+    this.text = text;
+    this.attrAnd = attrAnd;
+    this.attrOr = attrOr;
+    this.cursor = cursor;
+  }
+
+  public BrlapiWriteStruct () {
+    this(-1, 0, 0, null, null, null, -1); 
+  }
+}
