d55fec21a68843c85218654246f06ba0abbde4e2
Dave Mielke <dave@mielke.cc>
2006-10-27 05:06:11 +0000

    Make recent type name changes to Apitest.java. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2601 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Bindings/Java/Apitest.java
+++ Bindings/Java/Apitest.java
@@ -1,22 +1,20 @@
 public class Apitest {
   public static void main(String argv[]) {
     try {
-      Brlapi brlapi = new Brlapi();
-      int fd;
       BrlapiSize size;
       int tty;
       BrlapiKey key;
       long keylong;
-      Brlapi.settings settings = new Brlapi.settings();
-      Brlapi.writeStruct ws2 = new Brlapi.writeStruct(-1, 10, 20,
+      BrlapiSettings settings = new BrlapiSettings();
+      BrlapiWriteStruct ws2 = new BrlapiWriteStruct(-1, 10, 20,
 	  "Key Pressed         ",
 	  "????????????????????".getBytes(),
 	  null,3);
       System.out.print("Connecting to BrlAPI... ");
       System.loadLibrary("brlapi_java");
-      fd = brlapi.initializeConnection(settings, settings);
+      Brlapi brlapi = new Brlapi(settings);
       size = brlapi.getDisplaySize();
-      System.out.println("done (fd="+fd+")");
+      System.out.println("done (fd="+brlapi.getFileDescriptor()+")");
       System.out.println("Connected to "+settings.hostName+" using key file "+settings.authKey);
       System.out.println("driver "+brlapi.getDriverId()+" "+brlapi.getDriverName());
       System.out.println("display "+size.x()+"x"+size.y());
