06afffb20e8aaa2947d14ee2320538d9e780e0e1
Dave Mielke <dave@mielke.cc>
2006-10-31 16:37:55 +0000

    Lisp bindings changes: (ml)    Split out the keycode expansion from read-key into expand-key.    Implement brlapi_write as write-region.    Add some function documentation.    Help the compiler optimize by adding some explicit function arg type declarations.
    git-svn-id: svn://mielke.cc/main/brltty@2644 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Bindings/Lisp/brlapi.lisp
+++ Bindings/Lisp/brlapi.lisp
@@ -1,11 +1,14 @@
 ;;; -*- outline-regexp: ";;;;;*"; indent-tabs-mode: nil -*-
 
+(eval-when (:compile-toplevel)
+  (declaim (optimize (safety 3) (debug 3))))
 (defpackage :brlapi
   (:use :common-lisp :cffi)
   (:export #:connect #:disconnect
 	   #:driver-id #:driver-name #:display-size
            #:get-tty #:leave-tty
-           #:write-text #:read-key))
+           #:write-text #:write-dots #:write-region
+           #:read-key #:expand-key))
 (in-package :brlapi)
 
 
@@ -29,6 +32,7 @@
 
 (defmethod translate-from-foreign (value (name (eql 'brlapi-code)))
   "Raise a BRLAPI-ERROR if VALUE, a brlapi-code, is -1."
+  (declare (integer value))
   (if (eql value -1)
       (error 'brlapi-error
              :text (foreign-funcall "brlapi_strerror"
@@ -63,7 +67,8 @@
 (defun driver-id ()
   (with-foreign-pointer-as-string (str 4 str-size)
     (foreign-funcall "brlapi_getDriverId"
-                     :string str :int str-size brlapi-code)))
+                     :string str :int str-size
+                     brlapi-code)))
 
 (defun driver-name ()
   (with-foreign-pointer-as-string (str 64 str-size)
@@ -79,6 +84,7 @@
 ;;;; TTY acquisition
 
 (defun get-tty (tty &optional how)
+  (declare (integer tty))
   (unless (stringp how) (setf how ""))
   (foreign-funcall "brlapi_enterTtyMode" :int tty :string how brlapi-code))
 
@@ -87,13 +93,20 @@
 
 ;;;; Output
 
-(defcfun ("brlapi_writeText" write-text) brlapi-code
-  (cursor :int)
-  (text :string))
+(defun write-text (text &key (cursor -1))
+  "Write TEXT (a string) to the braille display."
+  (declare (string text))
+  (declare (integer cursor))
+  (if (eql (foreign-funcall "brlapi_writeText"
+                            :int cursor :string text
+                            brlapi-code)
+           0)
+      text))
 
 (defbitfield (dots :uint8)
   (:dot1 #x01) :dot2 :dot3 :dot4 :dot5 :dot6 :dot7 :dot8)
 (defun write-dots (&rest dots-list)
+  "Write the given dots list to the display."
   (with-foreign-object (dots 'dots (display-size))
     (loop for i below (min (display-size) (length dots-list))
           do (setf (mem-aref dots 'dots i)
@@ -101,6 +114,47 @@
     (loop for i from (length dots-list) below (display-size)
           do (setf (mem-aref dots 'dots i) 0))
     (foreign-funcall "brlapi_writeDots" :pointer dots brlapi-code)))
+
+(defcstruct write-struct
+  (display-number :int)
+  (region-begin :int)
+  (region-size :int)
+  (text :pointer)
+  (attr-and :pointer)
+  (attr-or :pointer)
+  (cursor :int)
+  (charset :string))
+
+(defun write-region (text &key (begin 1) size (cursor -1) (display-number -1)
+                          (charset "") attr-and attr-or)
+  "Update a specific region of the braille display and apply and/or masks."
+  (let ((size (or size (min (display-size)
+                            (max (length text)
+                                 (length attr-and)
+                                 (length attr-or))))))    
+    (with-foreign-objects ((ws 'write-struct)
+                           (txt :string size)
+                           (attra 'dots size)
+                           (attro 'dots size))
+      (loop for i below size
+            do (setf (mem-aref txt :uint8 i) (char-code #\SPACE)))
+      (loop for i below (min size (length text))
+            do (setf (mem-aref txt :uint8 i) (char-code (aref text i))))
+      (loop for i below size
+            do (setf (mem-aref attra :uint8 i) #XFF (mem-aref attro :uint8 i) 0))
+      (loop for i below (min size (length attr-and))
+            do (setf (mem-aref attra :uint8 i) (foreign-bitfield-value 'dots (nth i attr-and))))
+      (loop for i below (min size (length attr-or))
+            do (setf (mem-aref attro :uint8 i) (foreign-bitfield-value 'dots (nth i attr-or))))
+      (setf (foreign-slot-value ws 'write-struct 'display-number) display-number
+            (foreign-slot-value ws 'write-struct 'cursor) cursor
+            (foreign-slot-value ws 'write-struct 'region-begin) begin
+            (foreign-slot-value ws 'write-struct 'region-size) size
+            (foreign-slot-value ws 'write-struct 'charset) charset
+            (foreign-slot-value ws 'write-struct 'text) txt
+            (foreign-slot-value ws 'write-struct 'attr-or) attro
+            (foreign-slot-value ws 'write-struct 'attr-and) attra)
+      (eql (foreign-funcall "brlapi_write" :pointer ws brlapi-code) 0))))
 
 
 ;;;; Input
@@ -110,9 +164,17 @@
   (with-foreign-object (key 'key-code)
     (case (foreign-funcall "brlapi_readKey" :boolean block :pointer key brlapi-code)
       (0 nil)
-      (1 (with-foreign-objects ((command :int) (args :int) (flags :int))
-           (foreign-funcall "brlapi_expandKeyCode"
-                            key-code (mem-ref key 'key-code)
-                            :pointer command :pointer args :pointer flags
-                            :int)
-           (list (mem-ref command :int) (mem-ref args :int) (mem-ref flags :int)))))))
+      (1 (mem-ref key 'key-code)))))
+
+(defun expand-key (keycode)
+  (with-foreign-objects ((command :int) (arg :int) (flags :int))
+    (foreign-funcall "brlapi_expandKeyCode"
+                     key-code keycode
+                     :pointer command :pointer arg :pointer flags
+                     :int)
+    (values (intern (foreign-funcall "brlapi_getKeyName"
+                                     :int (mem-ref command :int)
+                                     :int (mem-ref arg :int)
+                                     :string))
+            (mem-ref arg :int)
+            (mem-ref flags :int))))
