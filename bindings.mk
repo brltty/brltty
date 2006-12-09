77b1fb2c3e583e0f9cd6a0c1e5917eda4a9e4a83
Dave Mielke <dave@mielke.cc>
2006-12-09 16:17:27 +0000

    Some make file symbol renaming for the bindings. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2756 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- bindings.mk
+++ bindings.mk
@@ -15,16 +15,18 @@
 # This software is maintained by Dave Mielke <dave@mielke.cc>.
 ###############################################################################
 
-BRLDEFS_HDR = $(SRC_TOP)$(PGM_DIR)/brldefs.h
-BRLDEFS_AWK = $(SRC_TOP)$(PGM_DIR)/brldefs.awk
-APIDEFS_AWK = $(SRC_TOP)$(PGM_DIR)/brlapi.awk
-
 API_NAME = brlapi
 API_DIR = $(BLD_TOP)$(PGM_DIR)
-API_LIB = $(API_DIR)/lib$(API_NAME).$(LIB_EXT)
+API_LIB = $(API_DIR)/$(LIB_PFX)$(API_NAME).$(LIB_EXT)
 API_LDFLAGS = -L$(API_DIR) -l$(API_NAME)
 
-API_HEADER = $(API_DIR)/brlapi.h
-API_constants = $(API_DIR)/brlapi_constants.h
-API_HEADERS = $(API_HEADER) $(API_constants)
+API_HDR = $(API_DIR)/$(API_NAME).h
+API_AWK = $(SRC_TOP)$(PGM_DIR)/$(API_NAME).awk
+
+API_constants = $(API_DIR)/$(API_NAME)_constants.h
+API_HDRS = $(API_HDR) $(API_constants)
+
+BRL_NAME = brldefs
+BRL_HDR = $(SRC_TOP)$(PGM_DIR)/$(BRL_NAME).h
+BRL_AWK = $(SRC_TOP)$(PGM_DIR)/$(BRL_NAME).awk
 
