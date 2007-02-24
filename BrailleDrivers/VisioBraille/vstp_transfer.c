88fb1cbf005840b74020b6404102d1622fde1ed3
Dave Mielke <dave@mielke.cc>
2007-02-24 03:25:32 +0000

    vstp_transfer.c needs to include brlapi_protocol.h. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2924 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- BrailleDrivers/VisioBraille/vstp_transfer.c
+++ BrailleDrivers/VisioBraille/vstp_transfer.c
@@ -24,16 +24,19 @@
  * handles file transfers
  */
 
-#include <sys/stat.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
-#include <malloc.h>
-#include "Programs/brlapi.h"
 #include <ctype.h>
 #include <errno.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "Programs/brlapi.h"
+#include "Programs/brlapi_protocol.h"
+
 #include "vstp.h"
 
 /* SEND */
