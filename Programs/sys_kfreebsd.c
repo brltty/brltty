0c0c236e39a9da662a1aa77930817a05ff4fe00a
Dave Mielke <dave@mielke.cc>
2007-03-09 13:58:59 +0000

    Add support for kFreeBSD. (ml)
    git-svn-id: svn://mielke.cc/main/brltty@2936 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Programs/sys_kfreebsd.c
@@ -0,0 +1,49 @@
+/*
+ * BRLTTY - A background process providing access to the console screen (when in
+ *          text mode) for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2007 by The BRLTTY Developers.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation.  Please see the file COPYING for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#include "prologue.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#include "misc.h"
+#include "system.h"
+
+#include "sys_prog_none.h"
+
+#include "sys_boot_none.h"
+
+#include "sys_exec_unix.h"
+
+#define SHARED_OBJECT_LOAD_FLAGS (RTLD_NOW | RTLD_GLOBAL)
+#include "sys_shlib_dlfcn.h"
+
+#include "sys_beep_spkr.h"
+
+#ifdef ENABLE_PCM_SUPPORT
+#define PCM_OSS_DEVICE_PATH "/dev/dsp"
+#include "sys_pcm_oss.h"
+#endif /* ENABLE_PCM_SUPPORT */
+
+#ifdef ENABLE_MIDI_SUPPORT
+#include "sys_midi_none.h"
+#endif /* ENABLE_MIDI_SUPPORT */
+
+#include "sys_ports_none.h"
