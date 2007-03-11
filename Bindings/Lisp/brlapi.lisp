ffd615bd04f49f22c99fde958127aafafd1a5f08
Dave Mielke <dave@mielke.cc>
2007-03-11 00:23:49 +0000

    Add copyright notices to the Lisp bindings. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2942 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Bindings/Lisp/brlapi.lisp
+++ Bindings/Lisp/brlapi.lisp
@@ -1,3 +1,21 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; libbrlapi - A library providing access to braille terminals for applications.
+;
+; Copyright (C) 2006-2007 by Mario Lang <mlang@delysid.org>
+;
+; libbrlapi comes with ABSOLUTELY NO WARRANTY.
+;
+; This is free software, placed under the terms of the
+; GNU Lesser General Public License, as published by the Free Software
+; Foundation; either version 2.1 of the License,
+; or (at your option) any later version.
+; Please see the file COPYING-API for details.
+;
+; Web Page: http://mielke.cc/brltty/
+;
+; This software is maintained by Dave Mielke <dave@mielke.cc>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (eval-when (:compile-toplevel)
   (declaim (optimize (safety 3) (debug 3))))
 
