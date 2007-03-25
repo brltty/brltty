00e83da5dc27e7c92dadb7446344ba459fd9068c
Dave Mielke <dave@mielke.cc>
2007-03-25 15:32:51 +0000

    brlapi_type_t has been renamed to brlapi_packetType_t. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@2968 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Bindings/Java/bindings.c
+++ Bindings/Java/bindings.c
@@ -86,7 +86,7 @@ static void ThrowError(JNIEnv *jenv, const char *msg) {
   }
 }
 
-static void exceptionHandler(brlapi_handle_t *handle, int err, brlapi_type_t type, const void *buf, size_t size) {
+static void exceptionHandler(brlapi_handle_t *handle, int err, brlapi_packetType_t type, const void *buf, size_t size) {
   jarray jbuf;
   jclass jcexcep;
   jmethodID jinit;
@@ -724,7 +724,7 @@ JNIEXPORT jstring JNICALL Java_BrlapiNative_getPacketTypeName(JNIEnv *jenv, jcla
 
   env = jenv;
 
-  if (!(type = brlapi_getPacketTypeName((brlapi_type_t) jtype))) {
+  if (!(type = brlapi_getPacketTypeName((brlapi_packetType_t) jtype))) {
     ThrowError(jenv, __func__);
     return NULL;
   }
