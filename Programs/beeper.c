cf4a75db2e8771d46e359ceb3ed79431a7289db6
Dave Mielke <dave@mielke.cc>
2007-08-28 17:33:06 +0000

    Standardize the beeper source code. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@3216 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/beeper.c
+++ Programs/beeper.c
@@ -21,62 +21,69 @@
 #include "system.h"
 #include "notes.h"
 
-static int beeperOpened = 0;
+static int beeperEnabled = 0;
 
-static int beeperConstruct (int errorLevel) {
-   if (!beeperOpened) {
-      if (!canBeep()) {
-         LogPrint(LOG_DEBUG, "cannot open beeper.");
-         return 0;
-      }
-      beeperOpened = 1;
-      LogPrint(LOG_DEBUG, "beeper opened.");
-   }
-   return 1;
+static int
+beeperConstruct (int errorLevel) {
+  if (!beeperEnabled) {
+    if (!canBeep()) {
+      LogPrint(LOG_DEBUG, "beeper not available");
+      return 0;
+    }
+
+    beeperEnabled = 1;
+    LogPrint(LOG_DEBUG, "beeper enabled");
+  }
+
+  return 1;
 }
 
-static int beeperPlay (int note, int duration) {
-   if (beeperOpened) {
-      LogPrint(LOG_DEBUG, "tone: msec=%d note=%d",
-               duration, note);
+static int
+beeperPlay (int note, int duration) {
+  if (beeperEnabled) {
+    LogPrint(LOG_DEBUG, "tone: msec=%d note=%d",
+             duration, note);
 
-      if (!note) {
-         accurateDelay(duration);
-	 return 1;
-      }
+    if (!note) {
+      accurateDelay(duration);
+      return 1;
+    }
 
-      if (asynchronousBeep((int)noteFrequencies[note], duration*4)) {
-         accurateDelay(duration);
-	 stopBeep();
-         return 1;
-      }
+    if (asynchronousBeep((int)noteFrequencies[note], duration*4)) {
+      accurateDelay(duration);
+      stopBeep();
+      return 1;
+    }
 
-      if (startBeep((int)noteFrequencies[note])) {
-         accurateDelay(duration);
-	 stopBeep();
-         return 1;
-      }
+    if (startBeep((int)noteFrequencies[note])) {
+      accurateDelay(duration);
+      stopBeep();
+      return 1;
+    }
 
-      if (synchronousBeep((int)noteFrequencies[note], duration)) return 1;
-   }
-   return 0;
+    if (synchronousBeep((int)noteFrequencies[note], duration)) return 1;
+  }
+
+  return 0;
 }
 
-static int beeperFlush (void) {
-   return beeperOpened;
+static int
+beeperFlush (void) {
+  return beeperEnabled;
 }
 
-static void beeperDestruct (void) {
-   if (beeperOpened) {
-      beeperOpened = 0;
-      endBeep();
-      LogPrint(LOG_DEBUG, "beeper closed.");
-   }
+static void
+beeperDestruct (void) {
+  if (beeperEnabled) {
+    beeperEnabled = 0;
+    endBeep();
+    LogPrint(LOG_DEBUG, "beeper disabled");
+  }
 }
 
 const NoteGenerator beeperNoteGenerator = {
-   beeperConstruct,
-   beeperPlay,
-   beeperFlush,
-   beeperDestruct
+  beeperConstruct,
+  beeperPlay,
+  beeperFlush,
+  beeperDestruct
 };
