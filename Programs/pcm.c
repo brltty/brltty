f413092025c4949e36dff26fb7a6425ba1a1dad7
Dave Mielke <dave@mielke.cc>
2007-08-28 19:28:33 +0000

    Standardize the PCM code. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@3221 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/pcm.c
+++ Programs/pcm.c
@@ -34,191 +34,225 @@ static PcmAmplitudeFormat amplitudeFormat;
 static unsigned char *blockAddress = NULL;
 static size_t blockUsed = 0;
 
-static int pcmConstruct (int errorLevel) {
-   if (pcm) return 1;
-   if ((pcm = openPcmDevice(errorLevel, opt_pcmDevice))) {
-      blockSize = getPcmBlockSize(pcm);
-      sampleRate = getPcmSampleRate(pcm);
-      channelCount = getPcmChannelCount(pcm);
-      amplitudeFormat = getPcmAmplitudeFormat(pcm);
-      if ((blockAddress = malloc(blockSize))) {
-         blockUsed = 0;
-         LogPrint(LOG_DEBUG, "PCM opened: blk=%d rate=%d chan=%d fmt=%d",
-                  blockSize, sampleRate, channelCount, amplitudeFormat);
-         return 1;
-      } else {
-        LogError("PCM buffer allocation");
-      }
-      closePcmDevice(pcm);
-      pcm = NULL;
-   } else {
-      LogPrint(LOG_DEBUG, "cannot open PCM.");
-   }
-   return 0;
-}
+static int
+pcmConstruct (int errorLevel) {
+  if (pcm) return 1;
 
-static int flushBytes (void) {
-   int ok = writePcmData(pcm, blockAddress, blockUsed);
-   if (ok) {
-      blockUsed = 0;
-   } else {
-      LogError("PCM write");
-   }
-   return ok;
-}
+  if ((pcm = openPcmDevice(errorLevel, opt_pcmDevice))) {
+    blockSize = getPcmBlockSize(pcm);
+    sampleRate = getPcmSampleRate(pcm);
+    channelCount = getPcmChannelCount(pcm);
+    amplitudeFormat = getPcmAmplitudeFormat(pcm);
+    blockUsed = 0;
 
-static int writeBytes (const unsigned char *address, size_t length) {
-   while (length > 0) {
-      size_t count = blockSize - blockUsed;
-      if (length < count) count = length;
-      memcpy(&blockAddress[blockUsed], address, count);
-      address += count;
-      length -= count;
-      if ((blockUsed += count) == blockSize)
-         if (!flushBytes())
-            return 0;
-   }
-   return 1;
-}
-
-static int writeAmplitude (int amplitude) {
-   unsigned char sample[4];
-   size_t length;
-   int channel;
-   switch (amplitudeFormat) {
-      default:
-         length = 0;
-         break;
-      case PCM_FMT_U8:
-         amplitude += 0X8000;
-      case PCM_FMT_S8:
-         sample[0] = amplitude >> 8;
-         length = 1;
-         break;
-      case PCM_FMT_U16B:
-         amplitude += 0X8000;
-      case PCM_FMT_S16B:
-         sample[0] = amplitude >> 8;
-         sample[1] = amplitude;
-         length = 2;
-         break;
-      case PCM_FMT_U16L:
-         amplitude += 0X8000;
-      case PCM_FMT_S16L:
-         sample[0] = amplitude;
-         sample[1] = amplitude >> 8;
-         length = 2;
-         break;
-      case PCM_FMT_ULAW: {
-         int negative = amplitude < 0;
-         int exponent = 0X7;
-         unsigned char value;
-         const unsigned int bias = 0X84;
-         const unsigned int clip = 0X7FFF - bias;
-         if (negative) amplitude = -amplitude;
-         if (amplitude > clip) amplitude = clip;
-         amplitude += bias;
-         while ((exponent > 0) && !(amplitude & 0X4000)) {
-            amplitude <<= 1;
-            --exponent;
-         }
-         value = (exponent << 4) | ((amplitude >> 10) & 0X0F);
-         if (negative) value |= 0X80;
-         sample[0] = ~value;
-         length = 1;
-         break;
-      }
-      case PCM_FMT_ALAW: {
-         int negative = amplitude < 0;
-         int exponent = 0X7;
-         unsigned char value;
-         if (negative) amplitude = -amplitude;
-         while ((exponent > 0) && !(amplitude & 0X4000)) {
-            amplitude <<= 1;
-            --exponent;
-         }
-         if (!exponent) amplitude >>= 1;
-         value = (exponent << 4) | ((amplitude >> 10) & 0X0F);
-         if (negative) value |= 0X80;
-         sample[0] = value ^ 0X55;
-         length = 1;
-         break;
-      }
-   }
-   for (channel=0; channel<channelCount; ++channel)
-      if (!writeBytes(sample, length))
-         return 0;
-   return 1;
-}
-
-static int pcmPlay (int note, int duration) {
-   if (pcm) {
-      long int sampleCount = sampleRate * duration / 1000;
-      if (note) {
-         /* A triangle waveform sounds nice, is lightweight, and avoids
-          * relying too much on floating-point performance and/or on
-          * expensive math functions like sin(). Considerations like
-	  * these are especially important on PDAs without any FPU.
-          */ 
-         float maxAmplitude = 32767.0 * prefs.pcmVolume / 100;
-         float waveLength = sampleRate / noteFrequencies[note];
-         float stepSample = 4 * maxAmplitude / waveLength;
-         float currSample = 0;
-         if (waveLength <= 2) stepSample = 0;
-         LogPrint(LOG_DEBUG, "tone: msec=%d smct=%lu note=%d",
-                  duration, sampleCount, note);
-         while (sampleCount > 0) {
-            do {
-               sampleCount--;
-               if (!writeAmplitude(currSample)) return 0;
-               currSample += stepSample;
-               if (currSample >= maxAmplitude) {
-                  currSample = 2*maxAmplitude - currSample;
-                  stepSample = -stepSample;
-               } else if (currSample <= -maxAmplitude) {
-                  currSample = -2*maxAmplitude - currSample;
-                  stepSample = -stepSample;
-               }
-            } while ((stepSample < 0) || (currSample < 0) || (currSample > stepSample));
-         }
-      } else {
-         LogPrint(LOG_DEBUG, "tone: msec=%d smct=%lu note=%d",
-                  duration, sampleCount, note);
-         while (sampleCount > 0) {
-            if (!writeAmplitude(0)) return 0;
-            --sampleCount;
-         }
-      }
+    if ((blockAddress = malloc(blockSize))) {
+      LogPrint(LOG_DEBUG, "PCM enabled: blk=%d rate=%d chan=%d fmt=%d",
+               blockSize, sampleRate, channelCount, amplitudeFormat);
       return 1;
-   }
-   return 0;
+    } else {
+      LogError("malloc");
+    }
+
+    closePcmDevice(pcm);
+    pcm = NULL;
+  }
+
+  LogPrint(LOG_DEBUG, "PCM not available");
+  return 0;
 }
 
-static int flushBlock (void) {
-   while (blockUsed)
-      if (!writeAmplitude(0))
-         return 0;
-   return 1;
+static int
+flushBytes (void) {
+  int ok = writePcmData(pcm, blockAddress, blockUsed);
+  if (ok) blockUsed = 0;
+  return ok;
 }
 
-static int pcmFlush (void) {
-   return (pcm)? flushBlock(): 0;
+static int
+writeBytes (const unsigned char *address, size_t length) {
+  while (length > 0) {
+    size_t count = blockSize - blockUsed;
+    if (length < count) count = length;
+    memcpy(&blockAddress[blockUsed], address, count);
+    address += count;
+    length -= count;
+
+    if ((blockUsed += count) == blockSize)
+      if (!flushBytes())
+        return 0;
+  }
+
+  return 1;
 }
 
-static void pcmDestruct (void) {
-   if (pcm) {
-      flushBlock();
-      free(blockAddress);
-      blockAddress = NULL;
-      closePcmDevice(pcm);
-      pcm = NULL;
-      LogPrint(LOG_DEBUG, "PCM closed.");
-   }
+static int
+writeAmplitude (int amplitude) {
+  unsigned char sample[4];
+  size_t length;
+
+  switch (amplitudeFormat) {
+    default:
+      length = 0;
+      break;
+
+    case PCM_FMT_U8:
+      amplitude += 0X8000;
+    case PCM_FMT_S8:
+      sample[0] = amplitude >> 8;
+      length = 1;
+      break;
+
+    case PCM_FMT_U16B:
+      amplitude += 0X8000;
+    case PCM_FMT_S16B:
+      sample[0] = amplitude >> 8;
+      sample[1] = amplitude;
+      length = 2;
+      break;
+
+    case PCM_FMT_U16L:
+      amplitude += 0X8000;
+    case PCM_FMT_S16L:
+      sample[0] = amplitude;
+      sample[1] = amplitude >> 8;
+      length = 2;
+      break;
+
+    case PCM_FMT_ULAW: {
+      int negative = amplitude < 0;
+      int exponent = 0X7;
+      unsigned char value;
+      const unsigned int bias = 0X84;
+      const unsigned int clip = 0X7FFF - bias;
+
+      if (negative) amplitude = -amplitude;
+      if (amplitude > clip) amplitude = clip;
+      amplitude += bias;
+
+      while ((exponent > 0) && !(amplitude & 0X4000)) {
+        amplitude <<= 1;
+        --exponent;
+      }
+
+      value = (exponent << 4) | ((amplitude >> 10) & 0X0F);
+      if (negative) value |= 0X80;
+      sample[0] = ~value;
+      length = 1;
+      break;
+    }
+
+    case PCM_FMT_ALAW: {
+      int negative = amplitude < 0;
+      int exponent = 0X7;
+      unsigned char value;
+
+      if (negative) amplitude = -amplitude;
+
+      while ((exponent > 0) && !(amplitude & 0X4000)) {
+        amplitude <<= 1;
+        --exponent;
+      }
+
+      if (!exponent) amplitude >>= 1;
+      value = (exponent << 4) | ((amplitude >> 10) & 0X0F);
+      if (negative) value |= 0X80;
+      sample[0] = value ^ 0X55;
+      length = 1;
+      break;
+    }
+  }
+
+  {
+    int channel;
+    for (channel=0; channel<channelCount; ++channel) {
+      if (!writeBytes(sample, length)) return 0;
+    }
+  }
+
+  return 1;
+}
+
+static int
+pcmPlay (int note, int duration) {
+  if (pcm) {
+    long int sampleCount = sampleRate * duration / 1000;
+
+    if (note) {
+      /* A triangle waveform sounds nice, is lightweight, and avoids
+       * relying too much on floating-point performance and/or on
+       * expensive math functions like sin(). Considerations like
+       * these are especially important on PDAs without any FPU.
+       */ 
+      float maxAmplitude = 32767.0 * prefs.pcmVolume / 100;
+      float waveLength = sampleRate / noteFrequencies[note];
+      float stepSample = 4 * maxAmplitude / waveLength;
+      float currSample = 0;
+
+      if (waveLength <= 2) stepSample = 0;
+      LogPrint(LOG_DEBUG, "tone: msec=%d smct=%lu note=%d",
+               duration, sampleCount, note);
+
+      while (sampleCount > 0) {
+        do {
+          sampleCount--;
+          if (!writeAmplitude(currSample)) return 0;
+          currSample += stepSample;
+
+          if (currSample >= maxAmplitude) {
+            currSample = 2*maxAmplitude - currSample;
+            stepSample = -stepSample;
+          } else if (currSample <= -maxAmplitude) {
+            currSample = -2*maxAmplitude - currSample;
+            stepSample = -stepSample;
+          }
+        } while ((stepSample < 0) || (currSample < 0) || (currSample > stepSample));
+      }
+    } else {
+      LogPrint(LOG_DEBUG, "tone: msec=%d smct=%lu note=%d",
+               duration, sampleCount, note);
+
+      while (sampleCount > 0) {
+        if (!writeAmplitude(0)) return 0;
+        --sampleCount;
+      }
+    }
+
+    return 1;
+  }
+
+  return 0;
+}
+
+static int
+flushBlock (void) {
+  while (blockUsed)
+    if (!writeAmplitude(0))
+      return 0;
+
+  return 1;
+}
+
+static int
+pcmFlush (void) {
+  return (pcm)? flushBlock(): 0;
+}
+
+static void
+pcmDestruct (void) {
+  if (pcm) {
+    flushBlock();
+    free(blockAddress);
+    blockAddress = NULL;
+
+    closePcmDevice(pcm);
+    pcm = NULL;
+    LogPrint(LOG_DEBUG, "PCM disabled");
+  }
 }
 
 const NoteGenerator pcmNoteGenerator = {
-   pcmConstruct,
-   pcmPlay,
-   pcmFlush,
-   pcmDestruct
+  pcmConstruct,
+  pcmPlay,
+  pcmFlush,
+  pcmDestruct
 };
