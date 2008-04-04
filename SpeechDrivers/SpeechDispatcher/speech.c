0fb17db4a5d53ac6a6ae138fb4d7940fd59f9f7a
Dave Mielke <dave@mielke.cc>
2008-04-04 15:44:04 +0000

    Try to reopen the connection to Speech Dispatcher if an error occurs. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@3650 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- SpeechDrivers/SpeechDispatcher/speech.c
+++ SpeechDrivers/SpeechDispatcher/speech.c
@@ -36,10 +36,100 @@ typedef enum {
 
 #include <libspeechd.h>
 
-static SPDConnection *connection = NULL;
+static SPDConnection *connectionHandle = NULL;
+static const char *moduleName;
+static const char *languageName;
+static SPDVoiceType voiceType;
+static signed int relativeRate;
+static signed int relativeVolume;
+static SPDPunctuation punctuationVerbosity;
+
+static void
+clearSettings (void) {
+  moduleName = NULL;
+  languageName = NULL;
+  voiceType = -1;
+  relativeRate = 0;
+  relativeVolume = 0;
+  punctuationVerbosity = -1;
+}
+
+static void
+closeConnection (void) {
+  if (connectionHandle) {
+    spd_close(connectionHandle);
+    connectionHandle = NULL;
+  }
+}
+
+typedef void (*SpeechdAction) (const void *data);
+
+static void
+speechdAction (SpeechdAction action, const void *data) {
+  if (connectionHandle) {
+    action(data);
+    if (!connectionHandle->stream) closeConnection();
+  }
+}
+
+static void
+setModule (const void *data) {
+  if (moduleName) spd_set_output_module(connectionHandle, moduleName);
+}
+
+static void
+setLanguage (const void *data) {
+  if (languageName) spd_set_language(connectionHandle, languageName);
+}
+
+static void
+setVoice (const void *data) {
+  if (voiceType != -1) spd_set_voice_type(connectionHandle, voiceType);
+}
+
+static void
+setRate (const void *data) {
+  spd_set_voice_rate(connectionHandle, relativeRate);
+}
+
+static void
+setVolume (const void *data) {
+  spd_set_volume(connectionHandle, relativeVolume);
+}
+
+static void
+setPunctuation (const void *data) {
+  if (punctuationVerbosity != -1) spd_set_punctuation(connectionHandle, punctuationVerbosity);
+}
+
+static void
+cancelSpeech (const void *data) {
+  spd_cancel(connectionHandle);
+}
+
+static int
+openConnection (void) {
+  if (!connectionHandle) {
+    if (!(connectionHandle = spd_open("brltty", "main", NULL, SPD_MODE_THREADED))) {
+      LogPrint(LOG_ERR, "speech dispatcher open failure");
+      return 0;
+    }
+
+    speechdAction(setModule, NULL);
+    speechdAction(setLanguage, NULL);
+    speechdAction(setVoice, NULL);
+    speechdAction(setRate, NULL);
+    speechdAction(setVolume, NULL);
+    speechdAction(setPunctuation, NULL);
+  }
+
+  return 1;
+}
 
 static int
 spk_construct (SpeechSynthesizer *spk, char **parameters) {
+  clearSettings();
+
   if (parameters[PARM_PORT] && *parameters[PARM_PORT]) {
     static const int minimumPort = 0X1;
     static const int maximumPort = 0XFFFF;
@@ -54,88 +144,107 @@ spk_construct (SpeechSynthesizer *spk, char **parameters) {
     }
   }
 
-  if ((connection = spd_open("brltty", "main", NULL, SPD_MODE_THREADED))) {
-    if (parameters[PARM_MODULE] && *parameters[PARM_MODULE]) {
-      spd_set_output_module(connection, parameters[PARM_MODULE]);
-    }
-
-    if (parameters[PARM_LANGUAGE] && *parameters[PARM_LANGUAGE]) {
-      spd_set_language(connection, parameters[PARM_LANGUAGE]);
-    }
-
-    if (parameters[PARM_VOICE] && *parameters[PARM_VOICE]) {
-      static const SPDVoiceType voices[] = {
-        SPD_MALE1, SPD_FEMALE1,
-        SPD_MALE2, SPD_FEMALE2,
-        SPD_MALE3, SPD_FEMALE3,
-        SPD_CHILD_MALE, SPD_CHILD_FEMALE
-      };
-      static const char *choices[] = {
-        "male1", "female1",
-        "male2", "female2",
-        "male3", "female3",
-        "child_male", "child_female",
-        NULL
-      };
-      unsigned int choice = 0;
-
-      if (validateChoice(&choice, parameters[PARM_VOICE], choices)) {
-        spd_set_voice_type(connection, voices[choice]);
-      } else {
-        LogPrint(LOG_WARNING, "%s: %s", "invalid voice type", parameters[PARM_VOICE]);
-      }
-    }
-
-    return 1;
-  } else {
-    LogPrint(LOG_ERR, "speech dispatcher open failure");
+  if (parameters[PARM_MODULE] && *parameters[PARM_MODULE]) {
+    moduleName = parameters[PARM_MODULE];
   }
 
-  return 0;
+  if (parameters[PARM_LANGUAGE] && *parameters[PARM_LANGUAGE]) {
+    languageName = parameters[PARM_LANGUAGE];
+  }
+
+  if (parameters[PARM_VOICE] && *parameters[PARM_VOICE]) {
+    static const SPDVoiceType voices[] = {
+      SPD_MALE1, SPD_FEMALE1,
+      SPD_MALE2, SPD_FEMALE2,
+      SPD_MALE3, SPD_FEMALE3,
+      SPD_CHILD_MALE, SPD_CHILD_FEMALE
+    };
+
+    static const char *choices[] = {
+      "male1", "female1",
+      "male2", "female2",
+      "male3", "female3",
+      "child_male", "child_female",
+      NULL
+    };
+
+    unsigned int choice = 0;
+
+    if (validateChoice(&choice, parameters[PARM_VOICE], choices)) {
+      voiceType = voices[choice];
+    } else {
+      LogPrint(LOG_WARNING, "%s: %s", "invalid voice type", parameters[PARM_VOICE]);
+    }
+  }
+
+  return openConnection();
 }
 
 static void
 spk_destruct (SpeechSynthesizer *spk) {
-  if (connection) {
-    spd_close(connection);
-    connection = NULL;
+  closeConnection();
+  clearSettings();
+}
+
+typedef struct {
+  const unsigned char *text;
+  size_t length;
+  size_t count;
+  SPDPriority priority;
+} SayData;
+
+static void
+sayText (const void *data) {
+  const SayData *say = data;
+
+  if (say->count == 1) {
+    spd_char(connectionHandle, say->priority, (const char *)say->text);
+  } else {
+    spd_sayf(connectionHandle, say->priority, "%.*s", say->length, say->text);
   }
 }
 
 static void
-spk_say (SpeechSynthesizer *spk, const unsigned char *buffer, size_t length, size_t count, const unsigned char *attributes) {
-  const SPDPriority priority = SPD_TEXT;
-  if (count == 1) {
-    spd_char(connection, priority, (const char *)buffer);
-  } else {
-    spd_sayf(connection, priority, "%.*s", length, buffer);
+spk_say (SpeechSynthesizer *spk, const unsigned char *text, size_t length, size_t count, const unsigned char *attributes) {
+  const SayData say = {
+    .text = text,
+    .length = length,
+    .count = count,
+    .priority = SPD_TEXT
+  };
+
+  speechdAction(sayText, &say);
+  if (!connectionHandle) {
+    if (openConnection()) {
+      speechdAction(sayText, &say);
+    }
   }
 }
 
 static void
 spk_mute (SpeechSynthesizer *spk) {
-  spd_cancel(connection);
+  speechdAction(cancelSpeech, NULL);
 }
 
 static void
 spk_rate (SpeechSynthesizer *spk, unsigned char setting) {
-  int value = getIntegerSpeechRate(setting, 100) - 100;
-  spd_set_voice_rate(connection, value);
-  LogPrint(LOG_DEBUG, "set rate: %u -> %d", setting, value);
+  relativeRate = getIntegerSpeechRate(setting, 100) - 100;
+  speechdAction(setRate, NULL);
+  LogPrint(LOG_DEBUG, "set rate: %u -> %d", setting, relativeRate);
 }
 
 static void
 spk_volume (SpeechSynthesizer *spk, unsigned char setting) {
-  int value = getIntegerSpeechVolume(setting, 100) - 100;
-  spd_set_volume(connection, value);
-  LogPrint(LOG_DEBUG, "set volume: %u -> %d", setting, value);
+  relativeVolume = getIntegerSpeechVolume(setting, 100) - 100;
+  speechdAction(setVolume, NULL);
+  LogPrint(LOG_DEBUG, "set volume: %u -> %d", setting, relativeVolume);
 }
 
 static void
 spk_punctuation (SpeechSynthesizer *spk, SpeechPunctuation setting) {
-  SPDPunctuation value = (setting <= SPK_PUNCTUATION_NONE)? SPD_PUNCT_NONE: 
+  punctuationVerbosity = (setting <= SPK_PUNCTUATION_NONE)? SPD_PUNCT_NONE: 
                          (setting >= SPK_PUNCTUATION_ALL)? SPD_PUNCT_ALL: 
                          SPD_PUNCT_SOME;
-  spd_set_punctuation(connection, value);
-  LogPrint(LOG_DEBUG, "set punctuation: %u -> %d", setting, value);
+  speechdAction(setPunctuation, NULL);
+  LogPrint(LOG_DEBUG, "set punctuation: %u -> %d", setting, punctuationVerbosity);
 }
