8741b2afa8508b5bca5727420cb366a04745a275
Dave Mielke <dave@mielke.cc>
2009-05-13 12:55:00 +0000

    Interpret the VarioPro status key byte correctly. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@4473 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/Braille/Baum/braille.c
+++ Drivers/Braille/Baum/braille.c
@@ -145,6 +145,7 @@ updateFunctionKeys (uint64_t keys, uint64_t mask, unsigned int shift, int *press
     mask <<= shift;
   }
 
+  keys &= mask;
   keys |= pressedKeys.functionKeys & ~mask;
   if (keys == pressedKeys.functionKeys) return 0;
 
@@ -1256,7 +1257,7 @@ handleBaumDataRegistersEvent (BrailleDisplay *brl, const BaumResponsePacket *pac
 
       case BAUM_MODULE_Status:
         if (packet->data.values.modular.data.registers.status.flags & BAUM_DRF_ButtonsChanged)
-          if (updateFunctionKeyByte(packet->data.values.modular.data.registers.status.buttons,
+          if (updateFunctionKeyByte(packet->data.values.modular.data.registers.status.buttons >> 4,
                                     BAUM_SHIFT_COMMAND, BAUM_WIDTH_COMMAND, keyPressed))
             return 1;
 
