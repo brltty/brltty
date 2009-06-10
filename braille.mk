d0783f42dfc28330e9c14cf267d9d2af66e21ad6
Dave Mielke <dave@mielke.cc>
2009-06-10 05:40:27 +0000

    A braille driver using key tables doesn't have help files. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@4542 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- braille.mk
+++ braille.mk
@@ -22,7 +22,7 @@ $(TXT2HLP)$X:
 
 braille-help::
 	$(INSTALL_DIRECTORY) $(BLD_TOP)$(DAT_DIR)
-	set -- *.hlp; \
+	set -- *.hlp && [ -f $$1 ] || exit 0 && \
 	for file; do $(INSTALL) $$file $(BLD_TOP)$(DAT_DIR)/brltty-$(DRIVER_CODE)-$$file; done
 
 braille-all:: braille-help
