7257c4962a9f7819e3a1d56d27d5df1ce4cbadd7
Dave Mielke <dave@mielke.cc>
2009-06-15 04:48:37 +0000

    Convert the Voyager driver to use key tables. (ml)
    git-svn-id: svn://mielke.cc/main/brltty@4584 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/Braille/Voyager/braille.c
+++ Drivers/Braille/Voyager/braille.c
@@ -21,7 +21,7 @@
  * Written by Stéphane Doyon  <s.doyon@videotron.ca>
  *
  * It is being tested on Voyager 44, should also support Voyager 70.
- * It is designed to be compiled in BRLTTY version 3.5.
+ * It is designed to be compiled in BRLTTY version 4.1.
  *
  * History:
  * 0.21, January 2005:
@@ -48,11 +48,6 @@
 #include <errno.h>
 
 #include "misc.h"
-
-typedef enum {
-  PARM_INPUTMODE
-} DriverParameter;
-#define BRLPARMS "inputmode"
 
 #define BRLSTAT ST_VoyagerStyle
 #define BRL_HAVE_FIRMNESS
@@ -508,7 +503,6 @@ static const InputOutputOperations usbOperations = {
 
 /* Global variables */
 static char firstRead; /* Flag to reinitialize brl_readCommand() function state. */
-static unsigned int inputMode;
 static TranslationTable outputTable;
 static unsigned char *currentCells = NULL; /* buffer to prepare new pattern */
 static unsigned char *previousCells = NULL; /* previous pattern displayed */
@@ -517,6 +511,64 @@ static unsigned char *previousCells = NULL; /* previous pattern displayed */
 static unsigned char cellCount;
 #define IS_TEXT_RANGE(key1,key2) (((key1) <= (key2)) && ((key2) < cellCount))
 #define IS_TEXT_KEY(key) IS_TEXT_RANGE((key), (key))
+
+typedef enum {
+  /* The top round keys behind the routing keys, numbered assuming they
+   * are to be used for braille input.
+   */
+  VO_KEY_Dot1 = 1,
+  VO_KEY_Dot2 = 2,
+  VO_KEY_Dot3 = 3,
+  VO_KEY_Dot4 = 4,
+  VO_KEY_Dot5 = 5,
+  VO_KEY_Dot6 = 6,
+  VO_KEY_Dot7 = 7,
+  VO_KEY_Dot8 = 8,
+
+  /* The front keys */
+  VO_KEY_A = 9,              /* Leftmost */
+  VO_KEY_B = 10,             /* Second from left */
+  VO_KEY_RoundLeft = 11,     /* Round key to the left of the central pad */
+  VO_KEY_Up = 12,            /* Up position of central pad */
+  VO_KEY_Down = 13,          /* Down position of central pad */
+  VO_KEY_RoundRight = 14,    /* Round key to the right of the central pad */
+  VO_KEY_C = 15,             /* Second from right */
+  VO_KEY_D = 16              /* Rightmost */
+} VO_NavigationKey;
+
+typedef enum {
+  VO_SET_NavigationKeys = 0,
+  VO_SET_RoutingKeys
+} VO_KeySet;
+
+static KEY_NAME_TABLE(keyNames_all) = {
+  KEY_SET_ENTRY(VO_SET_RoutingKeys, "RoutingKey"),
+
+  KEY_NAME_ENTRY(VO_KEY_Dot1, "Dot1"),
+  KEY_NAME_ENTRY(VO_KEY_Dot2, "Dot2"),
+  KEY_NAME_ENTRY(VO_KEY_Dot3, "Dot3"),
+  KEY_NAME_ENTRY(VO_KEY_Dot4, "Dot4"),
+  KEY_NAME_ENTRY(VO_KEY_Dot5, "Dot5"),
+  KEY_NAME_ENTRY(VO_KEY_Dot6, "Dot6"),
+  KEY_NAME_ENTRY(VO_KEY_Dot7, "Dot7"),
+  KEY_NAME_ENTRY(VO_KEY_Dot8, "Dot8"),
+
+  KEY_NAME_ENTRY(VO_KEY_A, "A"),
+  KEY_NAME_ENTRY(VO_KEY_B, "B"),
+  KEY_NAME_ENTRY(VO_KEY_RoundLeft, "RL"),
+  KEY_NAME_ENTRY(VO_KEY_Up, "Up"),
+  KEY_NAME_ENTRY(VO_KEY_Down, "Down"),
+  KEY_NAME_ENTRY(VO_KEY_RoundRight, "RR"),
+  KEY_NAME_ENTRY(VO_KEY_C, "C"),
+  KEY_NAME_ENTRY(VO_KEY_D, "D"),
+
+  LAST_KEY_NAME_ENTRY
+};
+
+static KEY_NAME_TABLE_LIST(keyNameTables_all) = {
+  keyNames_all,
+  NULL
+};
 
 static int
 brl_construct (BrailleDisplay *brl, char **parameters, const char *device) {
@@ -544,12 +596,10 @@ brl_construct (BrailleDisplay *brl, char **parameters, const char *device) {
         switch (count) {
           case 48:
             cellCount = 44;
-            brl->keyBindings = "44";
             break;
 
           case 72:
             cellCount = 70;
-            brl->keyBindings = "70";
             break;
 
           default:
@@ -572,6 +622,7 @@ brl_construct (BrailleDisplay *brl, char **parameters, const char *device) {
        */
       brl->textColumns = cellCount;		/* initialize size of display */
       brl->textRows = 1;		/* always 1 */
+      brl->keyNameTables = keyNameTables_all;
 
       if ((currentCells = malloc(cellCount))) {
         if ((previousCells = malloc(cellCount))) {
@@ -586,11 +637,6 @@ brl_construct (BrailleDisplay *brl, char **parameters, const char *device) {
               static const DotsTable dots = {0X01, 0X02, 0X04, 0X08, 0X10, 0X20, 0X40, 0X80};
               makeOutputTable(dots, outputTable);
             }
-
-            inputMode = 0;
-            if (*parameters[PARM_INPUTMODE])
-              if (!validateYesNo(&inputMode, parameters[PARM_INPUTMODE]))
-                LogPrint(LOG_WARNING, "%s: %s", "invalid input setting", parameters[PARM_INPUTMODE]);
 
             firstRead = 1;
             if (io->preparePort()) return 1;
@@ -668,80 +714,6 @@ brl_writeWindow (BrailleDisplay *brl, const wchar_t *text) {
   return 1;
 }
 
-
-/* Names and codes for display keys */
-
-/* The top round keys behind the routing keys, numbered assuming they
- * are to be used for braille input.
- */
-#define DOT1 0X01
-#define DOT2 0X02
-#define DOT3 0X04
-#define DOT4 0X08
-#define DOT5 0X10
-#define DOT6 0X20
-#define DOT7 0X40
-#define DOT8 0X80
-#define DOT_KEYS (DOT1 | DOT2 | DOT3 | DOT4 | DOT5 | DOT6 | DOT7 | DOT8)
-
-/* The front keys. Codes are shifted by 8 bits so they can be combined
- * with the codes for the top keys.
- */
-#define K_A    0X0100 /* Leftmost */
-#define K_B    0X0200 /* Second from the left */
-#define K_RL   0X0400 /* The round key to the left of the central pad */
-#define K_UP   0X0800 /* Up position of central pad */
-#define K_DOWN 0X1000 /* Down position of central pad */
-#define K_RR   0X2000 /* The round key to the right of the central pad */
-#define K_C    0X4000 /* Second from the right */
-#define K_D    0X8000 /* Rightmost */
-#define FRONT_KEYS (K_A | K_B | K_RL | K_UP | K_DOWN | K_RR | K_C | K_D)
-#define SPACE_BAR (K_B | K_C)
-
-/* OK what follows is pretty hairy. I got tired of individually maintaining
- * the sources and help files so here's my first attempt at "automatic"
- * generation of help files. This is my first shot at it, so be kind with
- * me.
- */
-
-/* These macros include an ordering hint for the help file and the help
- * text. GENHLP is not defined during compilation, so at compilation the
- * macros are expanded in a way that just drops the help-related
- * information.
- */
-#define KEY(keys, command) case keys: cmd = command; break;
-#ifdef GENHLP
-/* To generate the help files we do gcc -DGENHLP -E (and heavily post-process
- * the result). So these macros expand to something that is easily
- * searched/grepped for and "easily" post-processed.
- */
-#define HLP(where, keys, description) <HLP> where: keys : description </HLP>
-#else /* GENHLP */
-#define HLP(where, keys, description)
-#endif /* GENHLP */
-
-#define HKEY(where, keys, command, description) \
-  HLP(where, #keys, description) \
-  KEY(keys, command)
-
-#define PHKEY(where, prefix, keys, command, description) \
-  HLP(where, prefix "+" #keys, description) \
-  KEY(keys, command)
-
-#define CKEY(where, dots, command, description) \
-  HLP(where, "Chord-" #dots, description) \
-  KEY(dots, command)
-
-#define HKEY2(where, keys1, keys2, command1, command2, description) \
-  HLP(where, #keys1 "/" #keys2, description) \
-  KEY(keys1, command1); \
-  KEY(keys2, command2)
-
-#define PHKEY2(where, prefix, keys1, keys2, command1, command2, description) \
-  HLP(where, prefix "+ " #keys1 "/" #keys2, description) \
-  KEY(keys1, command1); \
-  KEY(keys2, command2)
-
 static int
 brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
   /* Structure to remember which keys are pressed */
@@ -755,19 +727,7 @@ brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
    * Updated immediately whenever a key is pressed.
    * Cleared after analysis whenever a key is released.
    */
-  static Keys activeKeys;
   static Keys pressedKeys;
-  /* For a key binding that triggers two commands */
-  static int pendingCommand;
-
-  /* Ordered list of pressed routing keys by number */
-  unsigned char routingKeys[MAXIMUM_CELLS];
-  /* number of entries in routingKeys */
-  int routingCount = 0;
-
-  /* recognized command */
-  int cmd = BRL_CMD_NOOP;
-  int keyPressed = 0;
 
   /* read buffer: packet[0] for DOT keys, packet[1] for keys A B C D UP DOWN
    * RL RR, packet[2]-packet[7] list pressed routing keys by number, maximum
@@ -779,15 +739,7 @@ brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
   if (firstRead) {
     /* initialize state */
     firstRead = 0;
-    memset(&activeKeys, 0, sizeof(activeKeys));
     memset(&pressedKeys, 0, sizeof(pressedKeys));
-    pendingCommand = EOF;
-  }
-
-  if (pendingCommand != EOF) {
-    cmd = pendingCommand;
-    pendingCommand = EOF;
-    return cmd;
   }
 
   {
@@ -825,7 +777,14 @@ brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
 
     /* We combine dot and front key info in keystate */
     keys.control = (packet[1] << 8) | packet[0];
-    if (keys.control & ~pressedKeys.control) keyPressed = 1;
+
+    for (i = VO_KEY_Dot1; i <= VO_KEY_D; i++) {
+      int mask = 1 << (i - 1);
+      if ((pressedKeys.control & mask) && !(keys.control & mask))
+	enqueueKeyEvent(VO_SET_NavigationKeys, i, 0);
+      if (!(pressedKeys.control & mask) && (keys.control & mask))
+	enqueueKeyEvent(VO_SET_NavigationKeys, i, 1);
+    }
     
     for (i=2; i<8; i++) {
       unsigned char key = packet[i];
@@ -838,220 +797,21 @@ brl_readCommand (BrailleDisplay *brl, BRL_DriverCommandContext context) {
       key--;
 
       keys.routing[key] = 1;
-      if (!pressedKeys.routing[key]) keyPressed = 1;
+      if (!pressedKeys.routing[key])
+	enqueueKeyEvent(VO_SET_RoutingKeys, key, 1);
+    }
+
+    {
+      int column;
+      for (column = 0; column < cellCount; column++)
+	if (!keys.routing[column] && pressedKeys.routing[column])
+	  enqueueKeyEvent(VO_SET_RoutingKeys, column, 0);
     }
 
     pressedKeys = keys;
-    if (keyPressed) activeKeys = keys;
   }
 
-  {
-    int key;
-    for (key=0; key<cellCount; key++)
-      if (activeKeys.routing[key])
-        routingKeys[routingCount++] = key;
-  }
-
-  if (routingCount == 0) {
-    /* No routing keys */
-
-    if (!(activeKeys.control & ~FRONT_KEYS)) {
-      /* Just front keys */
-
-      if (context == BRL_CTX_MENU) {
-	switch (activeKeys.control) {
-          HKEY2(891, K_DOWN, K_UP,
-                BRL_CMD_MENU_PREV_SETTING, BRL_CMD_MENU_NEXT_SETTING,
-                "Select previous/next setting");
-          HKEY(892, K_RL|K_RR, BRL_CMD_PREFMENU, "Exit menu");
-          HKEY(892, K_RL|K_UP, BRL_CMD_PREFLOAD, "Discard changes");
-          HKEY(892, K_RL|K_DOWN, BRL_CMD_PREFSAVE, "Save changes and exit menu");
-	}
-      }
-
-      if (cmd == BRL_CMD_NOOP) {
-	switch (activeKeys.control) {
-	  HKEY2(101, K_A, K_D,
-                BRL_CMD_FWINLT, BRL_CMD_FWINRT,
-                "Go backward/forward one window");
-	  HKEY2(501, K_RL|K_A, K_RL|K_D,
-                BRL_CMD_LNBEG, BRL_CMD_LNEND,
-                "Go to beginning/end of line");
-	  HKEY2(501, K_RR|K_A, K_RR|K_D,
-                BRL_CMD_CHRLT, BRL_CMD_CHRRT,
-                "Go left/right one character");
-	  HKEY2(501, K_UP|K_A, K_UP|K_D,
-                BRL_CMD_FWINLTSKIP, BRL_CMD_FWINRTSKIP,
-                "Go to previous/next non-blank window");
-	  HKEY2(501, K_DOWN|K_A, K_DOWN|K_D,
-                BRL_CMD_PRSEARCH, BRL_CMD_NXSEARCH,
-                "Search screen backward/forward for cut text");
-
-	  HKEY2(101, K_B, K_C,
-                BRL_CMD_LNUP, BRL_CMD_LNDN,
-                "Go up/down one line");
-	  HKEY2(101, K_A|K_B, K_A|K_C,
-                BRL_CMD_TOP_LEFT, BRL_CMD_BOT_LEFT,
-                "Go to top-left/bottom-left corner");
-	  HKEY2(101, K_D|K_B, K_D|K_C,
-                BRL_CMD_TOP, BRL_CMD_BOT,
-                "Go to top/bottom line");
-	  HKEY2(501, K_RL|K_B, K_RL|K_C,
-                BRL_CMD_ATTRUP, BRL_CMD_ATTRDN,
-		"Go to previous/next line with different highlighting");
-	  HKEY2(501, K_RR|K_B, K_RR|K_C,
-                BRL_CMD_PRDIFLN, BRL_CMD_NXDIFLN,
-                "Go to previous/next line with different content");
-	  HKEY2(501, K_UP|K_B, K_UP|K_C,
-                BRL_CMD_PRPGRPH, BRL_CMD_NXPGRPH,
-                "Go to previous/next paragraph (blank line separation)");
-	  HKEY2(501, K_DOWN|K_B, K_DOWN|K_C,
-                BRL_CMD_PRPROMPT, BRL_CMD_NXPROMPT,
-                "Go to previous/next prompt (same prompt as current line)");
-
-	  HKEY(101, K_RL, BRL_CMD_BACK, 
-               "Go back (undo unexpected cursor tracking motion)");
-	  HKEY(101, K_RR, BRL_CMD_HOME, "Go to cursor");
-	  HKEY(101, K_RL|K_RR, BRL_CMD_CSRTRK, "Cursor tracking (toggle)");
-
-	  HKEY2(101, K_UP, K_DOWN,
-                BRL_BLK_PASSKEY + BRL_KEY_CURSOR_UP,
-                BRL_BLK_PASSKEY + BRL_KEY_CURSOR_DOWN,
-                "Move cursor up/down (arrow keys)");
-	  HKEY(210, K_RL|K_UP, BRL_CMD_DISPMD, "Show attributes (toggle)");
-	  HKEY(210, K_RL|K_DOWN, BRL_CMD_SIXDOTS, "Six dots (toggle)");
-	  HKEY(210, K_RR|K_UP, BRL_CMD_AUTOREPEAT, "Autorepeat (toggle)");
-	  HKEY(210, K_RR|K_DOWN, BRL_CMD_AUTOSPEAK, "Autospeak (toggle)");
-
-	  HKEY(602, K_B|K_C, BRL_BLK_PASSDOTS+0, "Space bar")
-	  HKEY(302, K_A|K_D, BRL_CMD_CSRJMP_VERT,
-	       "Route cursor to current line");
-	}
-      }
-    } else if ((!inputMode || (activeKeys.control & SPACE_BAR)) &&
-               !(activeKeys.control & FRONT_KEYS & ~SPACE_BAR)) {
-      /* Dots combined with B or C or both but no other front keys.
-       * This is a chorded character typed in braille.
-       */
-      switch (activeKeys.control & DOT_KEYS) {
-        HLP(601, "Chord-1478", "Input mode (toggle)")
-        case DOT1|DOT4|DOT7|DOT8:
-          if (!keyPressed) cmd = BRL_CMD_NOOP | ((inputMode = !inputMode)? BRL_FLG_TOGGLE_ON: BRL_FLG_TOGGLE_OFF);
-          break;
-
-	CKEY(210, DOT1, BRL_CMD_ATTRVIS, "Attribute underlining (toggle)");
-	CKEY(610, DOT1|DOT2, BRL_BLK_PASSKEY + BRL_KEY_BACKSPACE, "Backspace key");
-	CKEY(210, DOT1|DOT4, BRL_CMD_CSRVIS, "Cursor visibility (toggle)");
-	CKEY(610, DOT1|DOT4|DOT5, BRL_BLK_PASSKEY + BRL_KEY_DELETE, "Delete key");
-	CKEY(610, DOT1|DOT5, BRL_BLK_PASSKEY + BRL_KEY_ESCAPE, "Escape key");
-	CKEY(210, DOT1|DOT2|DOT4, BRL_CMD_FREEZE, "Freeze screen (toggle)");
-	CKEY(201, DOT1|DOT2|DOT5, BRL_CMD_HELP, "Help screen (toggle)");
-	CKEY(610, DOT2|DOT4, BRL_BLK_PASSKEY + BRL_KEY_INSERT, "Insert key");
-	CKEY(201, DOT1|DOT2|DOT3, BRL_CMD_LEARN, "Learn mode (toggle)");
-	case DOT1|DOT2|DOT3|DOT4|DOT5|DOT6|DOT7|DOT8:
-	CKEY(205, DOT1|DOT3|DOT4, BRL_CMD_PREFMENU, "Preferences menu (toggle)");
-	CKEY(408, DOT1|DOT2|DOT3|DOT4, BRL_CMD_PASTE, "Paste cut text");
-	CKEY(206, DOT1|DOT2|DOT3|DOT5, BRL_CMD_PREFLOAD, "Reload preferences from disk");
-	CKEY(201, DOT2|DOT3|DOT4, BRL_CMD_INFO, "Status line (toggle)");
-	CKEY(610, DOT2|DOT3|DOT4|DOT5, BRL_BLK_PASSKEY + BRL_KEY_TAB, "Tab key");
-	CKEY(206, DOT2|DOT4|DOT5|DOT6, BRL_CMD_PREFSAVE, "Write preferences to disk");
-	CKEY(610, DOT4|DOT6, BRL_BLK_PASSKEY + BRL_KEY_ENTER, "Enter key");
-	CKEY(610, DOT2, BRL_BLK_PASSKEY+BRL_KEY_PAGE_UP, "Page up");
-	CKEY(610, DOT5, BRL_BLK_PASSKEY+BRL_KEY_PAGE_DOWN, "Page down");
-	CKEY(610, DOT3, BRL_BLK_PASSKEY+BRL_KEY_HOME, "Home key");
-	CKEY(610, DOT6, BRL_BLK_PASSKEY+BRL_KEY_END, "End key");
-	CKEY(610, DOT7, BRL_BLK_PASSKEY+BRL_KEY_CURSOR_LEFT, "Left arrow");
-	CKEY(610, DOT8, BRL_BLK_PASSKEY+BRL_KEY_CURSOR_RIGHT, "Right arrow");
-      }
-    } else if (!(activeKeys.control & ~DOT_KEYS)) {
-      /* Just dot keys */
-      /* This is a character typed in braille */
-      cmd = BRL_BLK_PASSDOTS;
-      if (activeKeys.control & DOT1) cmd |= BRL_DOT1;
-      if (activeKeys.control & DOT2) cmd |= BRL_DOT2;
-      if (activeKeys.control & DOT3) cmd |= BRL_DOT3;
-      if (activeKeys.control & DOT4) cmd |= BRL_DOT4;
-      if (activeKeys.control & DOT5) cmd |= BRL_DOT5;
-      if (activeKeys.control & DOT6) cmd |= BRL_DOT6;
-      if (activeKeys.control & DOT7) cmd |= BRL_DOT7;
-      if (activeKeys.control & DOT8) cmd |= BRL_DOT8;
-    }
-  } else if (!activeKeys.control) {
-    /* Just routing keys */
-    if (routingCount == 1) {
-      if (IS_TEXT_KEY(routingKeys[0])) {
-        HLP(301,"CRt#", "Route cursor to cell")
-        cmd = BRL_BLK_ROUTE + routingKeys[0];
-      }
-    } else if ((routingCount == 2) &&
-               IS_TEXT_RANGE(routingKeys[0], routingKeys[1])) {
-      HLP(405, "CRtx+CRty", "Cut text from x through y")
-      cmd = BRL_BLK_CUTBEGIN + routingKeys[0];
-      pendingCommand = BRL_BLK_CUTLINE + routingKeys[1];
-    }
-  } else if (activeKeys.control & (K_UP|K_RL|K_RR)) {
-    /* Some routing keys combined with UP RL or RR (actually any key
-     * combination that has at least one of those).
-     * Treated special because we use absolute routing key numbers
-     * (counting the status cell keys).
-     */
-    if (routingCount == 1) {
-      switch (activeKeys.control) {
-        case K_UP:
-          HLP(692, "UP+ CRa<CELLS-1>/CRa<CELLS>",
-              "Switch to previous/next virtual console")
-          if (routingKeys[0] == cellCount-1) {
-            cmd = BRL_CMD_SWITCHVT_NEXT;
-          } else if (routingKeys[0] == cellCount-2) {
-            cmd = BRL_CMD_SWITCHVT_PREV;
-          } else {
-            HLP(691, "UP+CRa#", "Switch to virtual console #")
-            cmd = BRL_BLK_SWITCHVT + routingKeys[0];
-          }
-          break;
-
-        PHKEY(501,"CRa#", K_RL,
-              BRL_BLK_SETMARK + routingKeys[0],
-              "Remember current position as mark #");
-        PHKEY(501,"CRa#", K_RR,
-              BRL_BLK_GOTOMARK + routingKeys[0],
-              "Go to mark #");
-      }
-    }
-  } else if ((routingCount == 1) && IS_TEXT_KEY(routingKeys[0])) {
-    /* One text routing key with some other keys */
-    switch (activeKeys.control) {
-      PHKEY(501, "CRt#", K_DOWN,
-            BRL_BLK_SETLEFT + routingKeys[0],
-            "Go right # cells");
-      PHKEY(401, "CRt#", K_A,
-            BRL_BLK_CUTBEGIN + routingKeys[0],
-            "Mark beginning of region to cut");
-      PHKEY(401, "CRt#", K_B,
-            BRL_BLK_CUTAPPEND + routingKeys[0],
-            "Mark beginning of cut region for append");
-      PHKEY(401, "CRt#", K_D,
-            BRL_BLK_CUTRECT + routingKeys[0],
-            "Mark bottom-right of rectangular region and cut");
-      PHKEY(401, "CRt#", K_C,
-            BRL_BLK_CUTLINE + routingKeys[0],
-            "Mark end of linear region and cut");
-      PHKEY2(501, "CRt#", K_A|K_B, K_D|K_C,
-             BRL_BLK_PRINDENT + routingKeys[0],
-             BRL_BLK_NXINDENT + routingKeys[0],
-             "Go to previous/next line indented no more than #");
-    }
-  }
-
-  if (keyPressed) {
-    /* key was pressed, start the autorepeat delay */
-    cmd |= BRL_FLG_REPEAT_DELAY;
-  } else {
-    /* key was released, clear state */
-    memset(&activeKeys, 0, sizeof(activeKeys));
-  }
-
-  return cmd;
+  return EOF;
 }
 
 /* Voltage: from 0->300V to 255->200V.
