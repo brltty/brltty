e158470ea0ed9a32d240840f929fd586eeaa272e
Dave Mielke <dave@mielke.cc>
2010-03-27 07:27:47 +0000

    Implement more of the Windows Bluetooth support. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@5087 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/bluetooth_windows.c
+++ Programs/bluetooth_windows.c
@@ -26,6 +26,7 @@
 #include "io_bluetooth.h"
 #include "bluetooth_internal.h"
 #include "log.h"
+#include "misc.h"
 
 struct BluetoothConnectionExtensionStruct {
   SOCKET socket;
@@ -33,21 +34,90 @@ struct BluetoothConnectionExtensionStruct {
   SOCKADDR_BTH remote;
 };
 
+static void
+bthSocketError (const char *action) {
+  DWORD error = WSAGetLastError();
+  LogWindowsCodeError(error, action);
+  setErrno(error);
+}
+
 BluetoothConnectionExtension *
 bthConnect (const BluetoothDeviceAddress *bda, unsigned char channel) {
-  errno = ENOSYS;
-  LogError("Bluetooth connect");
+  BluetoothConnectionExtension *bcx;
+
+  if ((bcx = malloc(sizeof(*bcx)))) {
+    memset(bcx, 0, sizeof(*bcx));
+
+    if ((bcx->socket = socket(PF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM)) != -1) {
+      bcx->local.addressFamily = AF_BTH;
+      bcx->local.btAddr = BTH_ADDR_NULL;
+      bcx->local.port = 0;
+
+      if (bind(bcx->socket, (SOCKADDR *)&bcx->local, sizeof(bcx->local)) != -1) {
+        bcx->remote.addressFamily = AF_BTH;
+        memcpy(&bcx->remote.btAddr, bda->bytes, sizeof(bda->bytes));
+        bcx->remote.port = channel;
+
+        if (connect(bcx->socket, (SOCKADDR *)&bcx->remote, sizeof(bcx->remote)) != -1) {
+          return bcx;
+        } else {
+          DWORD error = WSAGetLastError();
+
+          if ((error != ERROR_HOST_DOWN) && (error != ERROR_HOST_UNREACHABLE)) {
+            LogWindowsCodeError(error, "RFCOMM connect");
+          }
+
+          setErrno(error);
+        }
+      } else {
+        bthSocketError("RFCOMM bind");
+      }
+
+      closesocket(bcx->socket);
+    } else {
+      bthSocketError("RFCOMM socket");
+    }
+
+    free(bcx);
+  } else {
+    LogError("malloc");
+  }
+
   return NULL;
 }
 
 void
 bthDisconnect (BluetoothConnectionExtension *bcx) {
+  closesocket(bcx->socket);
+  free(bcx);
 }
 
 int
 bthAwaitInput (BluetoothConnection *connection, int milliseconds) {
-  errno = ENOSYS;
-  LogError("Bluetooth wait");
+  BluetoothConnectionExtension *bcx = connection->extension;
+  fd_set input;
+  struct timeval timeout;
+
+  FD_ZERO(&input);
+  FD_SET(bcx->socket, &input);
+
+  memset(&timeout, 0, sizeof(timeout));
+  timeout.tv_sec = milliseconds / 1000;
+  timeout.tv_usec = (milliseconds % 1000) * 1000;
+
+  switch (select(1, &input, NULL, NULL, &timeout)) {
+    case -1:
+      bthSocketError("RFCOMM wait");
+      break;
+
+    case 0:
+      errno = EAGAIN;
+      break;
+
+    default:
+      return 1;
+  }
+
   return 0;
 }
 
@@ -63,7 +133,10 @@ bthReadData (
 
 ssize_t
 bthWriteData (BluetoothConnection *connection, const void *buffer, size_t size) {
-  errno = ENOSYS;
-  LogError("Bluetooth write");
+  BluetoothConnectionExtension *bcx = connection->extension;
+  DWORD result;
+
+  if (WriteFile(bcx->socket, buffer, size, &result, NULL)) return result;
+  bthSocketError("RFCOMM write");
   return -1;
 }
