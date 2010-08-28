11750ab641e1d6e5176eabb9c330af734682fb9c
Dave Mielke <dave@mielke.cc>
2010-08-28 15:25:25 +0000

    Merge the VideoBraille braille driver source into a single file. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@5334 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/Braille/VideoBraille/braille.c
+++ Drivers/Braille/VideoBraille/braille.c
@@ -25,13 +25,100 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "log.h"
 #include "timing.h"
+#include "system.h"
 
 #include "brl_driver.h"
 #include "braille.h"
-#include "vblow.h"
 
 static unsigned char lastbuff[40];
+
+#define LPTSTATUSPORT LPTPORT+1
+#define LPTCONTROLPORT LPTPORT+2
+  
+static TranslationTable outputTable;
+
+static void vbclockpause() {
+  int i;
+  for (i = 0; i<=VBCLOCK*100; i++) ;
+}
+
+static void vbdisplay(unsigned char *vbBuf) {
+  int i,j;
+  char b;
+  for (j = 0; j<VBSIZE; j++) {
+    for (i = 7; i>=0; i--) {
+      b = (vbBuf[j] << i) & VBLPTDATA;
+      writePort1(LPTPORT, b);
+      vbclockpause();
+      writePort1(LPTPORT, b | VBLPTCLOCK);
+      vbclockpause();
+    }
+  }
+  writePort1(LPTPORT, b | VBLPTCLOCK);
+  for (i = 0; i<=7; i++) vbclockpause();
+  writePort1(LPTPORT, 0);
+  for (i = 0; i<=7; i++) vbclockpause();
+  writePort1(LPTPORT, VBLPTSTROBE);
+  for (i = 0; i<=7; i++) vbclockpause();
+  writePort1(LPTPORT, 0);
+  vbclockpause();
+}
+
+static int vbinit() {
+  if (enablePorts(LOG_ERR, LPTPORT, 3)) {
+    if (enablePorts(LOG_ERR, 0X80, 1)) {
+      makeTranslationTable(dotsTable_ISO11548_1, outputTable);
+
+      {
+        unsigned char alldots[40];
+        memset(alldots, 0XFF, 40);
+        vbdisplay(alldots);
+      }
+
+      return 0;
+    }
+    disablePorts(LPTPORT, 3);
+  }
+
+  logMessage(LOG_ERR, "Error: must be superuser");
+  return -1;
+}
+
+void vbsleep(long x) {
+  int i;
+  for (i = 0; i<x; i++) writePort1(0x80, 1);
+}
+
+static void vbtranslate(const unsigned char *vbBuf,unsigned char *vbDest,int size) {
+  int i;
+  for (i=0; i<size; i++) vbDest[i] = outputTable[vbBuf[i]];
+}
+
+static void BrButtons(vbButtons *dest) {
+  char i;
+  dest->bigbuttons = 0;
+  dest->keypressed = 0;
+  for (i = 47; i>=40; i--) {
+    writePort1(LPTPORT, i);
+    vbsleep(VBDELAY);
+    if ((readPort1(LPTSTATUSPORT) & 0x08)==0) {
+      dest->bigbuttons |= (1 << (i-40));
+      dest->keypressed = 1;
+    }
+  }
+  dest->routingkey = 0;
+  for (i = 40; i>0; i--) {
+    writePort1(LPTPORT, i-1);
+    vbsleep(VBDELAY);
+    if ((readPort1(LPTSTATUSPORT) & 0x08)==0) {
+      dest->routingkey = i;
+      dest->keypressed = 1;
+      break;
+    }
+  }
+}
 
 static int brl_construct(BrailleDisplay *brl, char **parameters, const char *dev) {
   /*	Seems to signal en error */ 
--- Drivers/Braille/VideoBraille/braille.h
+++ Drivers/Braille/VideoBraille/braille.h
@@ -26,3 +26,19 @@
 #define VBLPTSTROBE 0x40
 #define VBLPTCLOCK 0x20
 #define VBLPTDATA 0x80
+
+#define KEY_UP 0x1
+#define KEY_LEFT 0x2
+#define KEY_RIGHT 0x4
+#define KEY_DOWN 0x8
+#define KEY_ATTRIBUTES 0x10
+#define KEY_CURSOR 0x20
+#define KEY_HOME 0x40
+#define KEY_MENU 0x80
+
+typedef struct {
+  unsigned char bigbuttons;
+  char routingkey : 7;
+  char keypressed : 1;
+} vbButtons;
+
