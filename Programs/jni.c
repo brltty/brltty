f21bfc4e256be7e2aaa8f0d17d47d366ead5fafb
Dave Mielke <dave@mielke.cc>
2012-05-11 07:28:03 +0000

    Finish (but one known bug left) the code to run brltty from Java. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@6417 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/jni.c
+++ Programs/jni.c
@@ -23,19 +23,58 @@
 
 #include "embed.h"
 
+static jobject jProgramArguments = NULL;
+static const char **cProgramArguments = NULL;
+
 JNIEXPORT jint JNICALL
-Java_org_a11y_BRLTTY_Native_construct (JNIEnv *env, jobject arguments) {
-  jint length = (*env)->GetArrayLength(env, arguments);
-  printf("length=%d\n", length);
-  return 2;
+Java_brltty_construct (JNIEnv *env, jobject object, jobject argumentArray) {
+  jint argumentCount = (*env)->GetArrayLength(env, argumentArray);
+
+  jProgramArguments = argumentArray;
+  cProgramArguments = malloc((argumentCount + 2) * sizeof(*cProgramArguments));
+
+  cProgramArguments[0] = PACKAGE_NAME;
+  cProgramArguments[argumentCount+1] = NULL;
+
+  {
+    unsigned int i;
+
+    for (i=0; i<argumentCount; i+=1) {
+      jstring jArgument = (*env)->GetObjectArrayElement(env, argumentArray, i);
+      jboolean isCopy;
+      cProgramArguments[i+1] = (*env)->GetStringUTFChars(env, jArgument, &isCopy);
+    }
+  }
+
+  return brlttyConstruct(argumentCount+1, cProgramArguments);
 }
 
 JNIEXPORT jboolean JNICALL
-Java_org_a11y_BRLTTY_Native_update (JNIEnv *env, jobject this) {
+Java_brltty_update (JNIEnv *env, jobject object) {
   return brlttyUpdate()? JNI_TRUE: JNI_FALSE;
 }
 
 JNIEXPORT void JNICALL
-Java_org_a11y_BRLTTY_Native_destruct (JNIEnv *env, jobject this) {
+Java_brltty_destruct (JNIEnv *env, jobject object) {
   brlttyDestruct();
+
+  if (jProgramArguments) {
+    if (cProgramArguments) {
+      unsigned int i = 0;
+
+printf("length=%d\n", (*env)->GetArrayLength(env, jProgramArguments)); fflush(stdout);
+      while (cProgramArguments[++i]) {
+printf("arg=%d\n", i); fflush(stdout);
+        jstring jArgument = (*env)->GetObjectArrayElement(env, jProgramArguments, i-1);
+printf("releasing\n"); fflush(stdout);
+        (*env)->ReleaseStringUTFChars(env, jArgument, cProgramArguments[i]);
+printf("released\n"); fflush(stdout);
+      }
+
+      free(cProgramArguments);
+      cProgramArguments = NULL;
+    }
+
+    jProgramArguments = NULL;
+  }
 }
