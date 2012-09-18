d33d7db301aac5a90167cb91f272305afd69871a
Dave Mielke <dave@mielke.cc>
2012-09-18 15:56:01 +0000

    Fix up the Android make files. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@6661 91a5dbb7-01b9-0310-9b5f-b28072856b6e

---
+++ Android/common.mk
@@ -0,0 +1,27 @@
+ANDROID_ASSETS_DIRECTORY = assets
+ANDROID_BINARIES_DIRECTORY = bin
+ANDROID_LIBRARIES_DIRECTORY = libs
+
+ANDROID_PACKAGE_FILE = $(ANDROID_BINARIES_DIRECTORY)/$(ANDROID_PACKAGE_NAME)-debug.apk
+
+ANDROID_LOCAL_FILES = local.properties
+
+all:: $(ANDROID_LOCAL_FILES)
+	ant debug
+
+$(ANDROID_LOCAL_FILES):
+	android update project --path .
+
+clean::
+	-rm -f $(ANDROID_LOCAL_FILES)
+	-rm -f -r $(ANDROID_ASSETS_DIRECTORY)
+	-rm -f -r $(ANDROID_BINARIES_DIRECTORY)
+	-rm -f -r $(ANDROID_LIBRARIES_DIRECTORY)
+	-rm -f -r gen
+
+install: all
+	adb install $(ANDROID_PACKAGE_FILE)
+
+uninstall:
+	adb uninstall $(ANDROID_PACKAGE_PATH)
+
