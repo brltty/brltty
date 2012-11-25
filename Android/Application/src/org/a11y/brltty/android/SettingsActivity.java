be3c73febbb6d278d5bd3c8532e6da7f08a6896d
Dave Mielke <dave@mielke.cc>
2012-11-25 06:37:09 +0000

    Add the beginnings of a braille device manager for Android. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@6766 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Android/Application/src/org/a11y/brltty/android/SettingsActivity.java
+++ Android/Application/src/org/a11y/brltty/android/SettingsActivity.java
@@ -25,11 +25,14 @@ import android.util.Log;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
+import android.preference.Preference;
+import android.preference.ListPreference;
 
 import android.content.Intent;
-import android.view.MenuItem;
+
+import android.bluetooth.*;
+import android.hardware.usb.*;
 
 public class SettingsActivity extends PreferenceActivity {
   private final String LOG_TAG = this.getClass().getName();
@@ -37,8 +40,6 @@ public class SettingsActivity extends PreferenceActivity {
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    getActionBar().setDisplayHomeAsUpEnabled(true);
   }
 
   @Override
@@ -46,33 +47,22 @@ public class SettingsActivity extends PreferenceActivity {
     loadHeadersFromResource(R.xml.settings_headers, target);
   }
 
-  @Override
-  public boolean onOptionsItemSelected (MenuItem item) {
-    switch (item.getItemId()) {
-      case android.R.id.home: {
-        FragmentManager fm = getFragmentManager();
-
-        if (fm.getBackStackEntryCount() > 0) {
-          fm.popBackStack();
-        } else {
-          Intent intent = new Intent(this, MainActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-          startActivity(intent);
-        }
-
-        return true;
-      }
-
-      default: {
-        return super.onOptionsItemSelected(item);
-      }
-    }
-  }
-
   public static abstract class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+    }
+
+    protected PreferenceActivity getPreferenceActivity () {
+      return (PreferenceActivity)getActivity();
+    }
+
+    protected Preference getPreference (String key) {
+      return getPreferenceActivity().findPreference(key);
+    }
+
+    protected ListPreference getListPreference (String key) {
+      return (ListPreference)getPreference(key);
     }
   }
 
@@ -85,12 +75,65 @@ public class SettingsActivity extends PreferenceActivity {
     }
   }
 
-  public static final class DeviceSettings extends SettingsFragment {
+  public static final class DeviceManager extends SettingsFragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
       addPreferencesFromResource(R.xml.settings_device);
+    }
+  }
+
+  public static final class DeviceFinder extends SettingsFragment {
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+    }
+
+    public static abstract class DeviceList {
+      private final List deviceList;
+
+      protected abstract List makeDeviceList ();
+
+      protected DeviceList () {
+        deviceList = makeDeviceList();
+      }
+    }
+
+    public static class BluetoothDeviceList extends DeviceList {
+      @Override
+      protected List makeDeviceList () {
+        return null;
+      }
+    }
+
+    public static class UsbDeviceList extends DeviceList {
+      @Override
+      protected List makeDeviceList () {
+        return null;
+      }
+    }
+
+    public static class SerialDeviceList extends DeviceList {
+      @Override
+      protected List makeDeviceList () {
+        return null;
+      }
+    }
+
+    private DeviceList deviceList = null;
+
+    private void refreshDeviceList () {
+      ListPreference preference = getListPreference("device-type");
+      Log.i("REFDEV", "pref: " + preference);
+      String value = preference.getValue();
+      Log.i("REFDEV", "val: " + value);
+      String className = getClass() + "." + value + "DeviceList";
+    }
+
+    @Override
+    public void onResume () {
+      refreshDeviceList();
     }
   }
 
