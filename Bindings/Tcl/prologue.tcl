f181030b08da02a37ffc69e93c44ce9d176e35d3
Dave Mielke <dave@mielke.cc>
2013-01-04 01:18:15 +0000

    Update the copyright lines to 2013 (from 2012). (dm)
    git-svn-id: svn://mielke.cc/main/brltty@6821 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Bindings/Tcl/prologue.tcl
+++ Bindings/Tcl/prologue.tcl
@@ -1,7 +1,7 @@
 ###############################################################################
 # libbrlapi - A library providing access to braille terminals for applications.
 #
-# Copyright (C) 2006-2012 by Dave Mielke <dave@mielke.cc>
+# Copyright (C) 2006-2013 by Dave Mielke <dave@mielke.cc>
 #
 # libbrlapi comes with ABSOLUTELY NO WARRANTY.
 #
