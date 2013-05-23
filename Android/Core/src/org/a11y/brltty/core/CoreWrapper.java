ebdc856242de39fcc0c4470bf43495926283d6ea
Dave Mielke <dave@mielke.cc>
2013-05-23 02:25:45 +0000

    Add methods for changing the text,attributes,contraction tables via the core thread. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@7125 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Android/Core/src/org/a11y/brltty/core/CoreWrapper.java
+++ Android/Core/src/org/a11y/brltty/core/CoreWrapper.java
@@ -18,10 +18,29 @@
 
 package org.a11y.brltty.core;
 
+import java.util.AbstractQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class CoreWrapper {
   public static native int construct (String[] arguments);
   public static native boolean update ();
   public static native void destruct ();
+
+  public static native boolean changeLogLevel (String logLevel);
+  public static native boolean changeTextTable (String textTable);
+  public static native boolean changeAttributesTable (String attributesTable);
+  public static native boolean changeContractionTable (String contractionTable);
+
+  private static final AbstractQueue<Runnable> runQueue = new LinkedBlockingDeque<Runnable>();
+
+  public static boolean runOnCoreThread (Runnable runnable) {
+    return runQueue.offer(runnable);
+  }
+
+  public static void processRunQueue () {
+    Runnable runnable;
+    while ((runnable = runQueue.poll()) != null) runnable.run();
+  }
 
   private static volatile boolean stop = false;
 
@@ -34,9 +53,8 @@ public class CoreWrapper {
 
     if (exitStatus == ProgramExitStatus.SUCCESS.value) {
       while (update()) {
-        if (stop) {
-          break;
-        }
+        if (stop) break;
+        processRunQueue();
       }
     } else if (exitStatus == ProgramExitStatus.FORCE.value) {
       exitStatus = ProgramExitStatus.SUCCESS.value;
