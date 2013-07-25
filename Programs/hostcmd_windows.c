0ac8c67ed810fd88a6e6de217c5aea78a85fb96e
Dave Mielke <dave@mielke.cc>
2013-07-25 12:07:57 +0000

    Add a basic implementation for runHostCommand() on Windows. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@7364 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/hostcmd_windows.c
+++ Programs/hostcmd_windows.c
@@ -18,6 +18,8 @@
 
 #include "prologue.h"
 
+#include "log.h"
+#include "sys_windows.h"
 #include "hostcmd_windows.h"
 #include "hostcmd_internal.h"
 
@@ -46,5 +48,45 @@ runCommand (
   HostCommandStream *streams,
   int asynchronous
 ) {
-  return 0;
+  int ok = 0;
+  char *line = makeWindowsCommandLine(command);
+
+  *result = 0XFF;
+
+  if (line) {
+    STARTUPINFO startup;
+    PROCESS_INFORMATION process;
+
+    memset(&startup, 0, sizeof(startup));
+    startup.cb = sizeof(startup);
+
+    memset(&process, 0, sizeof(process));
+
+    logMessage(LOG_DEBUG, "host command: %s", line);
+    if (CreateProcess(NULL, line, NULL, NULL, TRUE,
+                      CREATE_NEW_PROCESS_GROUP,
+                      NULL, NULL, &startup, &process)) {
+      DWORD status;
+      while ((status = WaitForSingleObject(process.hProcess, INFINITE)) == WAIT_TIMEOUT);
+      if (status == WAIT_OBJECT_0) {
+        DWORD code;
+        if (GetExitCodeProcess(process.hProcess, &code)) {
+          *result = code;
+        } else {
+          logWindowsSystemError("GetExitCodeProcess");
+        }
+      } else {
+        logWindowsSystemError("WaitForSingleObject");
+      }
+
+      CloseHandle(process.hProcess);
+      CloseHandle(process.hThread);
+    } else {
+      logWindowsSystemError("CreateProcess");
+    }
+
+    free(line);
+  }
+
+  return ok;
 }
