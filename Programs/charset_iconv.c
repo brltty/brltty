f2c734142b847f7a4c1ac36d763ec6057619276c
Dave Mielke <dave@mielke.cc>
2013-09-14 08:30:48 +0000

    Close the iconv handles on program exit. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@7742 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Programs/charset_iconv.c
+++ Programs/charset_iconv.c
@@ -30,6 +30,7 @@
 
 #include "log.h"
 #include "charset_internal.h"
+#include "program.h"
 
 #define CHARSET_ICONV_NULL ((iconv_t)-1)
 #define CHARSET_ICONV_HANDLE(name) iconv_t iconv##name = CHARSET_ICONV_NULL
@@ -66,50 +67,81 @@ getLocaleCharset (void) {
   return defaultCharset;
 }
 
+static void
+exitCharsetIconv (void *datga) {
+  static iconv_t *const handles[] = {
+    &iconvCharToWchar,
+    &iconvWcharToChar
+  };
+
+  iconv_t *const *handle = handles;
+  iconv_t *const *const end = handle + ARRAY_COUNT(handles);
+
+  while (handle < end) {
+    if (**handle != CHARSET_ICONV_NULL) {
+      iconv_close(**handle);
+      **handle = CHARSET_ICONV_NULL;
+    }
+
+    handle += 1;
+  }
+}
+
 int
 registerCharacterSet (const char *charset) {
+  int firstTime = 0;
   const char *const wcharCharset = getWcharCharset();
 
   typedef struct {
-    iconv_t *handle;
-    const char *fromCharset;
-    const char *toCharset;
-    unsigned permanent:1;
+    iconv_t *const handle;
+    const char *const fromCharset;
+    const char *const toCharset;
+
     iconv_t newHandle;
   } ConvEntry;
 
   ConvEntry convTable[] = {
-    {&iconvCharToWchar, charset, wcharCharset, 0, CHARSET_ICONV_NULL},
-    {&iconvWcharToChar, wcharCharset, charset, 0, CHARSET_ICONV_NULL},
-    {NULL, NULL, NULL, 1, CHARSET_ICONV_NULL}
-  };
-  ConvEntry *conv = convTable;
+    { .handle = &iconvCharToWchar,
+      .fromCharset = charset,
+      .toCharset = wcharCharset
+    },
 
-  while (conv->handle) {
-    if (conv->permanent && (*conv->handle != CHARSET_ICONV_NULL)) {
-      conv->newHandle = CHARSET_ICONV_NULL;
-    } else if ((conv->newHandle = iconv_open(conv->toCharset, conv->fromCharset)) == CHARSET_ICONV_NULL) {
+    { .handle = &iconvWcharToChar,
+      .fromCharset = wcharCharset,
+      .toCharset = charset
+    },
+  };
+
+  ConvEntry *conv = convTable;
+  const ConvEntry *convEnd = conv + ARRAY_COUNT(convTable);
+
+  while (conv < convEnd) {
+    if ((conv->newHandle = iconv_open(conv->toCharset, conv->fromCharset)) == CHARSET_ICONV_NULL) {
       logSystemError("iconv_open");
 
-      while (conv != convTable)
-        if ((--conv)->newHandle != CHARSET_ICONV_NULL)
-          iconv_close(conv->newHandle);
+      while (conv > convTable) {
+        conv -= 1;
+        iconv_close(conv->newHandle);
+      }
 
       return 0;
-    } else if (conv->permanent) {
-      *conv->handle = conv->newHandle;
-      conv->newHandle = CHARSET_ICONV_NULL;
     }
 
     conv += 1;
   }
 
-  while (conv != convTable) {
-    if ((--conv)->newHandle != CHARSET_ICONV_NULL) {
-      if (*conv->handle != CHARSET_ICONV_NULL) iconv_close(*conv->handle);
-      *conv->handle = conv->newHandle;
+  while (conv > convTable) {
+    conv -= 1;
+
+    if (*conv->handle == CHARSET_ICONV_NULL) {
+      firstTime = 1;
+    } else {
+      iconv_close(*conv->handle);
     }
+
+    *conv->handle = conv->newHandle;
   }
 
+  if (firstTime) onProgramExit("charset-iconv", exitCharsetIconv, NULL);
   return 1;
 }
