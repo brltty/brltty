ccaaee31cf0ed76424d5076fcae09da6367884ff
Dave Mielke <dave@mielke.cc>
2013-12-26 05:51:42 +0000

    Changes to the status key bindings for Papenmeier models with 22 status cells. (dm)
    git-svn-id: svn://mielke.cc/main/brltty@8313 91a5dbb7-01b9-0310-9b5f-b28072856b6e

--- Drivers/Braille/Papenmeier/models.h
+++ Drivers/Braille/Papenmeier/models.h
@@ -91,14 +91,14 @@ PM_BEGIN_STATUS(22)
   OFFS_EMPTY,
   OFFS_FLAG  + gscShowCursor,
   OFFS_FLAG  + gscShowAttributes,
-  OFFS_FLAG  + gscBlinkingCapitals,
-  OFFS_FLAG  + gscTextStyle,
   OFFS_FLAG  + gscSkipIdenticalLines,
-  OFFS_FLAG  + gscAlertTunes,
+  OFFS_FLAG  + gscTextStyle,
   OFFS_EMPTY,
   OFFS_EMPTY,
   OFFS_FLAG  + gscAutospeak,
   OFFS_FLAG  + gscAutorepeat,
+  OFFS_EMPTY,
+  OFFS_EMPTY,
   OFFS_EMPTY
 PM_END_STATUS
 
