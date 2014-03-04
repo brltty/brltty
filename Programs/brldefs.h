3931f9cc0197608626e5e595fa16a48f65a8eca8
Dave Mielke <dave@mielke.cc>
2014-03-03 20:10:04 -0500

    Restore BrlAPI's old api.h and brldefs.h in /usr/include/brltty/. (dm)

---
+++ Programs/brldefs.h
@@ -0,0 +1,33 @@
+/*
+ * BRLTTY - A background process providing access to the console screen (when in
+ *          text mode) for a blind person using a refreshable braille display.
+ *
+ * Copyright (C) 1995-2014 by The BRLTTY Developers.
+ *
+ * BRLTTY comes with ABSOLUTELY NO WARRANTY.
+ *
+ * This is free software, placed under the terms of the
+ * GNU Lesser General Public License, as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any
+ * later version. Please see the file LICENSE-LGPL for details.
+ *
+ * Web Page: http://mielke.cc/brltty/
+ *
+ * This software is maintained by Dave Mielke <dave@mielke.cc>.
+ */
+
+#ifndef BRLAPI_INCLUDED_BRLDEFS
+#define BRLAPI_INCLUDED_BRLDEFS
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#warning include <brlapi_brldefs.h> instead of <brltty/brldefs.h> (see man brlapi_deprecated)
+#include <brlapi_brldefs.h>
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* BRLAPI_INCLUDED_BRLDEFS */
