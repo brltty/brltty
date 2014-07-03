32e3773131d13adc7e738c4d74c90b3b96eda62e
Dave Mielke <dave@mielke.cc>
2014-07-03 09:57:36 -0400

    Make the Python bindings tester keep reading keys until a timeout. (dm)

--- Bindings/Python/apitest.py
+++ Bindings/Python/apitest.py
@@ -20,14 +20,14 @@
 import sys
 import sysconfig
 import os
+import time
 import ctypes
 
 def getProgramName ():
   return os.path.basename(sys.argv[0])
 
-def writeError (message):
-  file = sys.stderr
-  file.write((getProgramName() + ": " + message + "\n"))
+def logMessage (message, file=sys.stderr, label=getProgramName()):
+  file.write((label + ": " + message + "\n"))
   file.flush()
 
 def getBuildDirectory (directory):
@@ -59,43 +59,80 @@ def loadLibrary (directory, name):
   return ctypes.CDLL(os.path.join(directory, ("lib" + name + ".so")))
 
 sys.path.insert(0, getBuildDirectory("lib"))
-brlapiHandle = loadLibrary(getLibraryDirectory(), "brlapi")
+library = loadLibrary(getLibraryDirectory(), "brlapi")
+import brlapi
+
+def readKey (connection, timeout=0, interval=0.1, target=None, args=(), kwargs={}):
+  start = time.time()
+
+  while True:
+    key = connection.readKey(wait=0)
+
+    if key != None:
+      return key
+
+    if (time.time() - start) >= timeout:
+      break
+
+    time.sleep(interval)
+
+  if target != None:
+    return target(*args, **kwargs)
+
+  return None
+
+def handleTimeout ():
+  logMessage("done")
+  exit(0)
 
 if __name__ == "__main__":
-  import brlapi
-  import time
   import errno
+
+  def writeProperty (name, value):
+    sys.stdout.write(name + ": " + value + "\n")
 
   try:
     brl = brlapi.Connection()
 
     try:
+      writeProperty("Driver", brl.driverName)
+      writeProperty("Columns", str(brl.displaySize[0]))
+      writeProperty("Rows", str(brl.displaySize[1]))
+
       brl.enterTtyMode()
       brl.writeText("The Python bindings for BrlAPI seem to be working.")
 
-      key = brl.readKey()
-      properties = brl.expandKeyCode(key)
-      brl.writeText("Key:%ld (Typ:%x Cmd:%x Arg:%x Flg:%x)" % (
-        key,
-        properties["type"],
-        properties["command"],
-        properties["argument"],
-        properties["flags"]
-      ))
+      while True:
+        key = readKey(
+          connection = brl,
+          timeout = 10,
+          target = handleTimeout
+        )
 
-      time.sleep(10)
+        properties = brl.expandKeyCode(key)
+        line = "Key:%ld (Typ:%x Cmd:%x Arg:%x Flg:%x)" % (
+          key,
+          properties["type"],
+          properties["command"],
+          properties["argument"],
+          properties["flags"]
+        )
+
+        brl.writeText(line)
+        sys.stdout.write(line + "\n")
+
       brl.leaveTtyMode()
     finally:
       del brl
   except brlapi.ConnectionError as e:
     if e.brlerrno == brlapi.ERROR_CONNREFUSED:
-      writeError("Connection to %s refused. BRLTTY is too busy..." % e.host)
+      logMessage("Connection to %s refused. BRLTTY is too busy..." % e.host)
     elif e.brlerrno == brlapi.ERROR_AUTHENTICATION:
-      writeError("Authentication with %s failed. Please check the permissions of %s" % (e.host, e.auth))
+      logMessage("Authentication with %s failed. Please check the permissions of %s" % (e.host, e.auth))
     elif e.brlerrno == brlapi.ERROR_LIBCERR and (e.libcerrno == errno.ECONNREFUSED or e.libcerrno == errno.ENOENT):
-      writeError("Connection to %s failed. Is BRLTTY really running?" % (e.host))
+      logMessage("Connection to %s failed. Is BRLTTY really running?" % (e.host))
     else:
-      writeError("Connection to BRLTTY at %s failed: " % (e.host))
+      logMessage("Connection to BRLTTY at %s failed: " % (e.host))
     print(e)
     print(e.brlerrno)
     print(e.libcerrno)
