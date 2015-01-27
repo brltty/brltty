7533576031a2e72c3a38a422411b00846d58d96b
Dave Mielke <dave@mielke.cc>
2015-01-27 12:27:01 -0500

    Add braille input support to brltest. (dm)

--- Programs/brltest.c
+++ Programs/brltest.c
@@ -33,11 +33,14 @@
 #include "parse.h"
 #include "file.h"
 #include "brl.h"
+#include "brl_input.h"
+#include "ktb.h"
 
 BrailleDisplay brl;
 
 static char *opt_brailleDevice;
 static char *opt_driversDirectory;
+static char *opt_tablesDirectory;
 static char *opt_writableDirectory;
 
 BEGIN_OPTION_TABLE(programOptions)
@@ -49,6 +52,16 @@ BEGIN_OPTION_TABLE(programOptions)
     .internal.setting = DRIVERS_DIRECTORY,
     .internal.adjust = fixInstallPath,
     .description = "Path to directory for loading drivers."
+  },
+
+  { .letter = 'T',
+    .word = "tables-directory",
+    .flags = OPT_Hidden,
+    .argument = strtext("directory"),
+    .setting.string = &opt_tablesDirectory,
+    .internal.setting = TABLES_DIRECTORY,
+    .internal.adjust = fixInstallPath,
+    .description = strtext("Path to directory containing tables.")
   },
 
   { .letter = 'W',
@@ -160,7 +173,26 @@ main (int argc, char *argv[]) {
 
     if (braille->construct(&brl, parameterSettings, opt_brailleDevice)) {
       if (ensureBrailleBuffer(&brl, LOG_INFO)) {
-      //learnMode(10000);
+        if (brl.keyNames) {
+          char *path = makeInputTablePath(opt_tablesDirectory, braille->definition.code, brl.keyBindings);
+
+          if (path) {
+            brl.keyTable = compileKeyTable(path, brl.keyNames);
+
+            free(path);
+          }
+        }
+
+        startBrailleInput();
+        stopBrailleInput();
+
+        if (brl.keyTable) {
+          KeyTable *table = brl.keyTable;
+
+          brl.keyTable = NULL;
+          destroyKeyTable(table);
+        }
+
         braille->destruct(&brl);		/* finish with the display */
         exitStatus = PROG_EXIT_SUCCESS;
       } else {
