b3b54209ee30284c47dcf7651b897979378832f2
Dave Mielke <dave@mielke.cc>
2015-11-22 09:36:23 -0500

    Set the log level (run-debug.bat) via a variable to make editing it easier. (dm)

--- Windows/run-debug.bat
+++ Windows/run-debug.bat
@@ -3,8 +3,9 @@ setlocal
 
 set programDirectory=%~dp0
 set logFile=%programDirectory%debug.log
+set logLevel=debug
 
 echo Running BRLTTY in debug mode. When done, close this window (for example, by
 echo using the alt-space menu) and see %logFile%
 
-"%programDirectory%bin\brltty" -n -ldebug "-L%logFile%" %*
+"%programDirectory%bin\brltty" -n "-l%logLevel%" "-L%logFile%" %*
