f4139ecafded0bcb41488cc40c8192d0c7d87ad3
Dave Mielke <dave@mielke.cc>
2018-01-07 14:55:52 -0500

    Simplify the test for if INFO toggles infoMode or textMaximized. (dm)

--- Programs/cmd_toggle.c
+++ Programs/cmd_toggle.c
@@ -215,11 +215,10 @@ handleToggleCommands (int command, void *data) {
       break;
 
     case BRL_CMD_INFO:
-      if ((prefs.statusPosition == spNone) || !prefs.statusCount || haveStatusCells()) {
+      if (haveStatusCells() || !(textMaximized || statusCount)) {
         toggleModeSetting(&infoMode, command);
       } else {
         ToggleResult result = toggleModeSetting(&textMaximized, command);
-
         if (result > TOGGLE_SAME) reconfigureBrailleWindow();
       }
       break;
