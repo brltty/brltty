68f845cb10e351b0df6ca4876862520d714c0da5
Dave Mielke <dave@mielke.cc>
2018-01-29 23:28:36 -0500

    brlapi:print-object (Lisp) should include the model identifier. (dm)

--- Bindings/Lisp/apitest.lisp
+++ Bindings/Lisp/apitest.lisp
@@ -1,3 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; libbrlapi - A library providing access to braille terminals for applications.
+;
+; Copyright (C) 2006-2018 by Mario Lang <mlang@delysid.org>
+;
+; libbrlapi comes with ABSOLUTELY NO WARRANTY.
+;
+; This is free software, placed under the terms of the
+; GNU Lesser General Public License, as published by the Free Software
+; Foundation; either version 2.1 of the License, or (at your option) any
+; later version. Please see the file LICENSE-LGPL for details.
+;
+; Web Page: http://brltty.com/
+;
+; This software is maintained by Dave Mielke <dave@mielke.cc>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'asdf)
 (require 'sb-posix)
 
@@ -18,6 +35,7 @@
       (display (brlapi:open-connection))
     )
 
+    (brlapi:print-object display *standard-output*)
     (brlapi:close-connection display)
   )
 )
