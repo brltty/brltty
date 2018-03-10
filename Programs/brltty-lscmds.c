1f3cc86b7429d23c92612a2af3b4fd6a349ee538
Dave Mielke <dave@mielke.cc>
2018-03-10 13:09:26 -0500

    Implement the brltty-lscmds command. (dm)

--- Programs/brltty-lscmds.c
+++ Programs/brltty-lscmds.c
@@ -19,13 +19,65 @@
 #include "prologue.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <wchar.h>
 
 #include "program.h"
 #include "options.h"
 #include "ktb_cmds.h"
+#include "cmd.h"
 
 BEGIN_OPTION_TABLE(programOptions)
 END_OPTION_TABLE
+
+static void
+writeCharacter (char character) {
+  putc(character, stdout);
+}
+
+static void
+writeCharacters (char character, size_t count) {
+  while (count > 0) {
+    writeCharacter(character);
+    count -= 1;
+  }
+}
+
+static void
+endLine (void) {
+  writeCharacter('\n');
+}
+
+static void
+endHeader (char character, size_t length) {
+  endLine();
+  writeCharacters(character, length);
+  endLine();
+  endLine();
+}
+
+static void
+writeString (const char *string) {
+  while (*string) writeCharacter(*string++);
+}
+
+static void
+writeStringHeader (const char *header, char character) {
+  writeString(header);
+  endHeader(character, strlen(header));
+}
+
+static void
+writeText (const wchar_t *text) {
+  printf("%ls", text);
+}
+
+static void
+writeTextHeader (const wchar_t *header, char character) {
+  writeText(header);
+  endHeader(character, wcslen(header));
+}
 
 void
 commandGroupHook_hotkeys (CommandGroupHookData *data) {
@@ -35,18 +87,130 @@ void
 commandGroupHook_keyboardFunctions (CommandGroupHookData *data) {
 }
 
+static void
+listModifiers (int include, const char *type, int *started, const CommandModifierEntry *modifiers) {
+  if (include) {
+    if (!*started) {
+      *started = 1;
+      printf("The following modifiers may be specified:\n\n");
+    }
+
+    printf("* %s", type);
+
+    if (modifiers) {
+      const CommandModifierEntry *modifier = modifiers;
+      char punctuation = ':';
+
+      while (modifier->name) {
+        printf("%c %s", punctuation, modifier->name);
+        punctuation = ',';
+        modifier += 1;
+      }
+    }
+
+    endLine();
+  }
+}
+
+static void
+listCommand (const CommandEntry *cmd) {
+  writeStringHeader(cmd->name, '~');
+
+  {
+    const char *description = cmd->description;
+    writeCharacter(toupper(*description++));
+    printf("%s.\n\n", description);
+  }
+
+  {
+    int started = 0;
+
+    listModifiers(cmd->isColumn, "a column number", &started, NULL);
+    listModifiers(cmd->isOffset, "an offset", &started, NULL);
+
+    listModifiers(
+      cmd->isToggle, "Toggle", &started,
+      commandModifierTable_toggle
+    );
+
+    listModifiers(
+      cmd->isMotion, "Motion", &started,
+      commandModifierTable_motion
+    );
+
+    listModifiers(
+      cmd->isRow, "Row", &started,
+      commandModifierTable_row
+    );
+
+    listModifiers(
+      cmd->isVertical, "Vertical", &started,
+      commandModifierTable_vertical
+    );
+
+    listModifiers(
+      cmd->isInput, "Input", &started,
+      commandModifierTable_input
+    );
+
+    listModifiers(
+      (cmd->isCharacter || cmd->isBraille), "Character", &started,
+      commandModifierTable_character
+    );
+
+    listModifiers(
+      cmd->isBraille, "Braille", &started,
+      commandModifierTable_braille
+    );
+
+    listModifiers(
+      cmd->isKeyboard, "Keyboard", &started,
+      commandModifierTable_keyboard
+    );
+
+    if (started) endLine();
+  }
+}
+
+static void
+listCommandGroup (const CommandGroupEntry *grp) {
+  writeTextHeader(grp->name, '-');
+
+  const CommandListEntry *cmd = grp->commands.table;
+  const CommandListEntry *cmdEnd = cmd + grp->commands.count;
+
+  while (cmd < cmdEnd) {
+    listCommand(findCommandEntry(cmd->command));
+    cmd += 1;
+  }
+}
+
+static void
+listCommandGroups (void) {
+  const CommandGroupEntry *grp = commandGroupTable;
+  const CommandGroupEntry *grpEnd = grp + commandGroupCount;
+
+  while (grp < grpEnd) {
+    listCommandGroup(grp);
+    grp += 1;
+  }
+}
+
 int
 main (int argc, char *argv[]) {
-  ProgramExitStatus exitStatus = PROG_EXIT_FATAL;
-
   {
     static const OptionsDescriptor descriptor = {
       OPTION_TABLE(programOptions),
-      .applicationName = "brltty-cmdlist"
+      .applicationName = "brltty-lscmds"
     };
 
     PROCESS_OPTIONS(descriptor, argc, argv);
   }
+fflush(stdout);
 
-  return exitStatus;
+  writeStringHeader("BRLTTY Command List", '=');
+  writeString(".. contents::\n\n");
+
+  listCommandGroups();
+  return PROG_EXIT_SUCCESS;
 }
