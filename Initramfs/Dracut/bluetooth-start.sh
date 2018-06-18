bbf52c02df9b6d372b485acbc15159c1acf4eb13
Dave Mielke <Dave@Mielke.cc>
2018-06-18 07:51:57 -0400

    In the initramfs, don't start dbus if Bluetooth has been disabled. (dm)

--- Initramfs/Dracut/bluetooth-start.sh
+++ Initramfs/Dracut/bluetooth-start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 getargbool 1 rd.brltty && getargbool 1 rd.brltty.bluetooth && {
-   hciconfig hci0 up
-   systemctl --no-block start bluetooth
+   systemctl -q is-active bluetooth || {
+      hciconfig hci0 up
+      systemctl --no-block start bluetooth
+   }
 }
--- Initramfs/Dracut/dbus-start.sh
+++ Initramfs/Dracut/dbus-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-systemctl --no-block start dbus
+getargbool 1 rd.brltty && getargbool 1 rd.brltty.bluetooth && {
+   systemctl -q is-active dbus || {
+      systemctl --no-block start dbus
+   }
+}
