9d0de2e97e0dc0ce097fae893d85a9eaa40c514b
Dave Mielke <Dave@Mielke.cc>
2018-06-18 19:06:12 -0400

    The initramfs doesn't need to do hciconfig hci0 up. (dm)

--- Initramfs/Dracut/bluetooth-start.sh
+++ Initramfs/Dracut/bluetooth-start.sh
@@ -2,7 +2,6 @@
 
 getargbool 1 rd.brltty && getargbool 1 rd.brltty.bluetooth && {
    systemctl -q is-active bluetooth || {
-      hciconfig hci0 up
       systemctl --no-block start bluetooth
    }
 }
