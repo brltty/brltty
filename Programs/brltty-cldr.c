bb394e851328225d078d8ec93efff42b05b1660c
Dave Mielke <Dave@Mielke.cc>
2018-09-29 14:00:14 -0400

    Add the -f [--output-format] option to brltty-cldr. (dm)

--- Programs/brltty-cldr.c
+++ Programs/brltty-cldr.c
@@ -19,16 +19,170 @@
 #include "prologue.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
+#include "program.h"
+#include "options.h"
+#include "log.h"
 #include "cldr.h"
+
+#define DEFAULT_OUTPUT_FORMAT "%n\\n"
+
+static char *opt_outputFormat;
+
+BEGIN_OPTION_TABLE(programOptions)
+  { .letter = 'f',
+    .word = "output-format",
+    .argument = strtext("string"),
+    .setting.string = &opt_outputFormat,
+    .internal.setting = DEFAULT_OUTPUT_FORMAT,
+    .description = strtext("Format of each output line.")
+  },
+END_OPTION_TABLE
+
+static void
+putCharacter (int character) {
+  if (putchar(character) == EOF) {
+    logMessage(LOG_ERR, "output error %d: %s", errno, strerror(errno));
+    exit(PROG_EXIT_FATAL);
+  }
+}
+
+static void
+putString (const char *string) {
+  while (*string) putCharacter(*string++ & 0XFF);
+}
 
 static
 CLDR_ANNOTATION_HANDLER(handleAnnotation) {
-  printf("%s: %s\n", parameters->sequence, parameters->name);
-  return 1;
+  typedef enum {LITERAL, FORMAT, ESCAPE} State;
+  State state = LITERAL;
+  const char *format = opt_outputFormat;
+
+  while (*format) {
+    int character = *format & 0XFF;
+
+    switch (state) {
+      case LITERAL: {
+        switch (character) {
+          case '%':
+            state = FORMAT;
+            break;
+
+          case '\\':
+            state = ESCAPE;
+            break;
+
+          default:
+            putCharacter(character);
+            break;
+        }
+
+        break;
+      }
+
+      case FORMAT: {
+        switch (character) {
+          case 'n':
+            putString(parameters->name);
+            break;
+
+          case 's':
+            putString(parameters->sequence);
+            break;
+
+          case '%':
+            putCharacter(character);
+            break;
+
+          default:
+            logMessage(LOG_ERR, "unrecognized format character: %c", character);
+            exit(PROG_EXIT_SYNTAX);
+        }
+
+        state = LITERAL;
+        break;
+      }
+
+      case ESCAPE: {
+        static const char escapes[] = {
+          ['a'] = '\a',
+          ['b'] = '\b',
+          ['e'] = '\e',
+          ['f'] = '\f',
+          ['n'] = '\n',
+          ['r'] = '\r',
+          ['t'] = '\t',
+          ['v'] = '\v',
+          ['\\'] = '\\'
+        };
+
+        switch (character) {
+          default: {
+            if (character < ARRAY_COUNT(escapes)) {
+              char escape = escapes[character];
+
+              if (escape) {
+                putCharacter(escape);
+                break;
+              }
+            }
+
+            logMessage(LOG_ERR, "unrecognized escape character: %c", character);
+            exit(PROG_EXIT_SYNTAX);
+          }
+        }
+
+        state = LITERAL;
+        break;
+      }
+    }
+
+    format += 1;
+  }
+
+  switch (state) {
+    case LITERAL:
+      return 1;
+
+    case FORMAT:
+      logMessage(LOG_ERR, "missing format character");
+      break;
+
+    case ESCAPE:
+      logMessage(LOG_ERR, "missing escape character");
+      break;
+  }
+
+  exit(PROG_EXIT_SYNTAX);
 }
 
 int
 main (int argc, char *argv[]) {
-  return !cldrParseFile("en", handleAnnotation, NULL);
+  {
+    static const OptionsDescriptor descriptor = {
+      OPTION_TABLE(programOptions),
+      .applicationName = "brltty-cldr",
+      .argumentsSummary = "input-file"
+    };
+    PROCESS_OPTIONS(descriptor, argc, argv);
+  }
+
+  if (argc < 1) {
+    fprintf(stderr, "missing annotations file name\n");
+    return PROG_EXIT_SYNTAX;
+  }
+
+  const char *inputFile = *argv++;
+  argc -= 1;
+
+  if (argc > 0) {
+    fprintf(stderr, "too many parameters\n");
+    return PROG_EXIT_SYNTAX;
+  }
+
+  return cldrParseFile(inputFile, handleAnnotation, NULL)?
+         PROG_EXIT_SUCCESS:
+         PROG_EXIT_FATAL;
 }
