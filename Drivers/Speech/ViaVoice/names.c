2eda66e17773f1628a4de206430191edf65035e6
Dave Mielke <Dave@Mielke.cc>
2019-01-03 21:56:37 -0500

    Make a table (not used) of the documented ViaVoice voice names. (dm)

---
+++ Drivers/Speech/ViaVoice/names.c
@@ -0,0 +1,285 @@
+typedef struct {
+   const char *name; // must be first
+   int language;
+   int voice;
+} NameChoice;
+
+static const NameChoice nameChoices[] = {
+   {  .name = "Reed",
+      .language = eciGeneralAmericanEnglish,
+      .voice = 1
+   },
+
+   {  .name = "Shelley",
+      .language = eciGeneralAmericanEnglish,
+      .voice = 2
+   },
+
+   {  .name = "Sandy",
+      .language = eciGeneralAmericanEnglish,
+      .voice = 3
+   },
+
+   {  .name = "Grandma",
+      .language = eciGeneralAmericanEnglish,
+      .voice = 7
+   },
+
+   {  .name = "Grandpa",
+      .language = eciGeneralAmericanEnglish,
+      .voice = 8
+   },
+
+   {  .name = "Justin",
+      .language = eciBritishEnglish,
+      .voice = 1
+   },
+
+   {  .name = "Jane",
+      .language = eciBritishEnglish,
+      .voice = 2
+   },
+
+   {  .name = "Nicky",
+      .language = eciBritishEnglish,
+      .voice = 3
+   },
+
+   {  .name = "Nanny",
+      .language = eciBritishEnglish,
+      .voice = 7
+   },
+
+   {  .name = "Gramps",
+      .language = eciBritishEnglish,
+      .voice = 8
+   },
+
+   {  .name = "Carlos",
+      .language = eciCastilianSpanish,
+      .voice = 1
+   },
+
+   {  .name = "Pilar",
+      .language = eciCastilianSpanish,
+      .voice = 2
+   },
+
+   {  .name = "Pepe",
+      .language = eciCastilianSpanish,
+      .voice = 3
+   },
+
+   {  .name = "Abuela",
+      .language = eciCastilianSpanish,
+      .voice = 7
+   },
+
+   {  .name = "Abuelo",
+      .language = eciCastilianSpanish,
+      .voice = 8
+   },
+
+   {  .name = "Jacques",
+      .language = eciStandardFrench,
+      .voice = 1
+   },
+
+   {  .name = "Jacqueline",
+      .language = eciStandardFrench,
+      .voice = 2
+   },
+
+   {  .name = "Marius",
+      .language = eciStandardFrench,
+      .voice = 3
+   },
+
+   {  .name = "Mamie",
+      .language = eciStandardFrench,
+      .voice = 7
+   },
+
+   {  .name = "Grandpère",
+      .language = eciStandardFrench,
+      .voice = 8
+   },
+
+   {  .name = "Max",
+      .language = eciStandardGerman,
+      .voice = 1
+   },
+
+   {  .name = "Gisela",
+      .language = eciStandardGerman,
+      .voice = 2
+   },
+
+   {  .name = "Matti",
+      .language = eciStandardGerman,
+      .voice = 3
+   },
+
+   {  .name = "Oma",
+      .language = eciStandardGerman,
+      .voice = 7
+   },
+
+   {  .name = "Opa",
+      .language = eciStandardGerman,
+      .voice = 8
+   },
+
+   {  .name = "Enrico",
+      .language = eciStandardItalian,
+      .voice = 1
+   },
+
+   {  .name = "Lucia",
+      .language = eciStandardItalian,
+      .voice = 2
+   },
+
+   {  .name = "Chicco",
+      .language = eciStandardItalian,
+      .voice = 3
+   },
+
+   {  .name = "Nonna",
+      .language = eciStandardItalian,
+      .voice = 7
+   },
+
+   {  .name = "Nonno",
+      .language = eciStandardItalian,
+      .voice = 8
+   },
+
+   {  .name = "Li3 Jing4",
+      .language = eciMandarinChinese,
+      .voice = 1
+   },
+
+   {  .name = "Wang2 Yan4",
+      .language = eciMandarinChinese,
+      .voice = 2
+   },
+
+   {  .name = "Li3 Dong1 Dong1",
+      .language = eciMandarinChinese,
+      .voice = 3
+   },
+
+   {  .name = "Nai3 Nai",
+      .language = eciMandarinChinese,
+      .voice = 7
+   },
+
+   {  .name = "Ye2 Ye",
+      .language = eciMandarinChinese,
+      .voice = 8
+   },
+
+   {  .name = "Zhi4 Ming2",
+      .language = eciTaiwaneseMandarin,
+      .voice = 1
+   },
+
+   {  .name = "Chun1 Jiao1",
+      .language = eciTaiwaneseMandarin,
+      .voice = 2
+   },
+
+   {  .name = "Xiao3 Bu4 Dian3",
+      .language = eciTaiwaneseMandarin,
+      .voice = 3
+   },
+
+   {  .name = "A3 Ma4",
+      .language = eciTaiwaneseMandarin,
+      .voice = 7
+   },
+
+   {  .name = "A3 Gong1",
+      .language = eciTaiwaneseMandarin,
+      .voice = 8
+   },
+
+   {  .name = "João",
+      .language = eciBrazilianPortuguese,
+      .voice = 1
+   },
+
+   {  .name = "Cláudia",
+      .language = eciBrazilianPortuguese,
+      .voice = 2
+   },
+
+   {  .name = "Chico",
+      .language = eciBrazilianPortuguese,
+      .voice = 3
+   },
+
+   {  .name = "Avó",
+      .language = eciBrazilianPortuguese,
+      .voice = 7
+   },
+
+   {  .name = "Avô",
+      .language = eciBrazilianPortuguese,
+      .voice = 8
+   },
+
+   {  .name = "Taroo",
+      .language = eciStandardJapanese,
+      .voice = 1
+   },
+
+   {  .name = "Hanako",
+      .language = eciStandardJapanese,
+      .voice = 2
+   },
+
+   {  .name = "Jiroo",
+      .language = eciStandardJapanese,
+      .voice = 3
+   },
+
+   {  .name = "Obaachan",
+      .language = eciStandardJapanese,
+      .voice = 7
+   },
+
+   {  .name = "Taroo",
+      .language = eciStandardJapanese,
+      .voice = 8
+   },
+
+   {  .name = "Antti",
+      .language = eciStandardFinnish,
+      .voice = 1
+   },
+
+   {  .name = "Tarja",
+      .language = eciStandardFinnish,
+      .voice = 2
+   },
+
+   {  .name = "Pekka",
+      .language = eciStandardFinnish,
+      .voice = 3
+   },
+
+   {  .name = "Isoäiti",
+      .language = eciStandardFinnish,
+      .voice = 7
+   },
+
+   {  .name = "Isoisä",
+      .language = eciStandardFinnish,
+      .voice = 8
+   },
+
+   {  .name = NULL  }
+};
+
