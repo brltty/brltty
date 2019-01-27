137e62f1ddb5cdfddf6d4c6b0f6a5d16b0a70f76
Dave Mielke <Dave@Mielke.cc>
2019-01-27 11:38:56 -0500

    Resolve a couple of warnings when building on MinGW. (dm)
    Windows defines the address of socket option data as char* (instead of void*).

--- Programs/io_misc.c
+++ Programs/io_misc.c
@@ -419,7 +419,7 @@ connectSocket (
         int error;
         socklen_t length = sizeof(error);
 
-        if (getsockopt(socketDescriptor, SOL_SOCKET, SO_ERROR, &error, &length) != -1) {
+        if (getsockopt(socketDescriptor, SOL_SOCKET, SO_ERROR, (void *)&error, &length) != -1) {
           if (!error) return 0;
           errno = error;
         }
@@ -438,7 +438,7 @@ setSocketLingerTime (SocketDescriptor socketDescriptor, int seconds) {
     .l_linger = seconds
   };
 
-  if (setsockopt(socketDescriptor, SOL_SOCKET, SO_LINGER, &linger, sizeof(linger)) != -1) return 1;
+  if (setsockopt(socketDescriptor, SOL_SOCKET, SO_LINGER, (const void *)&linger, sizeof(linger)) != -1) return 1;
   logSystemError("setsockopt[SO_LINGER]");
   return 0;
 }
