597299e07237bbe338844e101f88eac9c9349e8c
Dave Mielke <Dave@Mielke.cc>
2019-11-11 04:53:50 -0500

    BrlAPI: Send an erorr when setting a string parameter fails. (dm)

--- Programs/brlapi_server.c
+++ Programs/brlapi_server.c
@@ -1251,13 +1251,14 @@ static void param_readString(const char *string, void *data, size_t *size)
   memcpy(data, string, *size);
 }
 
-static int param_writeString(int (*handler) (const char *string), void *data, size_t size)
+static int param_writeString(Connection *c, int (*handler) (const char *string), void *data, size_t size)
 {
   char string[size + 1];
   memcpy(string, data, size);
   string[size] = 0;
 
   if (handler(string)) return 1;
+  WERR(c->fd, BRLAPI_ERROR_INVALID_PARAMETER, "set parameter failed");
   return 0;
 }
 
@@ -1349,7 +1350,7 @@ static int param_deviceIdentifier_read(Connection *c, brlapi_param_t param, uint
 
 static int param_deviceIdentifier_write(Connection *c, brlapi_param_t param, uint64_t subparam, uint32_t flags, void *data, size_t size)
 {
-  return param_writeString(changeBrailleDevice, data, size);
+  return param_writeString(c, changeBrailleDevice, data, size);
 }
 
 /* BRLAPI_PARAM_DEVICE_ONLINE */
@@ -1524,7 +1525,7 @@ static int param_computerBrailleTable_read(Connection *c, brlapi_param_t param, 
 
 static int param_computerBrailleTable_write(Connection *c, brlapi_param_t param, uint64_t subparam, uint32_t flags, void *data, size_t size)
 {
-  return param_writeString(changeTextTable, data, size);
+  return param_writeString(c, changeTextTable, data, size);
 }
 
 /* BRLAPI_PARAM_LITERARY_BRAILLE_TABLE */
@@ -1536,7 +1537,7 @@ static int param_literaryBrailleTable_read(Connection *c, brlapi_param_t param, 
 
 static int param_literaryBrailleTable_write(Connection *c, brlapi_param_t param, uint64_t subparam, uint32_t flags, void *data, size_t size)
 {
-  return param_writeString(changeContractionTable, data, size);
+  return param_writeString(c, changeContractionTable, data, size);
 }
 
 /* BRLAPI_PARAM_MESSAGE_LOCALE */
@@ -1552,7 +1553,7 @@ static int changeMessageLocale (const char *locale) {
 
 static int param_messageLocale_write(Connection *c, brlapi_param_t param, uint64_t subparam, uint32_t flags, void *data, size_t size)
 {
-  return param_writeString(changeMessageLocale, data, size);
+  return param_writeString(c, changeMessageLocale, data, size);
 }
 
 /* For parameters yet to be implemented */
