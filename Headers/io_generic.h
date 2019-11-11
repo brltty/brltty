0526624225b6c3d9b43b501237cecdcf86335b98
Dave Mielke <Dave@Mielke.cc>
2019-11-11 16:57:25 -0500

    Add gioGetResourceIdentifier(). (dm)

--- Headers/io_generic.h
+++ Headers/io_generic.h
@@ -40,6 +40,7 @@ extern int gioDisconnectResource (GioEndpoint *endpoint);
 
 extern const void *gioGetApplicationData (GioEndpoint *endpoint);
 extern const char *gioMakeResourceIdentifier (GioEndpoint *endpoint, char *buffer, size_t size);
+extern char *gioGetResourceIdentifier (GioEndpoint *endpoint);
 extern char *gioGetResourceName (GioEndpoint *endpoint);
 
 extern ssize_t gioWriteData (GioEndpoint *endpoint, const void *data, size_t size);
