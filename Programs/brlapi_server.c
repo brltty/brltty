5fd9968beaadd2a6211fca635894af60c1c97648
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-11-16 12:45:37 +0100

    brlapi params: do not allow clients to read the global cell rendering
    That would be information leak.

--- Programs/brlapi_server.c
+++ Programs/brlapi_server.c
@@ -1817,7 +1817,6 @@ static const ParamDispatch paramDispatch[BRLAPI_PARAM_COUNT] = {
 
   [BRLAPI_PARAM_RENDERED_CELLS] = {
     .local = 1,
-    .global = 1,
     .read = param_renderedCells_read,
   },
 
