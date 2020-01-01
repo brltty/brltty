884fd06c65995349240bf93f80da8d2c4d0225c7
Dave Mielke <Dave@Mielke.cc>
2020-01-01 12:13:49 -0500

    Update the copyright from 2019 to 2020. (dm)

--- Drivers/Braille/VisioBraille/vstp_transfer.c
+++ Drivers/Braille/VisioBraille/vstp_transfer.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006-2019 S&S
+ *  Copyright (C) 2006-2020 S&S
  *  Samuel Thibault <samuel.thibault@ens-lyon.org>
  *  Sébastien Hinderer <sebastien.hinderer@ens-lyon.org>
  *
