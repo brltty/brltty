e027c8c2e0d0cc73ce7058a23dcb6c178eb66262
Dave Mielke <dave@mielke.cc>
2020-09-03 06:12:53 -0400

    Android: Restructure the status summary code. (dm)

--- Android/Application/src/org/a11y/brltty/android/StatusSummary.java
+++ Android/Application/src/org/a11y/brltty/android/StatusSummary.java
@@ -18,18 +18,15 @@
 
 package org.a11y.brltty.android;
 
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import android.content.Context;
+import android.os.BatteryManager;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import android.text.format.DateFormat;
 
-import android.content.Context;
-import android.os.BatteryManager;
-
 import android.telephony.TelephonyManager;
+import android.telephony.CellSignalStrength;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoCdma;
 import android.telephony.CellInfoGsm;
@@ -37,10 +34,13 @@ import android.telephony.CellInfoLte;
 import android.telephony.CellInfoNr;
 import android.telephony.CellInfoTdscdma;
 import android.telephony.CellInfoWcdma;
-import android.telephony.CellSignalStrength;
 
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public abstract class StatusSummary {
   private StatusSummary () {
@@ -57,286 +57,297 @@ public abstract class StatusSummary {
   private final static WifiManager wifiManager = (WifiManager)
           context.getSystemService(Context.WIFI_SERVICE);
 
-  public interface Getter {
+  public interface Item {
     public String getLabel ();
     public String getValue ();
   }
 
-  private static class Group {
-    private final Getter groupGetters[];
+  public static class Group {
+    private final Item[] groupItems;
 
-    public Group (Getter... getters) {
-      groupGetters = getters;
+    public final Item[] getItems () {
+      return groupItems;
     }
 
-    public final Getter[] getGetters () {
-      return groupGetters;
+    public Group (Item... items) {
+      groupItems = items;
+    }
+
+    public boolean prepare () {
+      return true;
     }
   }
 
-  public final static Getter DEVICE_TIME = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
-
-    @Override
-    public String getValue () {
-      String format = DateFormat.is24HourFormat(context)? "HH:mm": "h:mma";
-      return new SimpleDateFormat(format).format(new Date());
-    }
-  };
-
-  public final static Getter DEVICE_BATTERY = new Getter() {
-    private final int INT_NO_VALUE = APITests.havePie? Integer.MIN_VALUE: 0;
-    private final long LONG_NO_VALUE = Long.MIN_VALUE;
-
-    @Override
-    public String getLabel () {
-      return "Bat";
-    }
-
-    @Override
-    public String getValue () {
-      if (APITests.haveLollipop) {
-        int value = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-
-        if (value != INT_NO_VALUE) {
-          return Integer.toString(value) + '%';
-        }
+  private static class DeviceGroup extends Group {
+    public final static Item TIME = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
       }
 
-      return null;
-    }
-  };
+      @Override
+      public String getValue () {
+        String format = DateFormat.is24HourFormat(context)? "HH:mm": "h:mma";
+        return new SimpleDateFormat(format).format(new Date());
+      }
+    };
 
-  public final static Getter CELL_OPERATOR = new Getter() {
-    @Override
-    public String getLabel () {
-      return "Cell";
-    }
+    public final static Item BATTERY = new Item() {
+      private final int INT_NO_VALUE = APITests.havePie? Integer.MIN_VALUE: 0;
+      private final long LONG_NO_VALUE = Long.MIN_VALUE;
 
-    @Override
-    public String getValue () {
-      String name = telephonyManager.getNetworkOperatorName();
-      return name;
-    }
-  };
-
-  public final static Getter CELL_ROAMING = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
-
-    @Override
-    public String getValue () {
-      return telephonyManager.isNetworkRoaming()? "roam": null;
-    }
-  };
-
-  public final static Getter CELL_TYPE = new Getter() {
-    private final Map<Integer, String> networkTypes = new HashMap<>();
-
-    {
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_1xRTT, "1xRTT");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_CDMA, "CDMA");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_EDGE, "EDGE");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_EHRPD, "eHRPD");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_0, "EVDO revision 0");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_A, "EVDO revision A");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_B, "EVDO revision B");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_GPRS, "GPRS");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_HSDPA, "HSDPA");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_HSPA, "HSPA");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_HSPAP, "HSPA+");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_HSUPA, "HSUPA");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_IDEN, "iDen");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_LTE, "LTE");
-      networkTypes.put(TelephonyManager.NETWORK_TYPE_UMTS, "UMTS");
-    }
-
-    @Override
-    public String getLabel () {
-      return null;
-    }
-
-    @Override
-    public String getValue () {
-      int type = telephonyManager.getNetworkType();
-      if (type == TelephonyManager.NETWORK_TYPE_UNKNOWN) return null;
-      return networkTypes.get(type);
-    }
-  };
-
-  public final static Getter CELL_SIGNAL = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
-
-    private final CellSignalStrength getCellSignalStrength (CellInfo info) {
-      if (APITests.haveJellyBeanMR1) {
-        if (info instanceof CellInfoCdma) {
-          return ((CellInfoCdma)info).getCellSignalStrength();
-        }
-
-        if (info instanceof CellInfoGsm) {
-          return ((CellInfoGsm)info).getCellSignalStrength();
-        }
-
-        if (info instanceof CellInfoLte) {
-          return ((CellInfoLte)info).getCellSignalStrength();
-        }
+      @Override
+      public String getLabel () {
+        return "Bat";
       }
 
-      if (APITests.haveJellyBeanMR2) {
-        if (info instanceof CellInfoWcdma) {
-          return ((CellInfoWcdma)info).getCellSignalStrength();
-        }
-      }
+      @Override
+      public String getValue () {
+        if (APITests.haveLollipop) {
+          int value = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
 
-      if (APITests.haveQ) {
-        if (info instanceof CellInfoNr) {
-          return ((CellInfoNr)info).getCellSignalStrength();
-        }
-
-        if (info instanceof CellInfoTdscdma) {
-          return ((CellInfoTdscdma)info).getCellSignalStrength();
-        }
-      }
-
-      return null;
-    }
-
-    @Override
-    public String getValue () {
-      if (APITests.haveJellyBeanMR1) {
-        List<CellInfo> infoList = telephonyManager.getAllCellInfo();
-
-        if (infoList != null) {
-          CellInfo info = infoList.get(0);
-          CellSignalStrength css = getCellSignalStrength(info);
-
-          if (css != null) {
-            int level = css.getLevel();
-            return Integer.toString((level * 100) / 4) + '%';
+          if (value != INT_NO_VALUE) {
+            return Integer.toString(value) + '%';
           }
         }
+
+        return null;
+      }
+    };
+
+    public DeviceGroup () {
+      super(TIME, BATTERY);
+    }
+  }
+
+  private static class CellGroup extends Group {
+    public final static Item OPERATOR = new Item() {
+      @Override
+      public String getLabel () {
+        return "Cell";
       }
 
-      return null;
-    }
-  };
+      @Override
+      public String getValue () {
+        String name = telephonyManager.getNetworkOperatorName();
+        return name;
+      }
+    };
 
-  private static WifiInfo wifiInfo = null;
-
-  public final static Getter WIFI_SSID = new Getter() {
-    @Override
-    public String getLabel () {
-      return "WiFi";
-    }
-
-    @Override
-    public String getValue () {
-      wifiInfo = wifiManager.getConnectionInfo();
-
-      if (wifiInfo != null) {
-        String ssid = wifiInfo.getSSID();
-        if ((ssid != null) && !ssid.isEmpty()) return ssid;
+    public final static Item ROAMING = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
       }
 
-      return null;
-    }
-  };
+      @Override
+      public String getValue () {
+        return telephonyManager.isNetworkRoaming()? "roam": null;
+      }
+    };
 
-  public final static Getter WIFI_SIGNAL = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
+    public final static Item TECHNOLOGY = new Item() {
+      private final Map<Integer, String> networkTypes = new HashMap<>();
 
-    @Override
-    public String getValue () {
-      if (wifiInfo != null) {
-        int rssi = wifiInfo.getRssi();
-        int percentage = wifiManager.calculateSignalLevel(rssi, 100);
-        return Integer.toString(percentage) + '%';
+      {
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_1xRTT, "1xRTT");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_CDMA, "CDMA");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_EDGE, "EDGE");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_EHRPD, "eHRPD");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_0, "EVDO revision 0");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_A, "EVDO revision A");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_EVDO_B, "EVDO revision B");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_GPRS, "GPRS");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_HSDPA, "HSDPA");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_HSPA, "HSPA");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_HSPAP, "HSPA+");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_HSUPA, "HSUPA");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_IDEN, "iDen");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_LTE, "LTE");
+        networkTypes.put(TelephonyManager.NETWORK_TYPE_UMTS, "UMTS");
       }
 
-      return null;
-    }
-  };
-
-  public final static Getter WIFI_SPEED = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
-
-    @Override
-    public String getValue () {
-      if (wifiInfo != null) {
-        int speed = wifiInfo.getLinkSpeed();
-        return Integer.toString(speed) + WifiInfo.LINK_SPEED_UNITS;
+      @Override
+      public String getLabel () {
+        return null;
       }
 
-      return null;
-    }
-  };
+      @Override
+      public String getValue () {
+        int type = telephonyManager.getNetworkType();
+        if (type == TelephonyManager.NETWORK_TYPE_UNKNOWN) return null;
+        return networkTypes.get(type);
+      }
+    };
 
-  public final static Getter WIFI_FREQUENCY = new Getter() {
-    @Override
-    public String getLabel () {
-      return null;
-    }
+    public final static Item SIGNAL = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
+      }
 
-    @Override
-    public String getValue () {
-      if (APITests.haveLollipop) {
-        if (wifiInfo != null) {
-          int frequency = wifiInfo.getFrequency();
-          return Integer.toString(frequency) + WifiInfo.FREQUENCY_UNITS;
+      private final CellSignalStrength getCellSignalStrength (CellInfo info) {
+        if (APITests.haveJellyBeanMR1) {
+          if (info instanceof CellInfoCdma) {
+            return ((CellInfoCdma)info).getCellSignalStrength();
+          }
+
+          if (info instanceof CellInfoGsm) {
+            return ((CellInfoGsm)info).getCellSignalStrength();
+          }
+
+          if (info instanceof CellInfoLte) {
+            return ((CellInfoLte)info).getCellSignalStrength();
+          }
         }
+
+        if (APITests.haveJellyBeanMR2) {
+          if (info instanceof CellInfoWcdma) {
+            return ((CellInfoWcdma)info).getCellSignalStrength();
+          }
+        }
+
+        if (APITests.haveQ) {
+          if (info instanceof CellInfoNr) {
+            return ((CellInfoNr)info).getCellSignalStrength();
+          }
+
+          if (info instanceof CellInfoTdscdma) {
+            return ((CellInfoTdscdma)info).getCellSignalStrength();
+          }
+        }
+
+        return null;
       }
 
-      return null;
+      @Override
+      public String getValue () {
+        if (APITests.haveJellyBeanMR1) {
+          List<CellInfo> infoList = telephonyManager.getAllCellInfo();
+
+          if (infoList != null) {
+            CellInfo info = infoList.get(0);
+            CellSignalStrength css = getCellSignalStrength(info);
+
+            if (css != null) {
+              int level = css.getLevel();
+              return Integer.toString((level * 100) / 4) + '%';
+            }
+          }
+        }
+
+        return null;
+      }
+    };
+
+    public CellGroup () {
+      super(OPERATOR, ROAMING, TECHNOLOGY, SIGNAL);
     }
-  };
+  }
 
-  private final static Group GROUP_DEVICE = new Group(
-    DEVICE_TIME
-  , DEVICE_BATTERY
-  );
+  private static class WifiGroup extends Group {
+    private static WifiInfo wifiInfo = null;
 
-  private final static Group GROUP_CELL = new Group(
-    CELL_OPERATOR
-  , CELL_ROAMING
-  , CELL_TYPE
-  , CELL_SIGNAL
-  );
+    @Override
+    public boolean prepare () {
+      if (!super.prepare()) return false;
+      wifiInfo = wifiManager.getConnectionInfo();
+      return wifiInfo != null;
+    }
 
-  private final static Group GROUP_WIFI = new Group(
-    WIFI_SSID
-  , WIFI_SIGNAL
-  , WIFI_SPEED
-  , WIFI_FREQUENCY
-  );
+    public final static Item SSID = new Item() {
+      @Override
+      public String getLabel () {
+        return "WiFi";
+      }
 
-  private final static Group GROUPS[] = {
-    GROUP_DEVICE
-  , GROUP_CELL
-  , GROUP_WIFI
+      @Override
+      public String getValue () {
+        if (wifiInfo != null) {
+          String ssid = wifiInfo.getSSID();
+          if ((ssid != null) && !ssid.isEmpty()) return ssid;
+        }
+
+        return null;
+      }
+    };
+
+    public final static Item SIGNAL = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
+      }
+
+      @Override
+      public String getValue () {
+        if (wifiInfo != null) {
+          int rssi = wifiInfo.getRssi();
+          int percentage = wifiManager.calculateSignalLevel(rssi, 100);
+          return Integer.toString(percentage) + '%';
+        }
+
+        return null;
+      }
+    };
+
+    public final static Item SPEED = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
+      }
+
+      @Override
+      public String getValue () {
+        if (wifiInfo != null) {
+          int speed = wifiInfo.getLinkSpeed();
+          return Integer.toString(speed) + WifiInfo.LINK_SPEED_UNITS;
+        }
+
+        return null;
+      }
+    };
+
+    public final static Item FREQUENCY = new Item() {
+      @Override
+      public String getLabel () {
+        return null;
+      }
+
+      @Override
+      public String getValue () {
+        if (APITests.haveLollipop) {
+          if (wifiInfo != null) {
+            int frequency = wifiInfo.getFrequency();
+            return Integer.toString(frequency) + WifiInfo.FREQUENCY_UNITS;
+          }
+        }
+
+        return null;
+      }
+    };
+
+    public WifiGroup () {
+      super(SSID, SIGNAL, SPEED, FREQUENCY);
+    }
+  }
+
+  public final static Group DEVICE = new DeviceGroup();
+  public final static Group CELL = new CellGroup();
+  public final static Group WIFI = new WifiGroup();
+
+  private final static Group[] GROUPS = {
+    DEVICE, CELL, WIFI
   };
 
   public static String get () {
     StringBuilder status = new StringBuilder();
 
     for (Group group : GROUPS) {
+      if (!group.prepare()) continue;
       boolean first = true;
 
-      for (Getter getter : group.getGetters()) {
-        String value = getter.getValue();
+      for (Item item : group.getItems()) {
+        String value = item.getValue();
 
         if ((value == null) || value.isEmpty()) {
           if (first) break;
@@ -349,7 +360,7 @@ public abstract class StatusSummary {
         }
 
         if (status.length() > 0) status.append(' ');
-        String label = getter.getLabel();
+        String label = item.getLabel();
         if ((label != null) && !label.isEmpty()) status.append(label).append(':');
         status.append(value);
       }
