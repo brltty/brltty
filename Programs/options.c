e90bf8e87397682191cd593f145a09a8b1c77a3b
Dave Mielke <Dave@Mielke.cc>
2020-12-30 00:28:37 -0500

    Allow an option to not have a short (letter) specifier. (dm)

--- Programs/options.c
+++ Programs/options.c
@@ -46,26 +46,33 @@ typedef struct {
   const OptionEntry *optionTable;
   unsigned int optionCount;
 
-  unsigned char ensuredSettings[0X100];
+  uint8_t *ensuredSettings;
 
-  unsigned exitImmediately:1;
-  unsigned warning:1;
-  unsigned syntaxError:1;
+  uint8_t exitImmediately:1;
+  uint8_t warning:1;
+  uint8_t syntaxError:1;
 } OptionProcessingInformation;
-
-static int
-wordMeansTrue (const char *word) {
-  return strcasecmp(word, FLAG_TRUE_WORD) == 0;
-}
-
-static int
-wordMeansFalse (const char *word) {
-  return strcasecmp(word, FLAG_FALSE_WORD) == 0;
-}
 
 static int
 hasExtendableArgument (const OptionEntry *option) {
   return option->argument && (option->flags & OPT_Extend);
+}
+
+static uint8_t *
+getEnsuredSetting (
+  const OptionProcessingInformation *info,
+  const OptionEntry *option
+) {
+  return &info->ensuredSettings[option - info->optionTable];
+}
+
+static void
+setEnsuredSetting (
+  const OptionProcessingInformation *info,
+  const OptionEntry *option,
+  uint8_t yes
+) {
+  *getEnsuredSetting(info, option) = yes;
 }
 
 static int
@@ -74,7 +81,7 @@ ensureSetting (
   const OptionEntry *option,
   const char *value
 ) {
-  unsigned char *ensured = &info->ensuredSettings[option->letter];
+  uint8_t *ensured = getEnsuredSetting(info, option);
 
   if (!*ensured || hasExtendableArgument(option)) {
     *ensured = 1;
@@ -89,20 +96,22 @@ ensureSetting (
       }
     } else {
       if (option->setting.flag) {
-        if (wordMeansTrue(value)) {
-          *option->setting.flag = 1;
-        } else if (wordMeansFalse(value)) {
-          *option->setting.flag = 0;
-        } else if (!(option->flags & OPT_Extend)) {
-          logMessage(LOG_ERR, "%s: %s", gettext("invalid flag setting"), value);
-          info->warning = 1;
-        } else {
+        if (option->flags & OPT_Extend) {
           int count;
 
           if (isInteger(&count, value) && (count >= 0)) {
             *option->setting.flag = count;
           } else {
             logMessage(LOG_ERR, "%s: %s", gettext("invalid counter setting"), value);
+            info->warning = 1;
+          }
+        } else {
+          unsigned int on;
+
+          if (validateFlagKeyword(&on, value)) {
+            *option->setting.flag = on;
+          } else {
+            logMessage(LOG_ERR, "%s: %s", gettext("invalid flag setting"), value);
             info->warning = 1;
           }
         }
@@ -114,7 +123,7 @@ ensureSetting (
 }
 
 static void
-printHelp (
+showUsage (
   OptionProcessingInformation *info,
   FILE *outputStream,
   unsigned int lineWidth,
@@ -122,19 +131,21 @@ printHelp (
   int all
 ) {
   char line[lineWidth+1];
+  unsigned int letterWidth = 0;
   unsigned int wordWidth = 0;
   unsigned int argumentWidth = 0;
 
-  for (unsigned int optionIndex=0; optionIndex<info->optionCount; ++optionIndex) {
+  for (unsigned int optionIndex=0; optionIndex<info->optionCount; optionIndex+=1) {
     const OptionEntry *option = &info->optionTable[optionIndex];
+    if (!all && (option->flags & OPT_Hidden)) continue;
 
     if (option->word) {
       unsigned int length = strlen(option->word);
-
       if (option->argument) length += 1;
       wordWidth = MAX(wordWidth, length);
     }
 
+    if (option->letter) letterWidth = 3;
     if (option->argument) argumentWidth = MAX(argumentWidth, strlen(option->argument));
   }
 
@@ -145,34 +156,27 @@ printHelp (
     fputs(gettext("option"), outputStream);
     fputs(" ...]", outputStream);
   }
+
   if (argumentsSummary && *argumentsSummary) {
     fprintf(outputStream, " %s", argumentsSummary);
   }
+
   fprintf(outputStream, "\n");
 
-  for (unsigned int optionIndex=0; optionIndex<info->optionCount; ++optionIndex) {
+  for (unsigned int optionIndex=0; optionIndex<info->optionCount; optionIndex+=1) {
     const OptionEntry *option = &info->optionTable[optionIndex];
-    unsigned int lineLength = 0;
-
     if (!all && (option->flags & OPT_Hidden)) continue;
 
-    line[lineLength++] = '-';
-    line[lineLength++] = option->letter;
-    line[lineLength++] = ' ';
+    unsigned int lineLength = 0;
 
-    {
-      unsigned int end = lineLength + argumentWidth;
-
-      if (option->argument) {
-        size_t argumentLength = strlen(option->argument);
-
-        memcpy(line+lineLength, option->argument, argumentLength);
-        lineLength += argumentLength;
-      }
-
-      while (lineLength < end) line[lineLength++] = ' ';
+    if (option->letter) {
+      line[lineLength++] = '-';
+      line[lineLength++] = option->letter;
     }
-    line[lineLength++] = ' ';
+
+    while (lineLength < letterWidth) {
+      line[lineLength++] = ' ';
+    }
 
     {
       unsigned int end = lineLength + 2 + wordWidth;
@@ -185,6 +189,20 @@ printHelp (
         memcpy(line+lineLength, option->word, wordLength);
         lineLength += wordLength;
         if (option->argument) line[lineLength++] = '=';
+      }
+
+      while (lineLength < end) line[lineLength++] = ' ';
+    }
+    line[lineLength++] = ' ';
+
+    {
+      unsigned int end = lineLength + argumentWidth;
+
+      if (option->argument) {
+        size_t argumentLength = strlen(option->argument);
+
+        memcpy(line+lineLength, option->argument, argumentLength);
+        lineLength += argumentLength;
       }
 
       while (lineLength < end) line[lineLength++] = ' ';
@@ -286,84 +304,40 @@ processCommandLine (
   char ***argumentVector,
   const char *argumentsSummary
 ) {
-  int lastOptInd = -1;
-
-  const char resetPrefix = '+';
   const char *reset = NULL;
+  const char resetPrefix = '+';
   int resetLetter;
 
-#ifdef ALLOW_DOS_OPTION_SYNTAX
-  const char dosPrefix = '/';
-  int dosSyntax = 0;
-#endif /* ALLOW_DOS_OPTION_SYNTAX */
+  const int firstNonLetter = 0X80;
+  const OptionEntry *optionEntries[firstNonLetter + info->optionCount];
 
-  int optHelp = 0;
-  int optHelpAll = 0;
-  const OptionEntry *optionEntries[0X100];
-  char shortOptions[1 + (info->optionCount * 2) + 1];
-
-#ifdef HAVE_GETOPT_LONG
-  struct option longOptions[(info->optionCount * 2) + 1];
-
-  {
-    struct option *opt = longOptions;
-
-    for (unsigned int index=0; index<info->optionCount; index+=1) {
-      const OptionEntry *entry = &info->optionTable[index];
-
-      if (entry->word) {
-        opt->name = entry->word;
-        opt->has_arg = entry->argument? required_argument: no_argument;
-        opt->flag = NULL;
-        opt->val = entry->letter;
-        opt += 1;
-
-        if (!entry->argument && entry->setting.flag) {
-          static const char *const noPrefix = "no-";
-          size_t noLength = strlen(noPrefix);
-          char *name;
-
-          if (strncasecmp(noPrefix, entry->word, noLength) == 0) {
-            name = strdup(&entry->word[noLength]);
-          } else {
-            size_t size = noLength + strlen(entry->word) + 1;
-
-            if ((name = malloc(size))) {
-              snprintf(name, size, "%s%s", noPrefix, entry->word);
-            }
-          }
-
-          if (name) {
-            opt->name = name;
-            opt->has_arg = no_argument;
-            opt->flag = &resetLetter;
-            opt->val = entry->letter;
-            opt += 1;
-          } else {
-            logMallocError();
-          }
-        }
-      }
-    }
-
-    memset(opt, 0, sizeof(*opt));
-  }
-#endif /* HAVE_GETOPT_LONG */
-
-  for (unsigned int index=0; index<0X100; index+=1) {
+  for (unsigned int index=0; index<ARRAY_COUNT(optionEntries); index+=1) {
     optionEntries[index] = NULL;
   }
 
+  int optionLetters[info->optionCount];
+  char shortOptions[2 + (info->optionCount * 2) + 1];
+
   {
+    int nextNonLetter = firstNonLetter;
+
     char *opt = shortOptions;
     *opt++ = '+';
+    *opt++ = ':';
 
     for (unsigned int index=0; index<info->optionCount; index+=1) {
       const OptionEntry *entry = &info->optionTable[index];
-      optionEntries[entry->letter] = entry;
+      int letter = entry->letter;
 
-      *opt++ = entry->letter;
-      if (entry->argument) *opt++ = ':';
+      if (letter) {
+        *opt++ = letter;
+        if (entry->argument) *opt++ = ':';
+      } else {
+        letter = nextNonLetter++;
+      }
+
+      optionLetters[index] = letter;
+      optionEntries[letter] = entry;
 
       if (entry->argument) {
         if (entry->setting.string) *entry->setting.string = NULL;
@@ -375,17 +349,78 @@ processCommandLine (
     *opt = 0;
   }
 
-  if (*argumentCount > 1)
+#ifdef HAVE_GETOPT_LONG
+  struct option longOptions[(info->optionCount * 2) + 1];
+
+  {
+    struct option *opt = longOptions;
+
+    for (unsigned int index=0; index<info->optionCount; index+=1) {
+      const OptionEntry *entry = &info->optionTable[index];
+      const char *word = entry->word;
+      if (!word) continue;
+      int letter = optionLetters[index];
+
+      opt->name = word;
+      opt->has_arg = entry->argument? required_argument: no_argument;
+      opt->flag = NULL;
+      opt->val = letter;
+      opt += 1;
+
+      if (!entry->argument && entry->setting.flag) {
+        char *name;
+
+        const char *noPrefix = "no-";
+        size_t noLength = strlen(noPrefix);
+
+        if (strncasecmp(noPrefix, word, noLength) == 0) {
+          name = strdup(&word[noLength]);
+        } else {
+          size_t size = noLength + strlen(word) + 1;
+
+          if ((name = malloc(size))) {
+            snprintf(name, size, "%s%s", noPrefix, word);
+          }
+        }
+
+        if (name) {
+          opt->name = name;
+          opt->has_arg = no_argument;
+          opt->flag = &resetLetter;
+          opt->val = letter;
+          opt += 1;
+        } else {
+          logMallocError();
+        }
+      }
+    }
+
+    memset(opt, 0, sizeof(*opt));
+  }
+#endif /* HAVE_GETOPT_LONG */
+
 #ifdef ALLOW_DOS_OPTION_SYNTAX
-    if (*(*argumentVector)[1] == dosPrefix) dosSyntax = 1;
+  const char dosPrefix = '/';
+  int dosSyntax = 0;
+
+  if (*argumentCount > 1) {
+    if (*(*argumentVector)[1] == dosPrefix) {
+      dosSyntax = 1;
+    }
+  }
 #endif /* ALLOW_DOS_OPTION_SYNTAX */
 
   opterr = 0;
   optind = 1;
+  int lastOptInd = -1;
+
+  int optHelp = 0;
+  int optHelpAll = 0;
 
   while (1) {
     int option;
     char prefix = '-';
+    resetLetter = 0;
 
     if (optind == *argumentCount) {
       option = -1;
@@ -395,61 +430,55 @@ processCommandLine (
 #ifdef ALLOW_DOS_OPTION_SYNTAX
       if (dosSyntax) {
         prefix = dosPrefix;
-        optind++;
+        optind += 1;
 
         if (*argument != dosPrefix) {
           option = -1;
         } else {
           char *name = argument + 1;
           size_t nameLength = strcspn(name, ":");
-          char *value = (nameLength == strlen(name))? NULL: (name + nameLength + 1);
+          char *value = name[nameLength]? (name + nameLength + 1): NULL;
           const OptionEntry *entry;
 
           if (nameLength == 1) {
             entry = optionEntries[option = *name];
           } else {
-            int count = info->optionCount;
-            entry = info->optionTable;
             option = -1;
 
-            while (count--) {
-              if (entry->word) {
-                size_t wordLength = strlen(entry->word);
+            for (unsigned int index=0; index<info->optionCount; index+=1) {
+              entry = &info->optionTable[index];
+              const char *word = entry->word;
 
-                if ((wordLength == nameLength) &&
-                    (strncasecmp(entry->word, name, wordLength) == 0)) {
-                  option = entry->letter;
+              if (word) {
+                if ((nameLength == strlen(word)) &&
+                    (strncasecmp(word, name, nameLength) == 0)) {
+                  option = optionLetters[index];
                   break;
                 }
               }
-
-              entry++;
             }
 
             if (option < 0) {
-              option = 0;
               entry = NULL;
+              option = 0;
             }
           }
 
           optopt = option;
-          optarg = NULL;
+          optarg = value;
 
           if (!entry) {
             option = '?';
           } else if (entry->argument) {
-            if (!(optarg = value)) option = ':';
+            if (!optarg) option = ':';
           } else if (value) {
-            if (!entry->setting.flag) goto dosBadFlagValue;
+            unsigned int on;
 
-            if (!wordMeansTrue(value)) {
-              if (wordMeansFalse(value)) {
-                resetLetter = option;
-                option = 0;
-              } else {
-              dosBadFlagValue:
-                option = '?';
-              }
+            if (!validateFlagKeyword(&on, value)) {
+              option = '-';
+            } else if (!on) {
+              resetLetter = option;
+              option = 0;
             }
           }
         }
@@ -461,12 +490,13 @@ processCommandLine (
 
         if (!(option = *reset++)) {
           reset = NULL;
-          optind++;
+          optind += 1;
           continue;
         }
 
         {
           const OptionEntry *entry = optionEntries[option];
+
           if (entry && !entry->argument && entry->setting.flag) {
             resetLetter = option;
             option = 0;
@@ -488,18 +518,19 @@ processCommandLine (
 #endif /* HAVE_GETOPT_LONG */
       }
     }
-    if (option == -1) break;
 
+    if (option == -1) break;
     /* continue on error as much as possible, as often we are typing blind
      * and won't even see the error message unless the display comes up.
      */
+
     switch (option) {
       default: {
         const OptionEntry *entry = optionEntries[option];
 
         if (entry->argument) {
           if (!*optarg) {
-            info->ensuredSettings[option] = 0;
+            setEnsuredSetting(info, entry, 0);
             break;
           }
 
@@ -520,18 +551,18 @@ processCommandLine (
           }
         }
 
-        info->ensuredSettings[option] = 1;
+        setEnsuredSetting(info, entry, 1);
         break;
       }
 
-      case 0: {
+      case 0: { // reset a flag
         const OptionEntry *entry = optionEntries[resetLetter];
         *entry->setting.flag = 0;
-        info->ensuredSettings[resetLetter] = 1;
+        setEnsuredSetting(info, entry, 1);
         break;
       }
 
-      case '?': {
+      case '?': { // an unknown option has been specified
         const char *message = gettext("unknown option");
 
         if (optopt) {
@@ -544,23 +575,32 @@ processCommandLine (
         break;
       }
 
-      case ':': /* An invalid option has been specified. */
+      case ':': { // the operand for a string option hasn't been specified
         logMessage(LOG_ERR, "%s: %c%c", gettext("missing operand"), prefix, optopt);
         info->syntaxError = 1;
         break;
+      }
 
-      case 'H':                /* help */
+      case '-': { // the operand for an option is invalid
+        logMessage(LOG_ERR, "%s: %c%c: %s", gettext("invalid operand"), prefix, optopt, optarg);
+        info->warning = 1;
+        break;
+      }
+
+      case 'H': // full help
         optHelpAll = 1;
         /* fall through */
-      case 'h':                /* help */
+      case 'h': // help - show usage summary and then exit
         optHelp = 1;
         break;
     }
   }
-  *argumentVector += optind, *argumentCount -= optind;
+
+  *argumentVector += optind;
+  *argumentCount -= optind;
 
   if (optHelp) {
-    printHelp(info, stdout, 79, argumentsSummary, optHelpAll);
+    showUsage(info, stdout, 79, argumentsSummary, optHelpAll);
     info->exitImmediately = 1;
   }
 
@@ -681,7 +721,7 @@ processInternalSettings (
   OptionProcessingInformation *info,
   int config
 ) {
-  for (unsigned int optionIndex=0; optionIndex<info->optionCount; ++optionIndex) {
+  for (unsigned int optionIndex=0; optionIndex<info->optionCount; optionIndex+=1) {
     const OptionEntry *option = &info->optionTable[optionIndex];
 
     if (!(option->flags & OPT_Config) == !config) {
@@ -988,9 +1028,13 @@ exitOptions (void *data) {
 
 ProgramExitStatus
 processOptions (const OptionsDescriptor *descriptor, int *argumentCount, char ***argumentVector) {
+  uint8_t ensuredSettings[descriptor->optionCount];
+  memset(ensuredSettings, 0, sizeof(ensuredSettings));
+
   OptionProcessingInformation info = {
     .optionTable = descriptor->optionTable,
     .optionCount = descriptor->optionCount,
+    .ensuredSettings = ensuredSettings,
 
     .exitImmediately = 0,
     .warning = 0,
@@ -998,11 +1042,6 @@ processOptions (const OptionsDescriptor *descriptor, int *argumentCount, char **
   };
 
   onProgramExit("options", exitOptions, (void *)descriptor);
-
-  for (unsigned int index=0; index<0X100; index+=1) {
-    info.ensuredSettings[index] = 0;
-  }
-
   beginProgram(*argumentCount, *argumentVector);
   processCommandLine(&info, argumentCount, argumentVector, descriptor->argumentsSummary);
 
