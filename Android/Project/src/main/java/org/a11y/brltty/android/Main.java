0111be07ce387ee4e9c16155d0d432dc23905c95
Dave Mielke <dave@mielke.cc>
2021-04-27 18:05:31 -0400

    Android: Add a gradle project. (dm)

---
+++ Android/Project/src/main/java/org/a11y/brltty/android/Main.java
@@ -0,0 +1,15 @@
+package org.a11y.brltty.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
