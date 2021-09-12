e9765e9a2fc69f31d701fac962a3590101940111
Dave Mielke <Dave@Mielke.cc>
2021-09-12 15:50:23 -0400

    Add functions for parsing a csv file. (dm)

--- Tools/csv.tcl
+++ Tools/csv.tcl
@@ -55,3 +55,122 @@ proc csvMakeTable {rows {width ""}} {
    return $table
 }
 
+proc csvApplyHeaders {table} {
+   set rows [list]
+   set headers [lindex $table 0]
+
+   foreach columns [lrange $table 1 end] {
+      set row [dict create]
+      set count [llength $columns]
+      set index 0
+
+      foreach header $headers {
+         if {$index == $count} {
+            set column ""
+         } else {
+            set column [lindex $columns $index]
+            incr index
+         }
+
+         dict set row $header $column
+      }
+
+      lappend rows $row
+   }
+
+   return $rows
+}
+
+proc csvParseString {string} {
+   set rows [list]
+   set columns [list]
+   set column ""
+
+   set finishColumn {
+      lappend columns $column
+      set column ""
+   }
+
+   set finishRow {
+      if {([llength $columns] > 0) || ([string length $column] > 0)} {
+         eval $finishColumn
+         lappend rows $columns
+         set columns [list]
+      }
+   }
+
+   set state unquoted
+   set length [string length $string]
+   set index 0
+
+   while {$index < $length} {
+      set character [string index $string $index]
+      set isQuote [string equal $character {"}]
+      incr index
+
+      switch -exact $state {
+         unquoted {
+            if {$isQuote} {
+               set state inQuotes
+               continue
+            }
+
+            switch -exact $character {
+               "," {
+                  eval $finishColumn
+                  continue
+               }
+
+               "\n" {
+                  eval $finishRow
+                  continue
+               }
+
+               "\r" {
+                  eval $finishRow
+                  set state wasCarriageReturn
+                  continue
+               }
+            }
+         }
+
+         inQuotes {
+            if {$isQuote} {
+               set state wasQuote
+               continue
+            }
+         }
+
+         wasQuote {
+            if {!$isQuote} {
+               set state unquoted
+               incr index -1
+               continue
+            }
+
+            set state inQuotes
+         }
+
+         wasCarriageReturn {
+            if {[string equal $character "\n"]} {
+               set state unquoted
+               continue
+            }
+         }
+
+         default {
+            return -code error "unrecognized csv parse state: $state"
+         }
+      }
+
+      append column $character
+   }
+
+   eval $finishRow
+   return $rows
+}
+
+proc csvLoadFile {file} {
+   return [csvApplyHeaders [csvParseString [readFile $file]]]
+}
+
