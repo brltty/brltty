2cc341f76e4ef0a0e5b13448bf4452981c48ae01
Dave Mielke <dave@mielke.cc>
2021-11-15 11:31:32 -0500

    Android: Improve the USB device attached log. (dm)

--- Android/Gradle/app/src/main/java/org/a11y/brltty/android/settings/UsbDeviceAttachedMonitor.java
+++ Android/Gradle/app/src/main/java/org/a11y/brltty/android/settings/UsbDeviceAttachedMonitor.java
@@ -26,24 +26,49 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.hardware.usb.*;
 import java.util.Map;
+import java.util.LinkedHashMap;
 
 public class UsbDeviceAttachedMonitor extends Activity {
   private final static String LOG_TAG = UsbDeviceAttachedMonitor.class.getName();
 
-  private static void addParameter (Map<String, String> parameters, String name, String value) {
-    if ((value != null) && !value.isEmpty()) {
-      parameters.put(name, value);
+  private static void addProperty (Map<String, String> properties, String name, int value) {
+    if (value != 0) {
+      properties.put(name, String.format("%04X", value));
     }
   }
 
+  private static void addProperty (Map<String, String> properties, String name, String value) {
+    if (value != null) {
+      if (!value.isEmpty()) {
+        char quote = '"';
+        properties.put(name, String.format("%c%s%c", quote, value, quote));
+      }
+    }
+  }
+
+  private static Map<String, String> getProperties (UsbDevice device) {
+    Map<String, String> properties = new LinkedHashMap<>();
+
+    addProperty(properties, "VID", device.getVendorId());
+    addProperty(properties, "PID", device.getProductId());
+    addProperty(properties, "Serial", device.getSerialNumber());
+    addProperty(properties, "Manufacturer", device.getManufacturerName());
+    addProperty(properties, "Product", device.getProductName());
+
+    return properties;
+  }
+
   private static String makeDescription (UsbDevice device) {
-    Map<String, String> parameters = DeviceCollection.newParameters();
-    UsbDeviceCollection.putParameters(parameters, device);
+    Map<String, String> properties = getProperties(device);
+    StringBuilder description = new StringBuilder();
 
-    addParameter(parameters, "manufacturerName", device.getManufacturerName());
-    addParameter(parameters, "productName", device.getProductName());
+    for (String name : properties.keySet()) {
+      String value = properties.get(name);
+      if (description.length() > 0) description.append(' ');
+      description.append(name).append(':').append(value);
+    }
 
-    return UsbDeviceCollection.makeReference(parameters);
+    return description.toString();
   }
 
   @Override
