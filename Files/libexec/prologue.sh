cc9f18b581e532dc14669f2b866ff95e3cea9713
Dave Mielke <Dave@Mielke.cc>
2022-03-07 11:24:18 -0500

    Add the apiSetServer shell function.

--- Files/libexec/prologue.sh
+++ Files/libexec/prologue.sh
@@ -4,3 +4,8 @@ installTree="$(dirname "${BASH_SOURCE[0]}")"
 installTree="$(realpath --no-symlinks -- "${installTree}")"
 readonly installTree="$(dirname "${installTree}")"
 . "${installTree}/bin/brltty-prologue.sh"
+
+apiSetServer() {
+   export BRLAPI_HOST=":canute"
+}
+
