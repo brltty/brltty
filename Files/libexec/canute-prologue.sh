d6a797443278619b093492c6fe559d1e54ff8a98
Dave Mielke <Dave@Mielke.cc>
2022-03-09 07:58:03 -0500

    Add the brlttyPackageProperty shell function.

--- Files/libexec/canute-prologue.sh
+++ Files/libexec/canute-prologue.sh
@@ -5,10 +5,22 @@ installTree="$(realpath --no-symlinks -- "${installTree}")"
 readonly installTree="$(dirname "${installTree}")"
 . "${installTree}/bin/brltty-prologue.sh"
 
+readonly ldConfigurationFile="/etc/ld.so.conf.d/brlapi.conf"
+readonly apiLinksDirectory="${installTree}/lib"
+readonly canuteSystemdUnit="brltty@canute.path"
+
+brlttyPackageProperty() {
+   local resultVariable="${1}"
+   local name="${2}"
+
+   local result
+   result="$(PKG_CONFIG_PATH="${installTree}/lib/pkgconfig" pkg-config "--variable=${name}" -- brltty)" || return 1
+
+   setVariable "${resultVariable}" "${result}"
+   return 0
+}
+
 canuteSetServer() {
    export BRLAPI_HOST=":canute"
 }
 
-readonly ldConfigurationFile="/etc/ld.so.conf.d/brlapi.conf"
-readonly apiLinksDirectory="${installTree}/lib"
-readonly canuteSystemdUnit="brltty@canute.path"
--- Makefile
+++ Makefile
@@ -30,7 +30,7 @@ unconfigure:
 	-rm -f -- $(AUTOGEN_TARGET)
 
 install: build
-	sudo $(MAKE) --silent -C $(BUILD_TREE)/Programs -- install-commands install-tools install-manpages install-drivers install-tables install-core-headers install-api-libraries install-api-commands install-xbrlapi install-api-headers install-api-bindings
+	sudo $(MAKE) --silent -C $(BUILD_TREE)/Programs -- install-pkgconfig-file install-commands install-tools install-manpages install-drivers install-tables install-core-headers install-api-libraries install-api-commands install-xbrlapi install-api-headers install-api-bindings
 	sudo $(MAKE) --silent -C $(BUILD_TREE) -- install-systemd install-udev
 	Make/add-files -s $(SOURCE_TREE) -b $(BUILD_TREE)
 
