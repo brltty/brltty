30dc89bc93e0487b2a448f1488eb488ea9568cdb
Dave Mielke <Dave@Mielke.cc>
2022-10-28 15:47:23 -0400

    Failing to install all of the reqruired packages shouldn't be a make failure.

--- build.mk
+++ build.mk
@@ -2,7 +2,7 @@ CLONE_TARGET = $(SOURCE_TREE)/configure.ac
 clone: $(CLONE_TARGET)
 $(CLONE_TARGET):
 	git clone --quiet --branch master -- https://github.com/brltty/brltty $(SOURCE_TREE)
-	sudo $(SOURCE_TREE)/Tools/reqpkgs -q -i
+	-sudo $(SOURCE_TREE)/Tools/reqpkgs -q -i
 
 AUTOGEN_TARGET = $(SOURCE_TREE)/configure
 autogen: $(AUTOGEN_TARGET)
