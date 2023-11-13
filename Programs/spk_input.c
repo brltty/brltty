27fd8a1977fa3f250f7c6bb1d20c00f851c2ed55
Dave Mielke <Dave@Mielke.cc>
2023-11-13 05:34:44 -0500

    If multiple writes to the speech input FIFO occur quickly then they're read as a single sequence.

--- Programs/spk_input.c
+++ Programs/spk_input.c
@@ -38,97 +38,105 @@ struct SpeechInputObjectStruct {
 
 static
 NAMED_PIPE_INPUT_CALLBACK(handleSpeechInput) {
-  const unsigned char *buffer = parameters->buffer;
-  size_t bufferSize = parameters->length;
-  const unsigned char *end = buffer + bufferSize;
+  const unsigned char *const buffer = parameters->buffer;
+  const size_t bufferSize = parameters->length;
+  const unsigned char *const bufferEnd = buffer + bufferSize;
+  const unsigned char *from = buffer;
 
-  SayOptions options = 0;
-  unsigned char colour = SCR_COLOUR_DEFAULT;
-  int dontSpeak = 0;
-  int asTune = 0;
+  while (from < bufferEnd) {
+    SayOptions options = 0;
+    unsigned char colour = SCR_COLOUR_DEFAULT;
+    int dontSpeak = 0;
+    int asTune = 0;
 
-  while (buffer < end) {
-     if (*buffer != ASCII_ESC) break;
-     if (++buffer == end) break;
+    while (from < bufferEnd) {
+      if (*from != ASCII_ESC) break;
+      if (++from == bufferEnd) break;
 
-     switch (*buffer++) {
-       case '!':
-         options |= SAY_OPT_MUTE_FIRST;
-         break;
+      switch (*from++) {
+        case '!':
+          options |= SAY_OPT_MUTE_FIRST;
+          break;
 
-       case 'c':
-         if (buffer < end) colour = *buffer++;
-         break;
+        case 'c':
+          if (from < bufferEnd) colour = *from++;
+          break;
 
-       case 'd':
-         if (!prefs.autospeakDeletedCharacters) dontSpeak = 1;
-         break;
+        case 'd':
+          if (!prefs.autospeakDeletedCharacters) dontSpeak = 1;
+          break;
 
-       case 'i':
-         if (!prefs.autospeakInsertedCharacters) dontSpeak = 1;
-         break;
+        case 'i':
+          if (!prefs.autospeakInsertedCharacters) dontSpeak = 1;
+          break;
 
-       case 'l':
-         if (!prefs.autospeakSelectedLine) dontSpeak = 1;
-         break;
+        case 'l':
+          if (!prefs.autospeakSelectedLine) dontSpeak = 1;
+          break;
 
-       case 'r':
-         if (!prefs.autospeakReplacedCharacters) dontSpeak = 1;
-         break;
+        case 'r':
+          if (!prefs.autospeakReplacedCharacters) dontSpeak = 1;
+          break;
 
-       case 's':
-         if (!prefs.autospeakSelectedCharacter) dontSpeak = 1;
-         break;
+        case 's':
+          if (!prefs.autospeakSelectedCharacter) dontSpeak = 1;
+          break;
 
-       case 't':
-         asTune = 1;
-         break;
+        case 't':
+          asTune = 1;
+          break;
 
-       case 'w':
-         if (!prefs.autospeakCompletedWords) dontSpeak = 1;
-         break;
-     }
-  }
-
-  size_t textLength = end - buffer;
-  char text[textLength + 1];
-  memcpy(text, buffer, textLength);
-  text[textLength] = 0;
-  logMessage(LOG_CATEGORY(SPEECH_EVENTS), "speech input: %s", text);
-
-  if (options & SAY_OPT_MUTE_FIRST) {
-    if (asTune || !textLength) {
-      muteSpeech(&spk, "speech input");
-    }
-  }
-
-  if (textLength) {
-    if (asTune) {
-      TuneBuilder *tb = newTuneBuilder();
-
-      if (tb) {
-        setTuneSourceName(tb, "speech-input");
-        setTuneSourceIndex(tb, 0);
-
-        if (parseTuneString(tb, "p100")) {
-          if (parseTuneString(tb, text)) {
-            ToneElement *tune = getTune(tb);
-            if (tune) tunePlayTones(tune, TPO_FREE);
-          }
-        }
-
-        destroyTuneBuilder(tb);
+        case 'w':
+          if (!prefs.autospeakCompletedWords) dontSpeak = 1;
+          break;
       }
-    } else if (!dontSpeak) {
-      size_t attributesCount = countUtf8Characters(text);
-      unsigned char attributes[attributesCount + 1];
-      memset(attributes, colour, attributesCount);
-
-      sayUtf8Characters(
-        &spk, text, attributes,
-        textLength, attributesCount, options
-      );
     }
+
+    const unsigned char *to = memchr(from, ASCII_ESC, (bufferEnd - from));
+    if (!to) to = bufferEnd;
+
+    size_t textLength = to - from;
+    char text[textLength + 1];
+    memcpy(text, from, textLength);
+    text[textLength] = 0;
+    logMessage(LOG_CATEGORY(SPEECH_EVENTS), "speech input: %s", text);
+
+    if (options & SAY_OPT_MUTE_FIRST) {
+      if (asTune || !textLength) {
+        muteSpeech(&spk, "speech input");
+      }
+    }
+
+    if (textLength) {
+      if (asTune) {
+        TuneBuilder *tb = newTuneBuilder();
+
+        if (tb) {
+          setTuneSourceName(tb, "speech-input");
+          setTuneSourceIndex(tb, 0);
+
+          if (parseTuneString(tb, "p100")) {
+            if (parseTuneString(tb, text)) {
+              ToneElement *tune = getTune(tb);
+              if (tune) tunePlayTones(tune, TPO_FREE);
+            }
+          }
+
+          destroyTuneBuilder(tb);
+        }
+      } else if (!dontSpeak) {
+        size_t attributesCount = countUtf8Characters(text);
+        unsigned char attributes[attributesCount + 1];
+        memset(attributes, colour, attributesCount);
+
+        sayUtf8Characters(
+          &spk, text, attributes,
+          textLength, attributesCount, options
+        );
+      }
+    }
+
+    from = to;
   }
 
   return bufferSize;
