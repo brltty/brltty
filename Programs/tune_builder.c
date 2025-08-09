d9c191810a2d45aa862cab273a859d3ec1db72e8
Leonard de Ruijter <alderuijter@gmail.com>
2025-08-09 22:25:05 +0200

    Fix tune builder

--- Programs/tune_builder.c
+++ Programs/tune_builder.c
@@ -550,11 +550,14 @@ parseTuneText (TuneBuilder *tb, const wchar_t *text) {
   static const wchar_t *delimiters = WS_C(" \t\r\n");
   wchar_t *string = buffer;
   wchar_t *operand;
-
+#if !( (defined(__MINGW32__) && defined(__i386__)) || defined(__MSDOS__) )
   wchar_t *next;
+#endif /* __MINGW32__ */
 
   while ((operand = wcstok(string, delimiters
+#if !( (defined(__MINGW32__) && defined(__i386__)) || defined(__MSDOS__) )
                            , &next
+#endif /* __MINGW32__ */
                           ))) {
     if (*operand == '#') break;
     if (!parseCommand(tb, operand)) return 0;
